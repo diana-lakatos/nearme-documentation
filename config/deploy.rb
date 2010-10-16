43df89d36196c29bd3edab05ce6a54050bf73a92
Keith Pitt <me@keithpitt.com>
2010-10-17 01:28:05 +1030

    Working on the deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,17 @@
+
+begin
+  require 'thinking_sphinx/deploy/capistrano'
+rescue
+  puts "You have to install thinking-sphinx locally."
+  puts "gem install thinking-sphinx --pre"
+  exit 1
+end
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))  # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                                # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2'                           # Or whatever env you want it to run in.
+set :rvm_type, :user
+
 set :user, "deploy"
 set :application, "desksnearme"
 set :repository,  "git@github.com:railsrumble/rr10-team-154.git"
@@ -8,23 +22,32 @@ set :scm, :git
 
 role :web, "desksnear.me"                          # Your HTTP server, Apache/etc
 role :app, "desksnear.me"                          # This may be the same as your `Web` server
-role :db,  "desksnear.me", :primary => true # This is where Rails migrations will run
+role :db,  "desksnear.me", :primary => true        # This is where Rails migrations will run
 role :db,  "desksnear.me"
 
 after "deploy:symlink", "deploy:update_crontab"
+after "deploy:symlink", "thinking_sphinx:stop", "thinking_sphinx:configure", "thinking_sphinx:start"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+after "deploy:symlink" do
+  run "cd #{deploy_to}/current; bundle iuunstall --without=test"
+  run "cd #{deploy_to}/current; rake db:migrate RAILS_ENV=production --trace"
+end
 
 namespace :deploy do
+
   task :start do ; end
   task :stop do ; end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
   desc "Update the crontab file"
   task :update_crontab, :roles => :db do
     run "cd #{release_path} && whenever --update-crontab #{application}"
   end
+
 end
+
+after "deploy:setup", "thinking_sphinx:shared_sphinx_folder"
+
