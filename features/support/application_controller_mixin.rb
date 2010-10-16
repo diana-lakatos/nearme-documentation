54a95fd28cc4b6e9d89187a40457d27523925abb
Keith Pitt <me@keithpitt.com>
2010-10-16 17:03:18 +1030

    Dont need to ask if its the test env.

--- features/support/application_controller_mixin.rb
+++ features/support/application_controller_mixin.rb
@@ -1,9 +1,7 @@
 module ApplicationControllerMixin
-
   # Lookup the current user in the ENV
   def current_user
-    session[:user_id] = ENV.delete('CURRENT_USER_ID') if Rails.env.test? && ENV['CURRENT_USER_ID']
+    session[:user_id] = ENV.delete('CURRENT_USER_ID') if ENV['CURRENT_USER_ID']
     super
   end
-
 end
