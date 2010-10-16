055a3f0a170c67ac9a1aaa0f63e277591e995c12
Keith Pitt <me@keithpitt.com>
2010-10-16 17:43:04 +1030

    Started working on the workplace show feature.

--- app/models/workplace.rb
+++ app/models/workplace.rb
@@ -1,6 +1,10 @@
 class Workplace < ActiveRecord::Base
   acts_as_mappable :auto_geocode => true
   belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
-  validates_presence_of :name, :address, :maximum_desks, :confirm_bookings
+  validates_presence_of :name, :address, :maximum_desks
   validates_numericality_of :maximum_desks, :only_integer => true, :greater_than => 0
+
+  def belongs_to?(user)
+    user && user == creator
+  end
 end
