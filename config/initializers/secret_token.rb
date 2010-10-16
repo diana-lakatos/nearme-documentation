23684bcdf34bdb485ab58da7633b9dbcf16fc220
Warren Seen <warren@warrenseen.com>
2010-10-16 11:08:30 +1100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DesksnearMe::Application.config.secret_token = 'f6112bccda67820972abc9b311eec16c0a33e858f66390514b140f70b4951a9411825ab10956cc86aa29c2b2a7e12d2c448c6292e2e69fade6277127ee2efee5'
