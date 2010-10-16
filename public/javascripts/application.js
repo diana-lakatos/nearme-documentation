78635863c5fa49f83bf2c70d7340e509c308c82a
Bodaniel Jeanes <me@bjeanes.com>
2010-10-16 12:42:56 -0700

    Fancybox for photos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  $("#photos a:has(img), #photos [href$=.jpg], #photos a[href$=.png], #photos a[href$=.gif]")
+    .attr("rel", "photos").fancybox({
+      transitionIn: "elastic",
+      transitionOut: "elastic",
+      titlePosition: "over",
+      padding: 0
+    });
+});
