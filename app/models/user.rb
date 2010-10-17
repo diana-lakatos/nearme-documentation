9908791e7b6f98ff786146e30d6a164f24d938d7
Keith Pitt <me@keithpitt.com>
2010-10-17 19:44:51 +1030

    Name should be attr_accessable

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   
   def apply_omniauth(omniauth)
     self.email = omniauth['user_info']['email'] if email.blank?
