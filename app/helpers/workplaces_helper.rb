0844290bfb59ae25a5392dd24a1547499e68824e
Bodaniel Jeanes <me@bjeanes.com>
2010-10-17 16:51:16 -0700

    Adjust length of text on featured home page

--- app/helpers/workplaces_helper.rb
+++ app/helpers/workplaces_helper.rb
@@ -1,5 +1,5 @@
 module WorkplacesHelper
-  def workplace_inline_description(workplace)
-    raw(truncate(strip_tags(workplace.company_description_html), :length => 65))
+  def workplace_inline_description(workplace, length = 65)
+    raw(truncate(strip_tags(workplace.company_description_html), :length => length))
   end
 end
