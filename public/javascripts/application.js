a1b741c0218392d69b4f78efe9393af949a9a708
Bodaniel Jeanes <me@bjeanes.com>
2010-10-16 22:25:19 -0700

    Cache location for search bar and automatically fill search box if empty

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,15 @@
+function setSearchFormToLocation(form, location) {
+  var input  = $("input:text", form),
+      button = $("input.geolocation", form);
+  
+  button.addClass("active").attr("data-geo-val", location).click(function(){
+    input.val($(this).attr("data-geo-val")).focus();
+  });
+  
+  if(input.val() == "")
+    button.click();
+}
+
 $(function(){
   $(".fancy-photos a:has(img), .fancy-photos [href$=.jpg], .fancy-photos a[href$=.png], .fancy-photos a[href$=.gif]")
     .attr("rel", "photos").fancybox({
@@ -6,29 +18,36 @@ $(function(){
       titlePosition: "over",
       padding: 0
     });
+
+
+  var searchForm      = $("form.big_search");
+  
+  if(searchForm.length >= 1) {
+    var currentLocation = $.cookie("currentLocation");
     
-  if(Modernizr.geolocation) {
-    var form = $("form.big_search");
-    if(form.length == 1) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode({ address: position.coords.latitude + "," + position.coords.longitude }, function(results, status) {
-          var components = results[0].address_components,
-              city = null,
-              country = null;
-          for(var i = 0, l = components.length; i < l; i++) {
-             var types = components[i]['types'];
-             if(types.indexOf('locality') >= 0 && types.indexOf('political') >= 0) city = components[i]['long_name'];
-             if(types.indexOf('country') >= 0 && types.indexOf('political') >= 0) country = components[i]['long_name'];
-          }
-          if(city) {
-            $("input.geolocation", form).addClass("active").attr("geo-val", city + ", " + country).click(function(){
-              var form = $(this).parents('form').first();
-              $("input:text", form).val($(this).attr("geo-val")).focus();
-            });
-          }
+    if(currentLocation)
+      setSearchFormToLocation(searchForm, currentLocation);
+    else {
+      if(Modernizr.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var geocoder = new google.maps.Geocoder();
+          geocoder.geocode({ address: position.coords.latitude + "," + position.coords.longitude }, function(results, status) {
+            var components = results[0].address_components,
+                city = null,
+                country = null;
+            for(var i = 0, l = components.length; i < l; i++) {
+               var types = components[i]['types'];
+               if(types.indexOf('locality') >= 0 && types.indexOf('political') >= 0) city = components[i]['long_name'];
+               if(types.indexOf('country') >= 0 && types.indexOf('political') >= 0) country = components[i]['long_name'];
+            }
+            if(city) {
+              currentLocation = city + ", " + country;
+              $.cookie("currentLocation", currentLocation);
+              setSearchFormToLocation(searchForm, currentLocation);
+            }
+          });
         });
-      });
+      }
     }
   }
 });
