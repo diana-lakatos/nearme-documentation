002355013bfc4b329270c1eae774c62daf3aad12
Bodaniel Jeanes <me@bjeanes.com>
2010-10-17 03:51:54 -0700

    More emailing out when booking state changes

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -4,23 +4,37 @@ class BookingMailer < ActionMailer::Base
   default :from => "noreply@desksnear.me"
 
   def pending_confirmation(booking)
-    @booking   = booking
-    @workplace = booking.workplace
-    @user      = booking.user
-
+    setup_defaults(booking)
     generate_mail("Your booking is pending confirmation")
   end
 
-  def booking_created(booking)
-    @booking   = booking
-    @workplace = booking.workplace
-    @user      = @workplace.creator
-    @url       = dashboard_url
+  def booking_confirmed(booking)
+    setup_defaults(booking)
+    generate_mail("Your booking has been confirmed")
+  end
+
+  def unconfirmed_booking_created(booking)
+    setup_defaults(booking)
+
+    @user = @workplace.creator
+    @url  = dashboard_url
 
     generate_mail("A new booking requires your confirmation")
   end
 
+  def confirmed_booking_created(booking)
+    setup_defaults(booking)
+    @user = @workplace.creator
+    generate_mail("You have a new booking")
+  end
+
   private
+    def setup_defaults(booking)
+      @booking   = booking
+      @workplace = booking.workplace
+      @user      = booking.user
+    end
+
     def generate_mail(subject)
       mail :subject => "[DesksNear.Me] #{subject}",
            :to      => @user.email
--- app/observers/booking_observer.rb
+++ app/observers/booking_observer.rb
@@ -2,14 +2,16 @@ class BookingObserver < ActiveRecord::Observer
 
   def after_create(booking)
     if booking.workplace.confirm_bookings?
-      BookingMailer.booking_created(booking).deliver
+      BookingMailer.unconfirmed_booking_created(booking).deliver
       BookingMailer.pending_confirmation(booking).deliver
     else
+      BookingMailer.confirmed_booking_created(booking).deliver
     end
   end
 
   def after_confirm(booking, transction)
     Feed.create(:user => booking.user, :workplace => booking.workplace, :booking => booking, :activity => "booked")
+    BookingMailer.booking_confirmed(booking).deliver
   end
 
 end
