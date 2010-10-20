db310c8a6b28d7988ebe05e864f23193c1544283
Keith Pitt <me@keithpitt.com>
2010-10-20 19:10:22 +1030

    Updated db backup script.

--- extras/backup_database.rb
+++ extras/backup_database.rb
@@ -1,16 +1,17 @@
+#!/home/deploy/.rvm/bin/ruby
+
 require "rubygems"
 require "aws/s3"
 
-
-# user = "desksnearme"
-# db = "desksnearme_production"
-# file_name = "/home/deploy/backups/#{db}_#{Time.now.to_i}.tar.gz"
-user = "postgres"
-db = "desksnearme_development"
-dir = "/Users/keith/Code/desksnearme/extras/"
+user = "desksnearme"
+db = "desksnearme_production"
+dir = "/home/deploy/backups"
+# user = "postgres"
+# db = "desksnearme_development"
+# dir = "/Users/keith/Code/desksnearme/extras/"
 bucket = "desksnearme"
 
-puts "Connecting to S3"
+puts "[#{Time.now}] Connecting to S3"
 AWS::S3::Base.establish_connection!(
   :access_key_id     => 'AKIAIZ5FVYS75LSDRTYQ',
   :secret_access_key => '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'
@@ -19,14 +20,14 @@ AWS::S3::Base.establish_connection!(
 file_name = "#{db}_#{Time.now.to_i}.tar.gz"
 path = File.join(dir, file_name)
 
-puts "Backup up #{db} to #{path}"
+puts "[#{Time.now}] Backup up #{db} to #{path}"
 `pg_dump -U #{user} -f #{path} -F tar #{db}`
 
 upload_file = "/backups/#{file_name}"
-puts "Uploading to S3 #{upload_file}"
-AWS::S3::S3Object.store(upload_file, open(file_name), bucket)
+puts "[#{Time.now}] Uploading to S3 #{upload_file}"
+AWS::S3::S3Object.store(upload_file, open(path), bucket)
 
-puts "Removing old file..."
+puts "[#{Time.now}] Removing old file..."
 File.delete(path)
 
-puts "Done!"
+puts "[#{Time.now}] Done!"
