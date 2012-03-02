910fd6b37e4ee243df954a1d33cd302fc202d82e
Bodaniel Jeanes <me@bjeanes.com>
2012-03-01 18:16:29 -0600

    Update S3 details to use MDYD account

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage              = :s3
-    config.s3_access_key_id     = 'AKIAIZ5FVYS75LSDRTYQ'
-    config.s3_secret_access_key = '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'
-    config.s3_bucket            = 'desksnearme'
+    config.s3_access_key_id     = 'AKIAILGH3PESU3PCUJEQ'
+    config.s3_secret_access_key = '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'
+    config.s3_bucket            = 'desksnearme.production'
   else
     config.storage = :file
   end
