f867d6cbbe732a9d263bd986df1221632f1acba9
Sai Perchard <sai.perchard@saitech.com.au>
2012-06-28 22:35:08 +0930

    Fix insecure items (update CarrierWave - to use s3 via SSL; use SSL for typekit and google maps APIs).

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  if Rails.env.production? || Rails.env.development?
     config.storage              = :s3
     config.s3_access_key_id     = 'AKIAILGH3PESU3PCUJEQ'
     config.s3_secret_access_key = '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'
-    config.s3_bucket            = 'desksnearme.production'
+    config.s3_bucket            = 's3.amazonaws.com/desksnearme.production'
+    config.s3_use_ssl           = true
+    config.s3_cnamed            = true
   else
     config.storage = :file
   end
