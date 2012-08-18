2abded4aeba1586d3a0ae4493359a731f1842f78
Zee Spencer <zee@zeespencer.com>
2012-08-18 15:50:57 -0400

    Fixes failing location feature

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,7 +6,7 @@ class LocationsController < ApplicationController
     location.creator ||= current_user
     if location.save
       flash[:success] = "Successfully created location"
-      redirect_to new_location_listing_path(location)
+      redirect_to new_listing_path
     else
       flash.now[:error] = "There was a problem saving your location. Please try again"
       render :new
