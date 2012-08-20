cca93e373504daed411cb29c1fbf873e4b3e24c5
Zee Spencer <zee@zeespencer.com>
2012-08-20 14:41:18 -0400

    Fixes failing mailer test

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,8 @@
-if Rails.env.staging? || Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
+SENDGRID_MAIL_SETTINGS = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com'
-  }
-  ActionMailer::Base.delivery_method = :smtp
-else
-  ActionMailer::Base.delivery_method = :sendmail
-end
+}
