f863a6d724c82e3012b8186d32fd456da6f7d7f0
Zee Spencer <zee@zeespencer.com>
2012-09-02 15:40:25 -0400

    Change the secret so cookies are reset

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DesksnearMe::Application.config.secret_token = 'f6112bccda67820972abc9b311eec16c0a33e858f66390514b140f70b4951a9411825ab10956cc86aa29c2b2a7e12d2c448c6292e2e69fade6277127ee2efee5'
+DesksnearMe::Application.config.secret_token = 'l&]{l8=Y>b+f}&5Ku2[`6~jX4)Q6-xx,85&E]~+*?}V&rR91q[QCgxgvgD\PfUhJ.tVzm4znz!Hk?|aazas`c42GAjRItl}BkIa9>\89_S[=Uvm6TE"F4;VFVVcS}{/Y[6p8`Xj}A)%]F6m'
