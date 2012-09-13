2ab802126c5201630768f1bf88c1cd63351bd466
Nik Wakelin <nik@codetocustomer.com>
2012-09-13 19:22:12 +1200

    fix default reservation amount

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120831172659) do
+ActiveRecord::Schema.define(:version => 20120913065803) do
 
   create_table "amenities", :force => true do |t|
     t.string   "name"
@@ -167,10 +167,10 @@ ActiveRecord::Schema.define(:version => 20120831172659) do
     t.integer  "owner_id"
     t.string   "state"
     t.string   "confirmation_email"
-    t.integer  "total_amount_cents"
+    t.integer  "total_amount_cents", :default => 0
     t.string   "currency"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
     t.datetime "deleted_at"
     t.text     "comment"
   end
