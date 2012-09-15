c9c751e4ce3252afa0136f61de2bfa6907a9f448
Jared Armstrong <jared.armstrong@gmail.com>
2012-09-15 16:30:44 +1200

    Search controller uses new search backend

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,12 @@
+require "will_paginate/array"
 class SearchController < ApplicationController
 
   def index
     @search = find_search_query
     if @search
-      @locations = Listing.search_by_location(@search)
-      if @locations.any?
-        @listings = Listing.where(:location_id => @locations.map(&:id)).includes(:photos).
-          paginate(:page => params[:page], :per_page => 20)
-      else
-        @listings = []
-      end
+      @listings = Listing.find_by_search_params(@search)
+      @listings = @listings.paginate(:page => params[:page], :per_page => 20)
+
       @query = @search[:pretty]
       SearchQuery.create(:query => @search[:query], :agent => request.env['HTTP_USER_AGENT'])
     end
@@ -30,7 +27,7 @@ class SearchController < ApplicationController
       if query && !lat && !lng
         extract_search_from_geocoding(query)
       elsif query && lat && lng
-        extract_search_from_params(query, lat, lng)
+        extract_search_from_params(query, lat.to_f, lng.to_f)
       end
     end
 
@@ -43,13 +40,20 @@ class SearchController < ApplicationController
       geometry = loc['geometry']
 
       search[:pretty] = loc['formatted_address']
-      search[:lat] = geometry['location']['lat']
-      search[:lng] = geometry['location']['lng']
+      search[:midpiont] = [geometry['location']['lat'], geometry['location']['lng']]
 
       bounds = geometry['bounds']
       if bounds && (loc['types'] == ["country","political"]) || (loc['types'] == ["administrative_area_level_1","political"])
-        search[:northeast] = { :lat => bounds['northeast']['lat'], :lng => bounds['northeast']['lng'] }
-        search[:southwest] = { :lat => bounds['southwest']['lat'], :lng => bounds['southwest']['lng'] }
+        search[:boundingbox] = {
+          :start => {
+            :lat => bounds['northeast']['lat'].to_f,
+            :lon => bounds['northeast']['lon'].to_f
+          },
+          :end => {
+            :lat => bounds['southwest']['lat'].to_f,
+            :lon => bounds['southwest']['lon'].to_f
+          }
+        }
       end
 
       search
@@ -57,14 +61,22 @@ class SearchController < ApplicationController
 
     def extract_search_from_params(query, lat, lng)
       search = { :query => query, :lat => lat, :lng => lng }
-      if params[:nx]
-        search[:northeast] = { :lat => params[:nx] }
-        search[:northeast][:lng] = params[:ny] if params[:ny]
+
+      search[:midpoint] = [lat, lng]
+
+      if params[:nx] && params[:ny] && params[:sx] && params[:sy]
+        search[:boundingbox] = {
+          :start => {
+            :lat => params[:nx].to_f,
+            :lon => params[:ny].to_f
+          },
+          :end => {
+            :lat => params[:sx].to_f,
+            :lon => params[:xy].to_f
+          }
+        }
       end
-      if params[:sx]
-        search[:southwest] = { :lat => params[:sx] }
-        search[:southwest][:lng] = params[:sy] if params[:sy]
-      end
+
       search
     end
 
