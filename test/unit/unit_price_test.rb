dbc6b22a540768e0186e094626b0de9acbdaa104
Zee Spencer <zee@zeespencer.com>
2012-10-25 18:32:52 -0400

    Validate unit price uniqueness for period, price and listing

--- test/unit/unit_price_test.rb
+++ test/unit/unit_price_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class UnitPriceTest < ActiveSupport::TestCase
   should belong_to(:listing)
   should validate_numericality_of(:price_cents)
+  should validate_uniqueness_of(:period).scoped_to([:price_cents, :listing_id])
+
+
+
   test "it delegates to the listings currency" do
     listing = FactoryGirl.create(:listing)
     listing.stubs(:currency).returns(Money::Currency.find('EUR'))
