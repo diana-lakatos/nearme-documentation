a2b004c02f9f0502562e32938bd60ed7b1081501
Jared Armstrong <jared.armstrong@gmail.com>
2012-11-07 11:15:03 +1300

    Add example usage

--- app/mailers/special_mailer.rb
+++ app/mailers/special_mailer.rb
@@ -3,6 +3,16 @@ class SpecialMailer < DesksNearMeMailer
 
   # Notify a user that OpenID login has been removed, and advise them to log in that
   # they may need to reset their password if they haven't set one on their account.
+  #
+  # Usage:
+  #
+  #   User.joins(:authentications).where(:authentications => { :provider => 'open_id' }).uniq.find_each do |user|
+  #     begin
+  #       SpecialMailer.openid_support_discontinued(user).deliver
+  #     rescue
+  #       puts "Error trying to deliver to User##{user.id}: #{$!.inspect}"
+  #     end
+  #   end
   def openid_support_discontinued(user)
     @user = user
 
