04f2fd97d6cd7693bdd2866e4052f575053f4818
Zee Spencer <zee@zeespencer.com>
2012-11-20 18:01:17 -0600

    Geolocate if no search query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,6 +6,7 @@ class SearchController < ApplicationController
 
     @listings = Listing.find_by_search_params(@search).reject { |l| l.location.nil? } # tmp hax
     @listings = @listings.paginate(:page => params[:page], :per_page => 20)
+    @query = @search.location_string
 
     SearchQuery.create(:query => @search.location_string, :agent => request.env['HTTP_USER_AGENT'])
 
