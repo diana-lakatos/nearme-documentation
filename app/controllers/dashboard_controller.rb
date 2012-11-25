929056bc72396c8ba6831e3df1df66208cc1aa41
Jared Armstrong <jared.armstrong@gmail.com>
2012-11-26 12:00:35 +1300

    WIP implementing new dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,22 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
-  def index
-    @listings = current_user.listings.all
+  def show
+  end
+
+  # Legacy dashboard page for managing bookings (user)
+  def bookings
     @your_reservations = current_user.reservations.visible
+  end
+
+  # Legacy dashboard page for managing reservations (owner)
+  def reservations
     @listing_reservations = current_user.listing_reservations.upcoming
   end
+
+  # Legacy dashboard page for managing listings
+  def listings
+    @listings = current_user.listings.all
+  end
+
 end
