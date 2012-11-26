2062e60162a238c987e8e385ec7d7791b186baa0
Jared Armstrong <jared.armstrong@gmail.com>
2012-11-26 15:51:18 +1300

    Tweaks. Add user welcome to header

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,13 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def show
+    if current_user.reservations.visible.any?
+      redirect_to bookings_dashboard_url
+    elsif current_user.listing_reservations.upcoming.any?
+      redirect_to reservations_dashboard_url
+    else
+      redirect_to edit_user_registration_url
+    end
   end
 
   # Legacy dashboard page for managing bookings (user)
