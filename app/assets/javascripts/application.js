d7074205a83f6491caef2cf226e063c4869942e9
Sai Perchard <sai@perchard.com>
2012-12-18 17:21:54 -0800

    Add asterisks to space location and booking email fields on space page of add a space flow. Remove professional associations fields from account page (need to rethink approach to professional associations / industries). Style quantity select element on booking section of space page. Use 'default' Google Maps pin.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 //= require ./vendor/jquery.overlay
 //= require ./vendor/jquery.address-1.3.min
 //= require ./vendor/jquery.ui.touch-punch
+//= require ./vendor/customSelect.jquery
 //= require ./vendor/bootstrap
 //= require ./search
 //= require ./vendor/rails
