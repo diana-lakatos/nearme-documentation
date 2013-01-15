a9f440b13bb5377d21ad485e4c09b648dce3952c
Jared Armstrong <jared.armstrong@gmail.com>
2013-01-16 10:53:12 +1300

    Add billing unit tests

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,7 @@
 Stripe.api_key, STRIPE_PUBLIC_KEY = if Rails.env.production?
   ["sk_live_YJet2CBSWgQ2UeuvQiG0vKEC", "pk_live_h3zjCFhi02B4c9juuzmFOe3n"]
-else
+elsif Rails.env.development? || Rails.env.staging?
   ["sk_test_lpr4WQXQdncpXjjX6IJx01W7", "pk_test_iCGA8nFZdILrI1UtuMOZD2aq"]
+else
+  # Don't setup stripe keys in Test environment
 end
