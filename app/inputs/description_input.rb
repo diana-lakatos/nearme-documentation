0cf1401a3c1d9c3ca3112aec64b605f2100ca107
Zee Spencer <zee@zeespencer.com>
2013-01-16 09:00:41 -0800

    Switch to text area

--- app/inputs/description_input.rb
+++ app/inputs/description_input.rb
@@ -3,7 +3,7 @@ class DescriptionInput < SimpleForm::Inputs::Base
   def input
     limit = 250
     hint = "You will get better results with a more concise description. Descriptions are limited to #{limit} characters."
-    out  = "#{@builder.text_field(attribute_name, {:maxlength => limit})}".html_safe
+    out  = "#{@builder.text_area(attribute_name, {:maxlength => limit, :rows => 2})}".html_safe
     out << content_tag(:p, hint, :class => 'help-block')
   end
 end
