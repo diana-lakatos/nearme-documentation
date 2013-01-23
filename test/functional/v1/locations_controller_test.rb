1f7df40dcdfa6c802465a8675addc704a9593796
olivier <olivier@alpha2real.com>
2013-01-22 22:30:27 -0800

    add test for api controller

---
+++ test/functional/v1/locations_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class V1::LocationsControllerTest < ActionController::TestCase
+
+  ##
+  # List
+
+  test "list should return a list of location" do
+    authenticate!
+    company = FactoryGirl.create(:company, :name => 'company_XYZ', :creator_id => @user.id)
+    @location = FactoryGirl.create(:location, :company_id => company.id)
+    get :list
+    assert_response :success
+  end
+ end
