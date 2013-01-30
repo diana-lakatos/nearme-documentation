4512b0016e8aa4708f41bef25d137c2b452021aa
Jared Armstrong <jared.armstrong@gmail.com>
2013-01-30 17:47:47 +1300

    Hotfix for iOS search issues

--- app/models/listing/search/params/api.rb
+++ app/models/listing/search/params/api.rb
@@ -8,4 +8,9 @@ class Listing::Search::Params::Api < Listing::Search::Params
     options.has_key?(:query) || options.has_key?(:boundingbox) || options.has_key?(:location)
   end
 
+  # location[:lat], location[:lon] are actually the location of the user, not a 'search for this midpoint'
+  # The API doesn't actually provide a midpoint?
+  def provided_midpoint
+    [nil, nil]
+  end
 end
