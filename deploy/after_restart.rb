24596de6f2ee914cf6955366a9c8cee3cd4f65ea
Alex Eckermann <alex@xenondesign.com.au>
2013-02-02 13:16:02 +1030

    Adding after_restart hook for notifying New Relic of a deployment

---
+++ deploy/after_restart.rb
@@ -0,0 +1,2 @@
+description = (`cd #{current_path} && git log -1 --format="%s"`).chomp
+run "cd #{current_path} && bundle exec newrelic deployments --user='#{deployed_by}' --revision='#{revision}' '#{description}'"
