7ccc61c4d80f4e57967a7bab65b59d2f2c195e39
Alex Eckermann <alex@xenondesign.com.au>
2013-02-02 13:27:23 +1030

    Only allow the application master server to send commands to New Relic after_restart

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,2 +1,4 @@
-description = (`cd #{current_path} && git log -1 --format="%s"`).chomp
-run "cd #{current_path} && bundle exec newrelic deployments --user='#{deployed_by}' --revision='#{revision}' '#{description}'"
+on_app_master do
+  description = (`cd #{current_path} && git log -1 --format="%s"`).chomp
+  run "cd #{current_path} && bundle exec newrelic deployments --user='#{deployed_by}' --revision='#{revision}' '#{description}'"
+end
