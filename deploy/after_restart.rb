8b1637090263881f0070dbab8397b881036f6612
Alex Eckermann <alex@xenondesign.com.au>
2013-02-02 17:57:20 +1030

    Restart delayed_job workers after_restart on deploy

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,8 @@
+# New Relic deployment notification
 on_app_master do
   description = (`cd #{current_path} && git log -1 --format="%s"`).chomp
   run "cd #{current_path} && bundle exec newrelic deployments --user='#{deployed_by}' --revision='#{revision}' '#{description}'"
 end
+
+# Must restart utilities that require the environment
+sudo "monit -g dj_#{app} restart all"
