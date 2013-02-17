1764fa231d089161ea5f75f919af730b5e5c2718
Maciej Krajowski-Kukiel <mkk@quantads.net>
2013-02-17 20:27:17 +0100

    alt email content after signup if user has listing
    [finishes#44560997]
    
    removed unnecessary debug

--- app/mailers/after_signup_mailer.rb
+++ app/mailers/after_signup_mailer.rb
@@ -3,9 +3,22 @@ class AfterSignupMailer < DesksNearMeMailer
   def help_offer(user)
 
     @user = user
+    define_content
     mail to:      user.email,
-         from: "michelle@desksnear.me",
-         subject: "Welcome to DesksNear.me"
+      from: "michelle@desksnear.me",
+      subject: "Welcome to DesksNear.me"
+  end
+
+
+  private
+
+  def define_content
+
+    if @user.listings.count > 0
+      @content = "I notice you added a new listing, and wanted to reach out and see if you need any further help!"
+    else
+      @content = "I wanted to reach out and see if you need any help listing or booking a space." 
+    end
   end
 
 end
--- test/unit/mailers/after_signup_mailer_test.rb
+++ test/unit/mailers/after_signup_mailer_test.rb
@@ -14,4 +14,16 @@ class AfterSignupMailerTest < ActiveSupport::TestCase
     assert mail.html_part.body.include?("I'm Michelle, co-founder of Desks Near Me.")
   end
 
+  test "offer help with listing" do
+    mail = AfterSignupMailer.help_offer(@user)
+    assert mail.html_part.body.include?("if you need any help listing or booking a space")
+  end
+
+  test "offer help with anything if listing created" do
+    @listing = FactoryGirl.create(:listing, :creator => @user)
+    
+    mail = AfterSignupMailer.help_offer(@user)
+    assert mail.html_part.body.include?("I notice you added a new listing")
+  end
+
 end
