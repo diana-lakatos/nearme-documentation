521400804d17359cf794258781ab8642c3cb4651
Zee Spencer <zee@zeespencer.com>
2013-02-28 13:22:17 -0500

    Remove city from share location tweet
    [finishes#45334023]

--- app/helpers/sharing_helper.rb
+++ app/helpers/sharing_helper.rb
@@ -1,6 +1,6 @@
 module SharingHelper
   def tweet_location_url(location)
-    tweet_body = "If you're in #{location.city} think about working out of our place! #{location_url(location)}"
+    tweet_body = "Need a place to work? Check out our office on @desksnearme: #{location_url(location)}"
     "https://twitter.com/intent/tweet?text=#{url_encode(tweet_body)}"
   end
 end
