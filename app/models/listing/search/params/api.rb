aee94d76b5f1a640f02ad42e7f9b7a698e063069
Alex Eckermann <alex@xenondesign.com.au>
2013-03-15 11:19:28 -0700

    Remove the location object from the options hash for API search params.
    [delivers#46292257]
    
    * The iOS app provides this as the users actual location rather than a search query.
    
    https://www.pivotaltracker.com/story/show/46292257

--- app/models/listing/search/params/api.rb
+++ app/models/listing/search/params/api.rb
@@ -7,5 +7,12 @@ class Listing::Search::Params::Api < Listing::Search::Params
   def valid_search_method?
     options.has_key?(:query) || options.has_key?(:boundingbox) || options.has_key?(:location)
   end
-
+  
+  # The :location object provided is for the users actual location rather than the one
+  # they are searching for.
+  def midpoint
+    options.delete(:location)
+    super
+  end
+  
 end
