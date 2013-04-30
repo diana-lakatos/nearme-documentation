5a9aca7a7a248fc2323cf5368e280cc2efd31a24
Jared Armstrong <jared.armstrong@gmail.com>
2013-04-30 15:02:33 +1200

    Remove unused old availability template and helper method

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -16,30 +16,6 @@ module ReservationsHelper
     @reservation.total_amount > 0 && %w(USD CAD).include?(@reservation.currency)
   end
 
-  def reservation_schedule_for(listing, weeks = 1, &block)
-    new_row = false
-
-    listing.schedule(weeks).to_a.in_groups_of(5).each do |group|
-      group.each do |date, num_of_desks|
-        availability = case num_of_desks
-          when 0
-            "booked"
-          when 1, 2, 3
-            "last_reservations"
-          else
-            "available"
-        end
-
-        availability = "unavailable" if date.past?
-
-        yield(date, num_of_desks, availability, new_row)
-        new_row = false
-      end
-
-      new_row = true
-    end
-  end
-
   def reservation_total_price(reservation)
     if reservation.total_amount_cents.nil?
       "Free!"
