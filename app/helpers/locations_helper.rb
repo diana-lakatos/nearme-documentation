7a8eb1a80616b59586749d7d361e8d3fa8210621
Jared Armstrong <jared.armstrong@gmail.com>
2013-05-07 10:24:31 +1200

    Tweak w-hotels page organisation. Implement bookings module on w-hotels

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -12,22 +12,7 @@ module LocationsHelper
 
   def location_listings_json(location = @location)
     location.listings.map { |listing|
-      availability = listing.availability_status_between(Date.today, Date.today.advance(:years => 1))
-      {
-        :id => listing.id,
-        :name => listing.name,
-        :review_url => review_listing_reservations_url(listing),
-        :first_available_date => listing.first_available_date.strftime("%Y-%m-%d"),
-        :minimum_booking_days => listing.minimum_booking_days,
-        :quantity => listing.quantity,
-        :availability => availability.as_json,
-        :minimum_date => availability.start_date,
-        :maximum_date => availability.end_date,
-        :prices_by_days => Hash[
-          listing.prices_by_days.map { |k, v| [k, v.cents] }
-        ],
-        :initial_bookings => @initial_bookings[listing.id]
-      }
+      listing_bookings_data(listing)
     }.to_json
   end
 
