63ade5dea153a8f61e8f5abd6145a8c8e3d50c7c
Maciej Krajowski-Kukiel <maciej.krajowski@gmail.com>
2013-05-09 18:49:05 +0200

    adds sql query to mgration to populate db

--- db/migrate/20130503133439_add_mailchimp_columns_to_user.rb
+++ db/migrate/20130503133439_add_mailchimp_columns_to_user.rb
@@ -1,6 +1,19 @@
 class AddMailchimpColumnsToUser < ActiveRecord::Migration
-  def change
+  def up
     add_column :users, :mailchimp_synchronized_at, :timestamp
     add_column :users, :verified, :boolean, :default => false
+
+    # script has been run in different environment, so we need to populate new column
+    # for all users that were affected
+    connection.execute <<-SQL
+      UPDATE users 
+      SET mailchimp_synchronized_at = '2013-05-08 12:38:25.099763'
+      WHERE id <= 2575
+    SQL
+  end
+
+  def down
+    remove_column :users, :mailchimp_synchronized_at
+    remove_column :users, :verified
   end
 end
