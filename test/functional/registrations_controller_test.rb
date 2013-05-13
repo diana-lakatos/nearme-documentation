9f2d52fb2fc625b5853023c15fb74b5ab1d04b68
Maciej Krajowski-Kukiel <maciej.krajowski@gmail.com>
2013-05-13 16:54:07 +0200

    redirect user to dashboard after successful verification

--- test/functional/registrations_controller_test.rb
+++ test/functional/registrations_controller_test.rb
@@ -14,7 +14,9 @@ class RegistrationsControllerTest < ActionController::TestCase
     should "verify user if token and id are correct" do
       get :verify, :id => @user.id, :token => @user.email_verification_token
       @user.reload
+      @controller.current_user.id == @user.id
       assert @user.verified
+      assert_redirected_to manage_locations_path
     end
 
     should "handle situation when user is verified" do
@@ -22,19 +24,24 @@ class RegistrationsControllerTest < ActionController::TestCase
       @user.save!
       get :verify, :id => @user.id, :token => @user.email_verification_token
       @user.reload
+      @controller.current_user.id == @user.id
       assert @user.verified
+      assert_redirected_to manage_locations_path
     end
 
     should "not verify user if id is incorrect" do
       get :verify, :id => (@user.id+1), :token => @user.email_verification_token
       @user.reload
+      assert_nil @controller.current_user
       assert !@user.verified
     end
 
     should "not verify user if token is incorrect" do
       get :verify, :id => @user.id, :token => @user.email_verification_token+"incorrect"
       @user.reload
+      assert_nil @controller.current_user
       assert !@user.verified
+      assert_redirected_to root_path
     end
 
   end
