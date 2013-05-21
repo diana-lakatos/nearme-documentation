22c841db92db8409f2cb36f4637b6073904fb838
Jared Armstrong <jared.armstrong@gmail.com>
2013-05-21 12:01:01 +1200

    render w-hotels page outside container for full width carousel

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,2 +1,9 @@
 module LayoutHelper
+  def render_content_outside_container?
+    @render_content_outside_container
+  end
+
+  def render_content_outside_container!
+    @render_content_outside_container = true
+  end
 end
