c98d538f4d5b721695715237fbfe8379c955611d
Harold Herbert <haroldherbert@me.com>
2013-05-23 13:12:46 -0700

    fixed pricing label alignment

--- db/schema.rb
+++ db/schema.rb
@@ -101,6 +101,19 @@ ActiveRecord::Schema.define(:version => 20130510001643) do
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
+  create_table "guest_ratings", :force => true do |t|
+    t.integer  "reservation_id"
+    t.integer  "user_id"
+    t.float    "value"
+    t.text     "comment"
+    t.datetime "deleted_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  add_index "guest_ratings", ["reservation_id", "value"], :name => "index_guest_ratings_on_reservation_id_and_value"
+  add_index "guest_ratings", ["user_id", "value"], :name => "index_guest_ratings_on_user_id_and_value"
+
   create_table "industries", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -140,6 +153,7 @@ ActiveRecord::Schema.define(:version => 20130510001643) do
     t.integer  "monthly_price_cents"
     t.boolean  "hourly_reservations",     :default => false, :null => false
     t.integer  "hourly_price_cents"
+    t.integer  "minimum_booking_minutes"
   end
 
   create_table "location_amenities", :force => true do |t|
@@ -178,7 +192,6 @@ ActiveRecord::Schema.define(:version => 20130510001643) do
     t.string   "country"
     t.string   "slug"
     t.integer  "location_type_id"
-    t.string   "name"
     t.string   "custom_page"
   end
 
@@ -239,8 +252,8 @@ ActiveRecord::Schema.define(:version => 20130510001643) do
     t.datetime "deleted_at"
     t.text     "comment"
     t.boolean  "create_charge"
-    t.string   "payment_status",     :default => "unknown", :null => false
     t.string   "payment_method",     :default => "manual",  :null => false
+    t.string   "payment_status",     :default => "unknown", :null => false
     t.integer  "quantity",           :default => 1,         :null => false
   end
 
@@ -324,5 +337,18 @@ ActiveRecord::Schema.define(:version => 20130510001643) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "visit_ratings", :force => true do |t|
+    t.integer  "reservation_id"
+    t.integer  "user_id"
+    t.float    "value"
+    t.text     "comment"
+    t.datetime "deleted_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  add_index "visit_ratings", ["reservation_id", "value"], :name => "index_visit_ratings_on_reservation_id_and_value"
+  add_index "visit_ratings", ["user_id", "value"], :name => "index_visit_ratings_on_user_id_and_value"
 
 end
