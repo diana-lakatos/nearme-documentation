0a35d7130130f59f56c2090c28ce278938f35072
Jared Armstrong <jared.armstrong@gmail.com>
2013-06-13 14:37:15 +1200

    revert to staging mixpanel account

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,8 +1,6 @@
 MIXPANEL_TOKEN = if Rails.env.production?
   '7ffff7057407af5fe5f71701a1ab26b2'
 else
-  'cc36cc3f9b51f9f476604fa5dd52f76d'
+  '897e1963583f7dde9ecac901683a9bfa'
 end
-
-# DesksnearMe::Application.config.middleware.use "Mixpanel::Middleware", MIXPANEL_TOKEN, { persist: true }
 
