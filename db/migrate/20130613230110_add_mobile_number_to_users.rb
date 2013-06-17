eb37c5755fc376a4f64a31725fe54619f0371fca
Jared Armstrong <jared.armstrong@gmail.com>
2013-06-17 15:43:16 +1200

    Fix modified migration

--- db/migrate/20130613230110_add_mobile_number_to_users.rb
+++ db/migrate/20130613230110_add_mobile_number_to_users.rb
@@ -5,7 +5,7 @@ class AddMobileNumberToUsers < ActiveRecord::Migration
   end
 
   def down
-    remove_column :users, :mobile_country_code
+    remove_column :users, :country_name
     remove_column :users, :mobile_number
   end
 end
