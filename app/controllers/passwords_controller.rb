ddf1ce130ff91b412abd5f650ed52f358bb06eb5
yratanov <organium@gmail.com>
2013-06-19 16:40:56 +0800

    redirect to home and flash after password recovery attempt * https://trello.com/c/Ryv8nlw7

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,2 +1,7 @@
 class PasswordsController < Devise::PasswordsController
+
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    root_path
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -88,4 +88,12 @@ module ApplicationHelper
       link_to(options, html_options)
     end
   end
+
+  def class_for_flash_type(type)
+    if type == :notice
+      'success'
+    else
+      type
+    end
+  end
 end
