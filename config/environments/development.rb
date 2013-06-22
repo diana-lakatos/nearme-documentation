cf521295f544e13cbbd3d30330053236819b53ea
Manimaran Malaichamy <manim@kyyba.com>
2013-06-22 16:11:05 +0530

    Group dates in email notifications in the same way as on the reservation modal
    * list the dates for both hourly listing as well as week/day listing
    * Align the dates to the right
    
    https://trello.com/c/7a4LzZzf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,19 +7,9 @@ DesksnearMe::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => '587',
-    :authentication => :plain,
-    :enable_starttls_auto => true,
-    :user_name      => "mani@maniempire.com",
-    :password       => "thevanyesu",
-    :domain         => 'maniempire.com'
-  }
-
+ 
   config.active_support.deprecation = :log
 
   config.action_dispatch.best_standards_support = :builtin
