20f4723322d36a8a9f24f453fbe3e7f9d7d3ed5c
Jared Armstrong <jared.armstrong@gmail.com>
2013-06-26 13:09:52 +1200

    Snapshot db schema prior to 2012.
    * Older migrations followed some bad practice and were
      causing issues for bootstrapping new environments.
    
    * This commit removes those migrations and replaces
      them with a schema snapshot as an initial migration.
    
    https://trello.com/c/Hri5ZJHp

--- db/migrate/20101017205855_add_fake_flag_to_workplaces.rb
+++
@@ -1,9 +0,0 @@
-class AddFakeFlagToWorkplaces < ActiveRecord::Migration
-  def self.up
-    add_column :workplaces, :fake, :boolean
-  end
-
-  def self.down
-    remove_column :workplaces, :fake
-  end
-end
