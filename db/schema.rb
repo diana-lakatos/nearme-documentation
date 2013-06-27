49497492f46bf173c325099be8cb0220f46efa5f
Rick Chua <rchua423@gmail.com>
2013-06-27 09:50:20 +0800

    To retain the query from home page in the search input
    https://trello.com/c/IWXXn2lP

--- db/schema.rb
+++ db/schema.rb
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(:version => 20130619141440) do
     t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.datetime "deleted_at"
     t.string   "secret"
     t.string   "token"
@@ -182,8 +182,8 @@ ActiveRecord::Schema.define(:version => 20130619141440) do
   add_index "locations", ["slug"], :name => "index_locations_on_slug"
 
   create_table "photos", :force => true do |t|
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "content_id"
     t.string   "image"
     t.string   "caption"
@@ -234,8 +234,8 @@ ActiveRecord::Schema.define(:version => 20130619141440) do
   create_table "search_queries", :force => true do |t|
     t.string   "query"
     t.text     "agent"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "sessions", :force => true do |t|
@@ -274,26 +274,28 @@ ActiveRecord::Schema.define(:version => 20130619141440) do
   add_index "user_relationships", ["follower_id"], :name => "index_user_relationships_on_follower_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                     :default => "",    :null => false
-    t.string   "encrypted_password",        :default => "",    :null => false
+    t.string   "email",                                    :default => "",    :null => false
+    t.string   "encrypted_password",        :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                            :default => "",    :null => false
     t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",             :default => 0
+    t.integer  "sign_in_count",                            :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
     t.boolean  "admin"
-    t.integer  "bookings_count",            :default => 0,     :null => false
+    t.integer  "bookings_count",                           :default => 0,     :null => false
     t.datetime "confirmation_sent_at"
     t.datetime "confirmed_at"
     t.datetime "deleted_at"
     t.datetime "locked_at"
-    t.integer  "failed_attempts",           :default => 0
+    t.datetime "reset_password_sent_at"
+    t.integer  "failed_attempts",                          :default => 0
     t.string   "authentication_token"
     t.string   "avatar"
     t.string   "confirmation_token"
@@ -304,7 +306,7 @@ ActiveRecord::Schema.define(:version => 20130619141440) do
     t.string   "job_title"
     t.text     "biography"
     t.datetime "mailchimp_synchronized_at"
-    t.boolean  "verified",                  :default => false
+    t.boolean  "verified",                                 :default => false
     t.string   "country_name"
     t.string   "mobile_number"
   end
