9434ba6555e48e4d948d578080071bde96736600
Manimaran Malaichamy <manim@kyyba.com>
2013-06-29 00:03:00 +0530

    add mock objects for mail listing *update mail listing UI for hourly listing

---
+++ test/unit/mailers/reservation_mailer_test.rb
@@ -0,0 +1,70 @@
+require 'test_helper'
+
+class ReservationMailerTest < ActiveSupport::TestCase
+
+  setup do
+    @reservation = FactoryGirl.create(:reservation)
+  end
+
+  test "notify guest of cancellation" do
+    mail = ReservationMailer.notify_guest_of_cancellation(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify guest of confirmation" do
+    mail = ReservationMailer.notify_guest_of_confirmation(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify guest of expiration" do
+    mail = ReservationMailer.notify_guest_of_expiration(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify guest of rejection" do
+    mail = ReservationMailer.notify_guest_of_rejection(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify guest with confirmation" do
+    mail = ReservationMailer.notify_guest_with_confirmation(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify host of cancellation" do
+    mail = ReservationMailer.notify_host_of_cancellation(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify host of confirmation" do
+    mail = ReservationMailer.notify_host_of_confirmation(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify host of expiration" do
+    mail = ReservationMailer.notify_host_of_expiration(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify host with confirmation" do
+    mail = ReservationMailer.notify_host_with_confirmation(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+  test "notify host without confirmation" do
+    mail = ReservationMailer.notify_host_without_confirmation(@reservation)
+
+    assert mail.html_part.body.include?(@reservation.listing.creator.name)
+  end
+
+
+end
