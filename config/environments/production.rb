2a77bcd104eaec5781ae383fe79c618661fdf258
pietia <pietia@appgrinder.pl>
2013-07-02 11:29:32 +0200

    Revert "Turn off minification temporarily."
    This reverts commit 495fa08270e26ad3cd2e0537826ab256c473f3be.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,6 @@ DesksnearMe::Application.configure do
   config.stripe_api_key = "sk_live_YJet2CBSWgQ2UeuvQiG0vKEC"
   config.stripe_public_key = "pk_live_h3zjCFhi02B4c9juuzmFOe3n"
 
-  # Minification was introducing IE bugs - turn off for now
-  #config.assets.compress = true
-  #config.assets.js_compressor = :uglifier
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
 end
