449f9a181ddbbdfadc02b7f2d508a76017f8a396
Jared Armstrong <jared.armstrong@gmail.com>
2013-07-13 13:32:52 +1200

    Cleanup

--- app/helpers/admin/resource_helper.rb
+++ app/helpers/admin/resource_helper.rb
@@ -4,41 +4,20 @@ module Admin::ResourceHelper
   end
 
   def admin_resource_name(resource)
-    if @resource_name_proc
-      @resource_name_proc[resource]
-    elsif resource.respond_to?(:name)
-      resource.name
-    elsif resource.respond_to?(:title)
-      resource.title
-    else
-      "#{resource.class.name}##{resource.id}"
+    [:name, :title].each do |method|
+      return resource.send(method) if resource.respond_to?(method)
     end
+
+    "#{resource.class.name}##{resource.id}"
   end
 
   def admin_resource_form_attributes(resource)
-    attrs = @form_attributes || resource.attributes.keys.map(&:to_s)
+    attrs = resource.attributes.keys.map(&:to_s)
     attrs -= %w(id created_at updated_at deleted_at)
-    attrs -= (@form_attributes_skip || [])
     attrs
   end
 
   def admin_resource_field_options(resource, field)
-    # Options defined in controller for field
-    options = (@form_attribute_options || {})[field]
-    options ||= {}
-
-    case field
-    when /country/
-      options[:as] = :select
-      options[:collection] = Country.all
-    end
-
-    case resource.class.columns_hash[field].type
-    # Standard options based on field type
-    when :boolean
-      #options[:wrapper] = :inline_checkbox
-    end
-
-    options
+    {}
   end
 end
