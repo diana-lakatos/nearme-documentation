139353f5c2d0d30b904976f10e80850aaa50ee31
Maciej Krajowski-Kukiel <maciej.krajowski@gmail.com>
2013-07-18 12:31:26 +0200

    convert time to utc in test

--- test/functional/listings/reservations_controller_test.rb
+++ test/functional/listings/reservations_controller_test.rb
@@ -40,10 +40,10 @@ class Listings::ReservationsControllerTest < ActionController::TestCase
       @listing = FactoryGirl.create(:listing, :name => 'ICS Listing')
       @reservation = FactoryGirl.build(:reservation_with_credit_card, :listing => @listing)
       @reservation.periods = []
-      Timecop.freeze(Time.local(2013, 6, 28, 10, 5, 0))
-      @reservation.add_period(DateTime.now.next_week.next_day(1))
-      @reservation.add_period(DateTime.now.next_week.next_day(2))
-      @reservation.add_period(DateTime.now.next_week.next_day(3))
+      Timecop.freeze(Time.zone.local(2013, 6, 28, 10, 5, 0).utc)
+      @reservation.add_period(Time.zone.local(2013, 7, 1, 10, 5, 0).utc)
+      @reservation.add_period(Time.zone.local(2013, 7, 2, 10, 5, 0).utc)
+      @reservation.add_period(Time.zone.local(2013, 7, 3, 10, 5, 0).utc)
       @reservation.save!
       sign_in @reservation.owner
       Rails.application.routes.url_helpers.stubs(:listing_reservation_url).returns("http://example.com/listings/1/reservations/1/export.ics")
@@ -53,17 +53,20 @@ class Listings::ReservationsControllerTest < ActionController::TestCase
       get :export, :format => :ics, :listing_id => @reservation.listing.id, :id => @reservation.id
       assert_response :success
       assert_equal "text/calendar", response.content_type
-      assert_equal [ "BEGIN:VCALENDAR", "VERSION:2.0", "CALSCALE:GREGORIAN", "METHOD:PUBLISH", "PRODID:iCalendar-Ruby",
-        "BEGIN:VEVENT", "CREATED:080500", "DESCRIPTION:42 Wallaby Way - ICS Listing", "DTEND:20130701T170000", "DTSTAMP:20130628T100500",
-        "DTSTART:20130701T090000", "CLASS:PUBLIC", "LAST-MODIFIED:080500", "LOCATION:42 Wallaby Way", "SEQUENCE:0", "SUMMARY:ICS Listing",
-        "UID:http://example.com/listings/1/reservations/1/export.ics", "URL:http://example.com/listings/1/reservations/1/export.ics", "END:VEVENT",
-        "BEGIN:VEVENT", "CREATED:080500", "DESCRIPTION:42 Wallaby Way - ICS Listing", "DTEND:20130702T170000", "DTSTAMP:20130628T100500",
-        "DTSTART:20130702T090000", "CLASS:PUBLIC", "LAST-MODIFIED:080500", "LOCATION:42 Wallaby Way", "SEQUENCE:0", "SUMMARY:ICS Listing",
-        "UID:http://example.com/listings/1/reservations/1/export.ics", "URL:http://example.com/listings/1/reservations/1/export.ics", "END:VEVENT",
-        "BEGIN:VEVENT", "CREATED:080500", "DESCRIPTION:42 Wallaby Way - ICS Listing", "DTEND:20130703T170000", "DTSTAMP:20130628T100500",
-        "DTSTART:20130703T090000", "CLASS:PUBLIC", "LAST-MODIFIED:080500", "LOCATION:42 Wallaby Way",
-        "SEQUENCE:0", "SUMMARY:ICS Listing", "UID:http://example.com/listings/1/reservations/1/export.ics", "URL:http://example.com/listings/1/reservations/1/export.ics",
-        "END:VEVENT", "END:VCALENDAR"], response.body.split("\r\n")
+      expected_result = ["BEGIN:VCALENDAR", "VERSION:2.0", "CALSCALE:GREGORIAN", "METHOD:PUBLISH", "PRODID:iCalendar-Ruby",
+        "BEGIN:VEVENT", "CREATED:100500", "DESCRIPTION:42 Wallaby Way - ICS Listing", "DTEND:20130701T170000",
+        "DTSTAMP:20130628T120500", "DTSTART:20130701T090000", "CLASS:PUBLIC", "LAST-MODIFIED:100500",
+        "LOCATION:42 Wallaby Way", "SEQUENCE:0", "SUMMARY:ICS Listing", "UID:http://example.com/listings/1/reservations/1/export.ics",
+        "URL:http://example.com/listings/1/reservations/1/export.ics", "END:VEVENT", "BEGIN:VEVENT", "CREATED:100500",
+        "DESCRIPTION:42 Wallaby Way - ICS Listing", "DTEND:20130702T170000", "DTSTAMP:20130628T120500", "DTSTART:20130702T090000",
+        "CLASS:PUBLIC", "LAST-MODIFIED:100500", "LOCATION:42 Wallaby Way", "SEQUENCE:0", "SUMMARY:ICS Listing",
+        "UID:http://example.com/listings/1/reservations/1/export.ics", "URL:http://example.com/listings/1/reservations/1/export.ics",
+        "END:VEVENT", "BEGIN:VEVENT", "CREATED:100500", "DESCRIPTION:42 Wallaby Way - ICS Listing", "DTEND:20130703T170000",
+        "DTSTAMP:20130628T120500", "DTSTART:20130703T090000", "CLASS:PUBLIC", "LAST-MODIFIED:100500", "LOCATION:42 Wallaby Way",
+        "SEQUENCE:0", "SUMMARY:ICS Listing", "UID:http://example.com/listings/1/reservations/1/export.ics", 
+        "URL:http://example.com/listings/1/reservations/1/export.ics",
+        "END:VEVENT", "END:VCALENDAR"]
+      assert_equal expected_result, response.body.split("\r\n")
     end
 
     teardown do
@@ -75,9 +78,9 @@ class Listings::ReservationsControllerTest < ActionController::TestCase
     {
       "listing_id" => @listing.id,
       "reservation" => {
-      "dates" => [Chronic.parse('Monday')],
-      "quantity"=>"1"
-    }
+        "dates" => [Chronic.parse('Monday')],
+        "quantity"=>"1"
+      }
     }
   end
 
