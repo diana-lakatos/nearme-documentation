c1c827a4339de6b437dbb9c3ad16b6e8b4013da7
Jared Armstrong <jared.armstrong@gmail.com>
2013-07-23 14:37:47 +1200

    Abstract admin collection search to Admin::ResourceController

--- app/controllers/admin/resource_controller.rb
+++ app/controllers/admin/resource_controller.rb
@@ -6,7 +6,21 @@ class Admin::ResourceController < Admin::BaseController
   protected
 
   def collection
-    end_of_association_chain.paginate(:page => params[:page])
+    scope = end_of_association_chain
+
+    if search = params[:search].presence
+      conditions = collection_search_fields.map { |f| "#{f} ILIKE :s" }.join ' OR '
+      escaped_search = ActiveRecord::Base.connection.quote_like_string(search)
+
+      scope = scope.where(conditions, :s => "#{escaped_search}%")
+    end
+
+    scope.paginate(:page => params[:page])
+  end
+
+  # Fields that we can search on
+  def collection_search_fields
+    %w(name)
   end
 end
 
