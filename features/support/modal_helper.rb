03e3935f769e5a13a134de6a47569441034af04c
pietia <pietia@appgrinder.pl>
2013-07-25 14:20:24 +0200

    this should prevent from infinite loops

--- features/support/modal_helper.rb
+++ features/support/modal_helper.rb
@@ -1,14 +1,20 @@
 module ModalHelper
-  def wait_modal_loaded(modal_selector)
-    begin
-      opacity = page.evaluate_script("$('#{modal_selector}').css('opacity');")
-    end while opacity != '1'
+  def wait_modal_loaded
+    page.has_selector?(".modal-overlay.visible")
+    page.has_selector?(".modal-content.visible") # necessary ?
+    sleep(0.25)
   end
 
-  def wait_modal_closed(modal_selector)
-    begin
-      opacity = page.evaluate_script("$('#{modal_selector}').css('opacity');")
-    end while opacity == '1'
+  def wait_modal_closed
+    page.has_no_selector?(".modal-content.visible")
+    page.has_no_selector?(".modal-overlay.visible") # necessary ?
+    sleep(0.25)
+  end
+
+  def work_in_modal
+    wait_modal_loaded
+    yield
+    wait_modal_closed
   end
 end
 
