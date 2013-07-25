4d869a9da1177f38027999c6c3061c3e72b1dde0
pietia <pietia@appgrinder.pl>
2013-07-25 15:47:53 +0200

    move outside of work_in_modal block

--- features/step_definitions/password_reset_steps.rb
+++ features/step_definitions/password_reset_steps.rb
@@ -5,8 +5,8 @@ When /^I begin to reset the password for that user$/ do
     page.should have_content('Fill in your email below')
     find('#user_email').set(user.email)
     click_on 'Reset Password'
-    page.should have_content('You will receive an email with instructions about how to reset your password in a few minutes.')
   end
+  page.should have_content('You will receive an email with instructions about how to reset your password in a few minutes.')
 end
 
 Then /^a password reset email should be sent to that user$/ do
