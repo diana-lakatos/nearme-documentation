f12287d51f8c086c0f360b53328db880fcc2527d
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2013-07-25 01:28:39 -0700

    remove debug statement

--- features/step_definitions/listing_steps.rb
+++ features/step_definitions/listing_steps.rb
@@ -85,7 +85,6 @@ end
 
 Then /^I (do not )?see a search result for the ([^\$].*) listing$/ do |negative, city|
   listing = instance_variable_get("@listing_in_#{city.downcase.gsub(' ', '_')}")
-  binding.pry
   if negative
     page.should have_no_selector('article[data-name="' + listing.name + '"]')
   else
