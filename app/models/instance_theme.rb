8cdabd9f35437aa8cf0ff56dd511c6afac94d9f1
Jared Armstrong <jared.armstrong@gmail.com>
2013-07-30 15:36:55 +1200

    Custom instance theme stylesheets
    * Crude instance theme precompilation strategy
    * Added Theme model with colors and images
    * Add simple Job base class to encapsulate background execution semantics
    
    https://trello.com/c/rRD7joCM
    https://trello.com/c/8oOUHfcR

---
+++ app/models/instance_theme.rb
@@ -0,0 +1,50 @@
+class InstanceTheme < ActiveRecord::Base
+  COLORS = %w(blue red orange green gray black white)
+  COLORS.each do |color|
+    attr_accessible "color_#{color}"
+  end
+
+  attr_accessible :name, :theme_icon_image, :theme_icon_retina_image,
+    :theme_logo_image, :theme_logo_retina_image, :theme_hero_image, :skip_compilation
+
+  # TODO: We may want the ability to have multiple themes, and draft states,
+  #       etc.
+  belongs_to :instance
+
+  mount_uploader :icon_image, InstanceThemeImageUploader
+  mount_uploader :icon_retina_image, InstanceThemeImageUploader
+  mount_uploader :logo_image, InstanceThemeImageUploader
+  mount_uploader :logo_retina_image, InstanceThemeImageUploader
+  mount_uploader :hero_image, InstanceThemeImageUploader
+  mount_uploader :compiled_stylesheet, InstanceThemeStylesheetUploader
+
+  # Precompile the theme, unless we're saving the compiled stylesheet.
+  after_save :recompile_theme, :if => :theme_changed?
+
+  # If true, will skip compiling the theme when saving
+  attr_accessor :skip_compilation
+
+  def recompile_theme
+    CompileInstanceThemeJob.perform(self) unless skip_compilation
+  end
+
+  # Checks if any of options that impact the theme stylesheet have been changed.
+  def theme_changed?
+    attrs = attributes.keys - %w(updated_at compiled_stylesheet name)
+    attrs.any? { |attr|
+      send("#{attr}_changed?")
+    }
+  end
+
+  def skipping_compilation(&blk)
+    begin
+      before, self.skip_compilation = self.skip_compilation, true
+      yield(self)
+    ensure
+      self.skip_compilation = before
+    end
+    self
+  end
+
+end
+
---
+++ app/uploaders/instance_theme_stylesheet_uploader.rb
@@ -0,0 +1,8 @@
+class InstanceThemeStylesheetUploader < BaseUploader
+  # TODO: Add propper caching headers to uploaded file
+
+  def extension_white_list
+    %w(css)
+  end
+end
+
