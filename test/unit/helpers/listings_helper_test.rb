23644ac7e0da091818db5675839d09bd081eeacc
Josef Šimánek <josef.simanek@gmail.com>
2013-07-31 20:58:38 +0200

    Fix typo in ListingsHelper test.

--- test/unit/helpers/listings_helper_test.rb
+++ test/unit/helpers/listings_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ListingsHelperTest < ActionView::TestCase
   include ListingsHelper
 
-  context '#spave_listing_placeholder_url' do
+  context '#space_listing_placeholder_url' do
     should "return valid placeholder" do
       expected_url = "http://placehold.it/100x200"
       assert_equal expected_url, space_listing_placeholder_url(width: 100, height: 200)
