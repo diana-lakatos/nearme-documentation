3d476c200d96f7207681deecd22bc67164af4403
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2013-07-31 06:50:05 -0700

    swap instance.bookable_noun for any references to listing nouns in the InquiryMailer

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,14 +1,16 @@
 class InquiryMailer < DesksNearMeMailer
 
-  def inquiring_user_notification(inquiry)
+  def inquiring_user_notification(instance, inquiry)
     @inquiry = inquiry
+    @instance = instance
 
     mail to:      inquiry.inquiring_user.full_email,
          subject: "We've passed on your inquiry about #{inquiry.listing.name}"
   end
 
-  def listing_creator_notification(inquiry)
+  def listing_creator_notification(instance, inquiry)
     @inquiry = inquiry
+    @instance = instance
 
     mail to:       inquiry.listing.creator.full_email,
          subject:  "New enquiry from #{inquiry.inquiring_user.name} about #{inquiry.listing.name}",
@@ -19,11 +21,11 @@ class InquiryMailer < DesksNearMeMailer
     class Preview < MailView
 
       def inquiring_user_notification
-        ::InquiryMailer.inquiring_user_notification(Inquiry.first)
+        ::InquiryMailer.inquiring_user_notification(@instance, Inquiry.first)
       end
 
       def listing_creator_notification
-        ::InquiryMailer.listing_creator_notification(Inquiry.first)
+        ::InquiryMailer.listing_creator_notification(@instance, Inquiry.first)
       end
 
     end
