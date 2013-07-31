ff9c7df6cd76239db92aa2395cd68e289fb65fa3
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2013-07-30 18:01:56 -0700

    swap instance.bookable_noun for "space" or "desk" in the AfterSignupMailer

--- app/mailers/after_signup_mailer.rb
+++ app/mailers/after_signup_mailer.rb
@@ -5,8 +5,9 @@ class AfterSignupMailer < DesksNearMeMailer
   layout false
 
 
-  def help_offer(user_id)
+  def help_offer(instance, user_id)
     @user = User.find(user_id)
+    @instance = instance
     @sent_by = 'Michelle R'
 
     mail to:      @user.email,
@@ -21,19 +22,19 @@ class AfterSignupMailer < DesksNearMeMailer
 
       def help_offer_with_listing
         @user = User.all.detect { |u| !u.listings.empty?  }
-        ::AfterSignupMailer.help_offer(@user)
+        ::AfterSignupMailer.help_offer(@instance, @user)
       end
 
       def help_offer_with_booking
         @user = User.all.detect { |u| !u.reservations.empty? && u.listings.empty? }
         raise "No user with booking and without listing" unless @user
-        ::AfterSignupMailer.help_offer(@user)
+        ::AfterSignupMailer.help_offer(@instance, @user)
       end
 
       def help_offer_without_listing_and_booking
         @user = User.all.detect { |u| u.listings.empty? && u.reservations.empty? }
         raise "No user without listing and without reservation" unless @user
-        ::AfterSignupMailer.help_offer(@user)
+        ::AfterSignupMailer.help_offer(@instance, @user)
       end
 
     end
