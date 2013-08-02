b3fe92d31ae787aed19bf1ec67401cf8e470f804
Maciej Krajowski-Kukiel <maciej.krajowski@gmail.com>
2013-08-02 19:58:46 +0200

    Pass instance_id and current domain to Mixpanel events.
    * add request.host and current_instance.id to all mixpanel events
    
    https://trello.com/c/jdePe1DM/142-pass-instance-id-and-current-domain-b-c-an-instance-might-have-multiple-domains-to-mixpanel-events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,12 +40,19 @@ class ApplicationController < ActionController::Base
         ActiveSupport::JSON.decode(cookies.signed[:mixpanel_session_properties]) rescue nil
       end
 
+      # Gather information about requests
+      request_details = {
+        :current_instance_id => current_instance.try(:id),
+        :current_host => request.try(:host)
+      }
+
       # Detect an anonymous identifier, if any.
       anonymous_identity = cookies.signed[:mixpanel_anonymous_id]
 
       MixpanelApi.new(
         MixpanelApi.mixpanel_instance,
         :current_user       => current_user,
+        :request_details    => request_details,
         :anonymous_identity => anonymous_identity,
         :session_properties => session_properties,
         :request_params     => params
