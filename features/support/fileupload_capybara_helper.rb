74024eb6cfe4ffd8e263497d2d6749a5925998ab
Josef Šimánek <josef.simanek@gmail.com>
2013-08-06 03:06:19 +0200

    Comment custom capybara make_input_visible helper.

--- features/support/fileupload_capybara_helper.rb
+++ features/support/fileupload_capybara_helper.rb
@@ -4,9 +4,13 @@ module FileuploadCapybaraHelper
     attach_file(name, image)
   end
 
+  # I experienced weird bug issued at thoughtbot/capybara-webkit#494.
+  # This is dirty fix how to set some dimensions to element.
+  # Capybara sometimes can't click to zero dimensioned element.
   def make_input_visible(name)
     page.execute_script "$(\"input[name='#{name}']\").css('width', '10px')"
     page.execute_script "$(\"input[name='#{name}']\").css('height', '10px')"
   end
 end
+
 World(FileuploadCapybaraHelper)
