90f0c28389a7bab56a452b59b170317a79ab12e2
Jared Armstrong <jared.armstrong@gmail.com>
2013-08-15 11:44:30 +1200

    Simple background processing of photo uploads

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,11 +10,13 @@ class Photo < ActiveRecord::Base
   default_scope -> { rank(:position) }
   scope :no_content, -> { where content_id: nil }
   scope :for_listing, -> { where content_type: 'Listing' }
+  scope :ready, -> { where(versions_generated: true) }
 
   acts_as_paranoid
 
   after_create :notify_user_about_change
   after_destroy :notify_user_about_change
+
   # Don't delete the photo from s3
   skip_callback :destroy, :after, :remove_image!
 
@@ -28,10 +30,34 @@ class Photo < ActiveRecord::Base
 
   AVAILABLE_CONTENT = ['Listing', 'Location']
 
+  before_create :disable_processing # Handle processing after saving the image
+  after_commit :enqueue_processing, on: :create
+
+  def generate_versions
+    image.recreate_versions!
+    self.versions_generated = true
+    save!
+  end
+
   def method_missing(method, *args, &block)
     super(method, *args, &block)
   rescue NoMethodError
     image.send(method, *args, &block)
+  end
+
+  private
+
+  # We disable processing so that CarrierWave doesn't generate and upload
+  # our specified versions.
+  def disable_processing
+    image.enable_processing = false
+    true
+  end
+
+  # We enqueue a processing job for after we've created and saved the photo.
+  # This enables us to control when the processing ocurrs.
+  def enqueue_processing
+    PhotoImageProcessJob.perform(id)
   end
 
 end
