75569071cde84bba44ca7c0c82f0925897c7ee3b
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2013-08-27 23:03:40 -0700

    redirect to first listings page if param[:page] is not an integer

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,5 @@
 class ListingsController < ApplicationController
+  before_filter :sanitize_page_param, :only => [:index]
   before_filter :find_listing, :only => [:show]
 
   def index
@@ -10,6 +11,12 @@ class ListingsController < ApplicationController
   end
 
   protected
+
+  def sanitize_page_param
+    if params[:page] && params[:page].to_i == 0
+      redirect_to listings_path(:page =>1), :flash => { :warning => "Requested page does not exist, showing first page." }
+    end
+  end
 
   def find_listing
     @listing = Listing.with_deleted.find(params[:id])
