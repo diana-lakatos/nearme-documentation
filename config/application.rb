4b0cff661e117309967ab0d1a075e5acb8269c3b
Josef Šimánek <josef.simanek@gmail.com>
2013-08-30 06:52:49 +0200

    Add social sharing icons to Location page.
    * using ZeroClipboard for clipboard copy
    * add SocialHelper for Facebook, Twitter, Linkedin and Email sharing of
      links
    * add Clipboard JS component for copying into system clipboard and
      toggling info tooltip
    
    https://trello.com/c/6HfZSAUJ/332

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module DesksnearMe
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     config.assets.paths           << %(#{Rails.root}/app/assets/fonts)
+    config.assets.paths           << %(#{Rails.root}/app/assets/swfs)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
