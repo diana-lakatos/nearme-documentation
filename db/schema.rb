7e737bd9790523cfc7c2eb410958dbaf894ced90
Krzysztof Kempiński <kkempin@gmail.com>
2013-09-10 10:05:52 +0200

    Host can enable/disable a listing.
    * add new listing field
    * change listing manage view
    * add listing scope
    * add tests
    
    https://trello.com/c/b8yLz3dY

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20130829005231) do
+=======
+ActiveRecord::Schema.define(:version => 20130909182922) do
+>>>>>>> 0431014... Host can enable/disable a listing.
 
   create_table "amenities", :force => true do |t|
     t.string   "name"
@@ -215,6 +219,7 @@ ActiveRecord::Schema.define(:version => 20130829005231) do
     t.string   "external_id"
     t.boolean  "free",                    :default => false
     t.datetime "draft"
+    t.boolean  "enabled",                 :default => true
   end
 
   create_table "location_amenities", :force => true do |t|
