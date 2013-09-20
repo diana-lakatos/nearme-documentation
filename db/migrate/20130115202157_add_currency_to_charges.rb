5401780107a949a1f52b29ddd6349ac85a03a755
Josef Šimánek <josef.simanek@gmail.com>
2013-09-20 04:02:59 +0200

    fix schema and migration

--- db/migrate/20130115202157_add_currency_to_charges.rb
+++ db/migrate/20130115202157_add_currency_to_charges.rb
@@ -2,13 +2,14 @@ class AddCurrencyToCharges < ActiveRecord::Migration
   def up
     add_column :charges, :currency, :string
 
-    execute <<-SQL
-      UPDATE reservation_charges
-      INNER JOIN
-        reservations ON (reservations.id = reservation_charges.reservation_id)
-      SET
-        reservation_charges.currency = reservation.currency
-    SQL
+    # FIXME: doesn't work
+    # execute <<-SQL
+    #   UPDATE reservation_charges
+    #   INNER JOIN
+    #     reservations ON (reservations.id = reservation_charges.reservation_id)
+    #   SET
+    #     reservation_charges.currency = reservation.currency
+    # SQL
   end
 
   def down
