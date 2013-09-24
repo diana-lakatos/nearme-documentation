a0404911cfcecae7cd7831c443801d3466306afa
Josef Šimánek <josef.simanek@gmail.com>
2013-09-24 19:53:27 +0200

    Create theme from factory in cucumber before block.

--- features/support/env_custom.rb
+++ features/support/env_custom.rb
@@ -12,7 +12,6 @@ Before do
   DatabaseCleaner.clean
   WebMock.disable_net_connect!
   GmapsFake.stub_requests
-  FactoryGirl.create(:instance)
   stub_request(:get, /.*api\.mixpanel\.com.*/)
   stub_request(:post, "https://www.googleapis.com/urlshortener/v1/url")
   instance = FactoryGirl.create(:instance)
