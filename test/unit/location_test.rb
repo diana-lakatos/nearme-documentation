34ecc4df9c0985cc34a3771bd52ea0e6797131a1
Josef Šimánek <josef.simanek@gmail.com>
2013-10-23 05:34:17 +0200

    Display edit icon next to location/listing for admin users.
    https://trello.com/c/GzcYvMv2/524

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -184,4 +184,20 @@ class LocationTest < ActiveSupport::TestCase
     end
   end
 
+  context '#is_user_admin?' do
+    should "be valid for location company admins" do
+      user = FactoryGirl.create(:user, admin: true, companies: [@location.company])
+      assert @location.is_user_admin?(user)
+    end
+
+    should "be invalid for location company users" do
+      user = FactoryGirl.create(:user, admin: false, companies: [@location.company])
+      refute @location.is_user_admin?(user)
+    end
+
+    should "be invalid for foreign users" do
+      user = FactoryGirl.create(:user)
+      refute @location.is_user_admin?(user)
+    end
+  end
 end
