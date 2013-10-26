9f5b7241a9d9bb8b10ec448247dd1aea80f7e513
Patrik Jira <patrikjira@gmail.com>
2013-10-26 16:08:13 +0200

    Improve messaging layout
    Center action button
    
    Move and format action button for mobiles
    
    Unite margins and paddings
    
    Swap colors for foreign-message and my-message
    
    https://trello.com/c/vypFpXKb/476-messaging-functionality

--- app/decorators/listing_message_decorator.rb
+++ app/decorators/listing_message_decorator.rb
@@ -5,8 +5,17 @@ class ListingMessageDecorator < Draper::Decorator
     owner_id == author_id ? listing.name : owner.name
   end
 
-  def css_class
-    read? ? 'read' : 'unread'
+  def css_class(user = nil)
+    classes = []
+    classes << (read? ? 'read' : 'unread')
+    if user
+      if author == user
+        classes << 'my-message'
+      else
+        classes << 'foreign-message'
+      end
+    end
+    classes.join(' ')
   end
 
 end
