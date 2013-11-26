87db0bc265902894affb104fbb3a2275816b3a44
Krzysztof Kempiński <kkempin@gmail.com>
2013-11-26 20:18:51 +0100

    Use instance logo instead of DNM logo in email layout.
    https://trello.com/c/1qDYTMSH

--- app/drops/platform_context_drop.rb
+++ app/drops/platform_context_drop.rb
@@ -1,6 +1,6 @@
 class PlatformContextDrop < BaseDrop
   delegate :name, :bookable_noun, :pages, :platform_context, :is_desksnearme, :blog_url, :twitter_url, :lessor, :lessee,
-    :facebook_url, :address, :phone_number, :gplus_url, :site_name, :support_url, :to => :platform_context_decorator
+    :facebook_url, :address, :phone_number, :gplus_url, :site_name, :support_url, :logo_image, :to => :platform_context_decorator
 
   def initialize(platform_context_decorator)
     @platform_context_decorator = platform_context_decorator
@@ -8,6 +8,10 @@ class PlatformContextDrop < BaseDrop
 
   def bookable_noun_plural
     @platform_context_decorator.bookable_noun.pluralize
+  end
+
+  def logo_url
+    @platform_context_decorator.logo_image.url || "https://s3.amazonaws.com/desksnearme.production/uploads/theme/logo_retina_image/1/logo_2x.png"
   end
 
   private
