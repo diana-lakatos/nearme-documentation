a7c0391cee0bd3e652fe6a7d344cb7524085690a
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2013-12-18 15:56:20 -0800

    Break large instance_admin/settings view out into partials.

--- app/controllers/instance_admin/settings_controller.rb
+++ app/controllers/instance_admin/settings_controller.rb
@@ -1,13 +1,11 @@
 class InstanceAdmin::SettingsController < InstanceAdmin::BaseController
   before_filter :find_instance
 
-
   def show
     @instance
   end
 
   def update
-    params[:instance][:pricing_options] = {} if params[:instance][:pricing_options].blank?
     if @instance.update_attributes(params[:instance])
       flash[:success] = t('flash_messages.instance_admin.settings.settings_updated_successfully')
       redirect_to :action => :show
