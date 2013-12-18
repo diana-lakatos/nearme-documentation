59fa7057b79a88bd7e5d5c1842eb019f917aca25
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2013-12-18 19:15:13 +0100

    Received follow up email after booking with wrong suggestions
    https://trello.com/c/aadeAH7m/819-received-follow-up-email-after-booking-check-out-these-new-spaces-in-your-area-with-suggestions-of-spaces-in-australia-instead-o

--- app/models/user.rb
+++ app/models/user.rb
@@ -405,10 +405,11 @@ class User < ActiveRecord::Base
   end
 
   def listings_in_near(results_size = 3, radius_in_km = 100)
-    if last_geolocated_location_latitude.to_f > 0 and last_geolocated_location_longitude.to_f > 0
-      locations_in_near = Location.near([last_geolocated_location_latitude, last_geolocated_location_longitude], radius_in_km, units: :km, order: :distance)
-    else
+    # we want allow greenwhich and friends, but probably 0 latitude and 0 longitude is not valid location :)
+    if last_geolocated_location_latitude.nil? || last_geolocated_location_longitude.nil? || (last_geolocated_location_latitude.to_f.zero? && last_geolocated_location_longitude.to_f.zero?)
       locations_in_near = Location.near(current_location, radius_in_km, units: :km, order: :distance)
+    else
+      locations_in_near = Location.near([last_geolocated_location_latitude, last_geolocated_location_longitude], radius_in_km, units: :km, order: :distance)
     end
     
     locations_in_near = Location if locations_in_near.count.zero?
