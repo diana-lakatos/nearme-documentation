0624d8edc7410250cbd1747909d07d8ef3c667d7
Patrik Jira <patrikjira@gmail.com>
2014-01-06 15:31:03 +0100

    Add InstagramProvider
    https://trello.com/c/rf6PwgBR/521-populate-user-profile-data-from-connected-social-accounts

--- app/models/authentication/instagram_provider.rb
+++ app/models/authentication/instagram_provider.rb
@@ -2,9 +2,46 @@ class Authentication::InstagramProvider < Authentication::BaseProvider
 
   KEY    = DesksnearMe::Application.config.instagram_key
   SECRET = DesksnearMe::Application.config.instagram_secret
+  META   = { name: "Instagram",
+             url: "http://instagram.com/",
+             auth: "OAuth 2" }
 
   def friend_ids
-    []
+    begin
+      @friend_ids ||= connection.user_follows.map(&:id)
+    rescue Instagram::BadRequest, Instagram::NotFound
+      raise ::Authentication::InvalidToken
+    end
+  end
+
+  def info
+    @info ||= begin
+      Info.new(connection.user)
+    rescue Instagram::BadRequest, Instagram::NotFound
+      raise ::Authentication::InvalidToken
+    end
+  end
+
+  class Info < BaseInfo
+
+    def initialize(raw)
+      @raw          = raw
+      @uid          = raw.id
+      @username     = raw.username
+      @name         = raw.full_name
+      @description  = raw.bio
+      @image_url    = raw.profile_picture
+      @profile_url  = "http://instagram.com/#{raw.username}"
+      @website_url  = raw.website
+      @provider     = 'Instagram'
+    end
+
+  end
+
+  private
+
+  def connection
+    @connection ||= Instagram.client(:access_token => token)
   end
 
 end
