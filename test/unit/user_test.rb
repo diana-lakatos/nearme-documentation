2499510f601ad7c420ae48ef659ed7cfb207243d
Josef Šimánek <josef.simanek@gmail.com>
2014-01-21 06:26:10 +0100

    NM-312 Don't rename email addresses on delete.
    * replace acts_as_paranoid with paranoia gem
    * introduce power_pg gem to enable partial index in db schema
    * remove email renaming when user is deleted
    * add partial index to ensure only one undeleted user is valid
    * use custom devise model for validating email
    * add rake task update:normalize_emails to fix revert deleted emails

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -145,46 +145,6 @@ class UserTest < ActiveSupport::TestCase
       end
 
     end
-
-
-    context 'deleting user and validation' do
-
-      setup do
-        User.destroy_all
-        @user = FactoryGirl.create(:user)
-        @email = @user.email
-      end
-
-      should 'allow to register a user with the same email as the one that already exists given he is deleted' do
-        @user.destroy
-        assert_nothing_raised do
-          @new_user = FactoryGirl.create(:user, :email => @email)
-        end
-      end
-
-      should 'not allow to register with the same email address as already existing user who is not deleted' do
-        assert_raise ActiveRecord::RecordInvalid do
-          FactoryGirl.create(:user, :email => @user.email)
-        end
-      end
-
-      should 'ensure that the email is the same after recover' do
-        @user.destroy
-        @user.recover
-        @user.reload
-        assert_equal @email, @user.email, "Email of recovered user is different from the one user was using before deletion"
-        assert !@user.deleted?, "User is still deleted after recovery"
-      end
-
-      should 'blow up if we try to recover user whose email has been taken while he was deleted' do
-        @user.destroy
-        FactoryGirl.create(:user, :email => @email)
-        assert_raise ActiveRecord::RecordInvalid do
-          @user.recover
-        end
-      end
-
-    end
   end
 
   context 'name' do
