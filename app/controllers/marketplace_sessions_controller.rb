80349ac3f13b77258f960c3fd3a55ceacea49d30
Krzysztof Kempiński <kkempin@gmail.com>
2014-01-23 15:12:01 +0100

    NM-322 Remove logging

--- app/controllers/marketplace_sessions_controller.rb
+++ app/controllers/marketplace_sessions_controller.rb
@@ -9,8 +9,6 @@ class MarketplaceSessionsController < ApplicationController
       session["authenticated_in_marketplace_#{platform_context.instance.id}".to_sym] = true
       redirect_url = session[:marketplace_return_to] || root_path
       session[:marketplace_return_to] = nil
-      Rails.logger.info '#'*90
-      Rails.logger.info redirect_url
       redirect_to redirect_url
     else
       flash[:error] = t('flash_messages.marketplace_sessions.wrong_password')
