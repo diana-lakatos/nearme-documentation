1a0b189f1892c209fcaf848ff9668d6b82745a92
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2014-02-02 12:41:44 +0100

    NM-362 private method file called for CarrierWave

--- app/uploaders/theme_image_uploader.rb
+++ app/uploaders/theme_image_uploader.rb
@@ -2,9 +2,4 @@ class ThemeImageUploader < BaseImageUploader
   def default_url
     nil
   end
-
-  def capture_dimensions
-    img = MiniMagick::Image::read(File.binread(@file.file))
-    [img[:width], img[:height]]
-  end
 end
