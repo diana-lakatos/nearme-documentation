71c3540ec02c5468f33dcc01e174f86b74c628fe
Patrik Jira <patrikjira@gmail.com>
2014-02-14 12:34:51 +0100

    NM-434 undefined method enabled?
    Add missing blog_instances via migration

---
+++ db/migrate/20140214110817_create_blog_instances_for_all_instances.rb
@@ -0,0 +1,25 @@
+class CreateBlogInstancesForAllInstances < ActiveRecord::Migration
+  class BlogInstance < ActiveRecord::Base
+    belongs_to :owner, polymorphic: true
+  end
+
+  class Instance < ActiveRecord::Base
+    has_one :blog_instance, :as => :owner
+  end
+
+  def up
+    # Create blog for all instances that havent it yet
+    Instance.all.each do |instance|
+      next if instance.blog_instance.present?
+      name = instance.name + " Blog"
+      puts "Creating #{name}"
+      blog_instance = BlogInstance.new(name: name)
+      blog_instance.owner_id = instance.id
+      blog_instance.owner_type = "Instance"
+      blog_instance.save!
+    end
+  end
+
+  def down
+  end
+end
