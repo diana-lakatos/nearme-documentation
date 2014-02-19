ea3e09656c9add7b2816f23bc3a0fe7d958601cc
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2014-02-18 17:31:15 -0800

    NM-471 Add javascript tabs to near-me/features.

--- app/assets/javascripts/platform_home.js
+++ app/assets/javascripts/platform_home.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require ./vendor/jquery-ui-1.9.2.custom.min
 //= require ./vendor/jquery.simplyscroll.min
 //= require ./vendor/jquery.icheck.min.js
 //= require ./components/flash
