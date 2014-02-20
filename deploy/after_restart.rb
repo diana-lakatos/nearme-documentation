0d7831868f5acbb666f6ad7c863d59f307820f9e
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2014-02-20 22:19:56 +0100

    Invoke rake reprocess:css only on master_app and not on utilities

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,10 +2,10 @@
 on_app_master do
   description = (`cd #{config.current_path} && git log -1 --format="%s"`).chomp
   run "cd #{config.current_path} && bundle exec newrelic deployments --user='#{config.deployed_by}' --revision='#{config.revision}' '#{description}'"
+
+  # Recompile the css for each instance's theme
+  run "bundle exec rake reprocess:css"
 end
 
 # Must restart utilities that require the environment
 sudo "monit -g dj_#{config.app} restart all"
-
-# Recompile the css for each instance's theme
-run "bundle exec rake reprocess:css"
