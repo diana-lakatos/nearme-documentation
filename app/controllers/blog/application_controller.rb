943142b382d3f50e507409e45d17b99108a9bb75
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2014-02-24 14:52:34 -0800

    NM-445 Move Near-Me into the root domains list for platform_context.

--- app/controllers/blog/application_controller.rb
+++ app/controllers/blog/application_controller.rb
@@ -14,14 +14,6 @@ class Blog::ApplicationController < ApplicationController
                      end
   end
 
-  def platform_context
-    if near_me?
-      @platform_context ||= PlatformContext.new(Instance.default_instance)
-    else
-      super
-    end
-  end
-
   def near_me?
     request.host == 'near-me.com'
   end
