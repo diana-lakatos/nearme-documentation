8e45245ff8bc869fb4d10ecd2224916f27dc9a9b
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2014-03-18 13:25:04 +0100

    Fix social authentications
    * some users don't have instance_id which is required - make sure they
     belong to default instance in that case (via console)
    * old authentications don't have instance_id which is required - again
     use console to provide default instance
    * fix typo in facebook
    * upgrade gems to fix security vulnerabilities and make things work
     change gem for linkedin (replace omniauth-linkedin-oauth2 with
     omniauth-linkedin) - it seems that it does not work with setup
     phase
    * fix typo for instagram
    * provide correct keys for social authentications in seeds

--- app/models/authentication/base_provider.rb
+++ app/models/authentication/base_provider.rb
@@ -9,8 +9,10 @@ class Authentication::BaseProvider
 
   def self.setup_proc 
     lambda do |env| 
-      env['omniauth.strategy'].options[:consumer_key] = PlatformContext.current.instance.send(:"#{provider}_consumer_key")
-      env['omniauth.strategy'].options[:consumer_secret] = PlatformContext.current.instance.send(:"#{provider}_consumer_secret")
+      Rails.logger.debug "will use: #{PlatformContext.current.instance.send(:"#{provider}_consumer_key").try(:strip)}, #{PlatformContext.current.instance.send(:"#{provider}_consumer_secret").try(:strip)}"
+      env['omniauth.strategy'].options[:consumer_key] = PlatformContext.current.instance.send(:"#{provider}_consumer_key").try(:strip)
+      env['omniauth.strategy'].options[:consumer_secret] = PlatformContext.current.instance.send(:"#{provider}_consumer_secret").try(:strip)
+      env['omniauth.strategy'].options.authorize_params[:state]
     end
   end
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, :setup => Authentication::TwitterProvider.setup_proc, image_size: 'original'
   provider :facebook, :setup => Authentication::FacebookProvider.setup_proc, image_size: {width: 500}
   provider :linkedin, :setup => Authentication::LinkedinProvider.setup_proc, scope: 'r_emailaddress r_network', fields: ['id', 'first-name', 'last-name', 'headline', 'industry', 'picture-url', 'public-profile-url', 'email-address', 'connections']
-  provider :instagram, :setup => Authentication::LinkedinProvider.setup_proc
+  provider :instagram, :setup => Authentication::InstagramProvider.setup_proc
 end
