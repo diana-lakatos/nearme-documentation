826a5321215ee519a139d3cef897d9c057f04150
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2014-03-18 22:50:20 -0700

    NM-226 Fix rating reminder mailer names.

--- app/mailers/rating_mailer.rb
+++ app/mailers/rating_mailer.rb
@@ -1,24 +1,14 @@
 class RatingMailer < InstanceMailer
   layout 'mailer'
 
-  def request_guest_rating(reservation)
-    @who_is_rating      = 'host'
+  def request_rating_of_guest_from_host(reservation)
     user = reservation.administrator
-
-    @who_is_rated       = 'guest'
-    @subject = reservation.owner
-
     @subject = "How was your experience hosting #{reservation.owner.first_name}?"
     request_rating(reservation, user)
   end
 
-  def request_host_rating(reservation)
-    @who_is_rating    = 'guest'
+  def request_rating_of_host_from_guest(reservation)
     user  = reservation.owner
-
-    @who_is_rated     = 'host'
-    @subject = reservation.administrator
-
     @subject = "How was your experience at '#{reservation.listing.name}'?"
     request_rating(reservation, user)
   end
@@ -35,8 +25,7 @@ class RatingMailer < InstanceMailer
     @location = @listing.location
     @user = user
     mail to: @user.email,
-         subject_locals: { reservation: @reservation, listing: @listing },
-         template_name: "request_rating_of_#{@who_is_rated}_from_#{@who_is_rating}"
+         subject_locals: { reservation: @reservation, listing: @listing }
   end
 
 end
