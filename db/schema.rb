88da140e822b958c76aaf3391589300f4e55d557
Krzysztof Kempiński <kkempin@gmail.com>
2014-03-28 11:13:36 +0100

    NM-529 Improve layout of post summary on blog index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140327114743) do
+ActiveRecord::Schema.define(:version => 20140328093851) do
 
 
 
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(:version => 20140327114743) do
     t.datetime "published_at"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.text     "excerpt"
   end
 
   create_table "charges", :force => true do |t|
