f622a183bbf1d06c1945ea340e9acb753a74e692
Krzysztof Kempiński <kkempin@gmail.com>
2014-03-31 09:47:52 +0200

    NM-530 Improve hero image format/display

--- app/models/blog_instance.rb
+++ app/models/blog_instance.rb
@@ -1,8 +1,11 @@
 class BlogInstance < ActiveRecord::Base
 
-  attr_accessible :enabled, :name, :header, :facebook_app_id
+  attr_accessible :enabled, :name, :header, :facebook_app_id,
+                  :header_text, :header_motto, :header_logo, :header_icon
 
   mount_uploader :header, HeroImageUploader
+  mount_uploader :header_logo, HeroImageUploader
+  mount_uploader :header_icon, HeroImageUploader
 
   has_many :blog_posts
   belongs_to :owner, polymorphic: true
