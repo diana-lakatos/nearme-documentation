53fd33ca8bdc3bcd6053f8dce54c1ea97f1f35de
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2014-05-05 20:14:55 +0200

    NM-664 instance_admin/analytics Intermittent divergent combustion
    * add test coverage for instance_admin/analytics/overview_controller
    * fix rake task to populate transactable type

---
+++ test/functional/instance_admin/analytics/overview_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class InstanceAdmin::Analytics::OverviewControllerTest < ActionController::TestCase
+
+  setup do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+    InstanceAdminAuthorizer.any_instance.stubs(:instance_admin?).returns(true)
+    InstanceAdminAuthorizer.any_instance.stubs(:authorized?).returns(true)
+  end
+
+  context 'show' do
+
+    should 'show listings from last 30 days' do
+      @fresh_transactable = FactoryGirl.create(:transactable)
+      @old_transactable = FactoryGirl.create(:transactable).update_column(:created_at, 31.days.ago)
+      get :show
+      assert_response :success
+      assert_equal([{"listings_count"=>"1", "listing_date"=>"#{Time.zone.now.to_date}", "transactable_type_id"=>TransactableType.first.id}], assigns(:last_month_listings).map(&:attributes))
+    end
+  end
+
+end
