477a1be0f6d273fefb70e614b5ac743f52710edc
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2014-05-06 15:39:45 -0700

    NM-666 Add destroy controller actions and buttons to /admin transactables.

--- app/controllers/admin/transactable_type_attributes_controller.rb
+++ app/controllers/admin/transactable_type_attributes_controller.rb
@@ -3,6 +3,22 @@ class Admin::TransactableTypeAttributesController < Admin::ResourceController
 
   before_filter :eval_parameters, :only => [:create, :update]
 
+  def create
+    resource = TransactableTypeAttribute.new(params[:transactable_type_attribute])
+    resource.transactable_type_id = params[:transactable_type_id]
+    resource.instance_id = TransactableType.find(params[:transactable_type_id]).instance_id
+    if resource.save
+      redirect_to admin_transactable_type_transactable_type_attribute_path(resource.transactable_type , resource)
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    TransactableTypeAttribute.find(params[:id]).destroy
+    redirect_to admin_instance_transactable_type_path(Instance.find(params[:transactable_type_id]), params[:transactable_type_id])
+  end
+
   def eval_parameters
     if params[:transactable_type_attribute]
       %w(validation_rules valid_values input_html_options wrapper_html_options).each do |serialized_param|
