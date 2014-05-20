cc532b68566e6128587f42035b3cd7592007d31f
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2014-05-20 17:49:31 +0200

    fixes reloading issue in development. Ask Josef for details

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,6 @@ module DesksnearMe
     config.autoload_paths -= Dir["#{config.root}/lib/previewers/"] unless defined? MailView
 
     config.eager_load_paths += ["#{Rails.root}/lib}"]
-    config.eager_load_paths += ["#{Rails.root}/drop}"]
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
