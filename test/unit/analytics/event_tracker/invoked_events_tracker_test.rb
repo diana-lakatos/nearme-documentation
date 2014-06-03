595428039df13bf73aa6afc3bfc2b2ed35f3c59d
Josef Šimánek <josef.simanek@gmail.com>
2014-06-04 01:08:00 +0200

    Use proper folder and test classname for invoked_events_tracker_test.rb

--- test/unit/analytics/event_tracker/invoked_events_tracker_test.rb
+++ test/unit/analytics/event_tracker/invoked_events_tracker_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Analytics::EventTracker::InvokedEventsTrackerTest < ActiveSupport::TestCase
+class InvokedEventsTrackerTest < ActiveSupport::TestCase
 
   context '#event tracker' do
 
