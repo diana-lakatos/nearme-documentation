c6a5d7ce68792a68a83b35760e6983cfd07ac292
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2014-06-10 14:32:06 +0200

    NM-625 Revisit google webmaster tools issues
    * force ssl on near-me

--- app/controllers/platform_home_controller.rb
+++ app/controllers/platform_home_controller.rb
@@ -1,4 +1,5 @@
 class PlatformHomeController < ActionController::Base
+  before_filter :require_ssl
   prepend_view_path InstanceViewResolver.instance
 
   protect_from_forgery
@@ -10,6 +11,16 @@ class PlatformHomeController < ActionController::Base
       redirect_to '/'
     end
     @platform_contact = PlatformContact.new
+  end
+
+  def require_ssl
+    if Rails.application.config.secure_app && !request.ssl?
+      if request.get? # we can't redirect non-get reqests
+        redirect_to url_for(protocol: 'https'), status: :moved_permanently
+      else
+        redirect_to root_url(protocol: 'https')
+      end
+    end
   end
 
   def contact
