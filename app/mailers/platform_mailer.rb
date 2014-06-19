b6e7059911caf06cff9e9c178b777829cf804116
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2014-06-19 00:01:24 -0700

    NM-xxx Re-add nearme lead intro mailer.

--- app/mailers/platform_mailer.rb
+++ app/mailers/platform_mailer.rb
@@ -4,12 +4,8 @@ class PlatformMailer < ActionMailer::Base
   NOTIFICATIONS_EMAIL = 'sales@near-me.com'
 
   def email_notification(email)
-    verifier = ActiveSupport::MessageVerifier.new(DesksnearMe::Application.config.secret_token)
-    unsubscribe_key = verifier.generate(email.email)
-    @unsubscribe_url = platform_email_unsubscribe_url(unsubscribe_key)
-
     mail from: 'michelle@near-me.com',
-         to: email.email,
+         to: email,
          subject: "Hello from NearMe!"
   end
 
