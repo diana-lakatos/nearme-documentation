5cc6d864dfa7893dfc2e5365f164540e0fe5b0e0
Jaromír Červenka <jaromir@virt.io>
2014-09-24 12:45:18 +0200

    style: Typo user_blog_eanbled? => user_blog_enabled?

--- app/controllers/user_blog/base_controller.rb
+++ app/controllers/user_blog/base_controller.rb
@@ -1,10 +1,10 @@
 class UserBlog::BaseController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :user_blog_eanbled?
+  before_filter :user_blog_enabled?
 
   private
 
-  def user_blog_eanbled?
+  def user_blog_enabled?
     redirect_to '/404' unless platform_context.instance.user_blogs_enabled?
   end
 end
