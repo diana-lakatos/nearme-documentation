9c4c4828bf21f0d473c07555e277bd02c146e9bb
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2014-10-17 13:08:47 +0200

    NM-720 InstanceView caching
    * keep track of last update of instance view for every instance
    * keep track of count of instance views for every instance
    * if count changes, whipe instance view cache
    * if instance view gets updated/created, whipe cache
    * this solution makes sure that all webservers will use the newest
     version of instance view (or will not use deleted instance view)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -177,9 +177,12 @@ class RegistrationsController < Devise::RegistrationsController
   def store_geolocated_location
     if user_signed_in? && params[:longitude] && params[:latitude]
       @user = current_user
-      @user.last_geolocated_location_longitude = params[:longitude]
-      @user.last_geolocated_location_latitude = params[:latitude]
-      @user.save if @user.changes.present?
+      delta = 0.0001
+      if ((@user.last_geolocated_location_longitude.to_f - params[:longitude].to_f).abs > delta) || ((@user.last_geolocated_location_latitude.to_f - params[:latitude].to_f).abs > delta)
+        @user.last_geolocated_location_longitude = params[:longitude]
+        @user.last_geolocated_location_latitude = params[:latitude]
+        @user.save
+      end
     end
     render :nothing => true
   end
