9c1bb905ef14a6775ee2d5ae3cdd0797c9b0676f
Josef Šimánek <josef.simanek@gmail.com>
2014-10-21 11:02:49 +0200

    Add tests for blog.

--- test/functional/user_blog/blog_controller_test.rb
+++ test/functional/user_blog/blog_controller_test.rb
@@ -6,18 +6,32 @@ class UserBlog::BlogControllerTest < ActionController::TestCase
     setup do
       @user = FactoryGirl.create(:user)
       sign_in @user
+      PlatformContext.current.instance.update_column(:user_blogs_enabled, true)
     end
 
 
     should 'redirect to 404 if user blogging is disabled on instance' do
+      PlatformContext.current.instance.update_column(:user_blogs_enabled, false)
       get :index
       assert_response :missing
     end
 
     should 'display settings if user blogging is enabled on instance' do
-      PlatformContext.current.instance.update_column :user_blogs_enabled, true
       get :settings
       assert_response :ok
+    end
+
+    should 'display blog posts' do
+      get :index
+      assert_response :ok
+    end
+
+    should 'update blog settings' do
+      name = 'My little ponny bloge'
+      patch :update_settings, user_blog: {name: name}
+      assert_response :redirect
+      assert_redirected_to user_blog_path
+      assert_equal @user.blog.reload.name, name
     end
   end
 end
--- test/functional/user_blog/blog_posts_controller_test.rb
+++ test/functional/user_blog/blog_posts_controller_test.rb
@@ -1,16 +1,64 @@
 require 'test_helper'
 
 class UserBlog::BlogPostsControllerTest < ActionController::TestCase
+  setup do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+    PlatformContext.current.instance.update_column(:user_blogs_enabled, true)
+  end
+
+  context 'with existing blog post' do
+    setup do
+      @blog = FactoryGirl.create(:user_blog_post, user: @user)
+    end
+
+    context '#edit' do
+      should 'render form' do
+        get :edit, id: @blog.id
+        assert_response :success
+      end
+    end
+
+    context '#update' do
+      should 'update' do
+        title = 'updated title'
+        put :update, id: @blog.id, user_blog_post: {title: title}
+        assert_equal @blog.reload.title, title
+      end
+    end
+
+    context '#destroy' do
+      should 'destroy' do
+        assert_difference('@user.blog_posts.count', -1) {
+          delete :destroy, id: @blog.id
+        }
+        assert_response :redirect
+        assert_redirected_to user_blog_path
+      end
+    end
+  end
 
   context '#new' do
-    setup do
-      @user = FactoryGirl.create(:user)
-      sign_in @user
+    should 'render' do
+      get :new
+      assert_response :success
     end
 
     should 'redirect to 404 if user blogging is disabled on instance' do
+      PlatformContext.current.instance.update_column(:user_blogs_enabled, false)
       get :new
       assert_response :missing
+    end
+  end
+
+  context '#create' do
+    should 'create new blog post' do
+      params = FactoryGirl.attributes_for(:user_blog_post)
+      assert_difference('@user.blog_posts.count') {
+        post :create, user_blog_post: params
+      }
+      assert_response :redirect
+      assert_redirected_to user_blog_path
     end
   end
 end
