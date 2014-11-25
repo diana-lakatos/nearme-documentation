c36856330c0d540cf7fb0002cf3b6ef62c54c448
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2014-11-25 10:23:21 +0100

    NM-1153 Make sure MPOs can't edit Capacity, Minimum booking minutes, and Listing type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119164053) do
+ActiveRecord::Schema.define(version: 20141125090820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
