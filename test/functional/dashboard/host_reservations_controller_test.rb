615a3f4de5f607485f031d9884d29a208890ff5f
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-01-18 14:25:47 +0100

    fix

--- test/functional/dashboard/host_reservations_controller_test.rb
+++ test/functional/dashboard/host_reservations_controller_test.rb
@@ -3,163 +3,149 @@ require 'test_helper'
 class Dashboard::HostReservationsControllerTest < ActionController::TestCase
 
   setup do
-    @listing = FactoryGirl.create(:listing_in_san_francisco)
-
-    @user = FactoryGirl.create(:user, name: "Example LastName")
+    @reservation = FactoryGirl.create(:reservation_with_credit_card)
+    @reservation.create_billing_authorization(token: "123", payment_gateway_class: "Billing::Gateway::Processor::Incoming::Stripe", payment_gateway_mode: "test")
+    @user = @reservation.listing.creator
     sign_in @user
     stub_mixpanel
     stub_request(:post, "https://www.googleapis.com/urlshortener/v1/url")
-
-    stub_billing_gateway(@listing.instance)
+    stub_billing_gateway(@reservation.instance)
     stub_active_merchant_interaction
+  end
 
+  should "track and redirect a host to the Manage Guests page when they confirm a booking" do
+    ReservationMailer.expects(:notify_guest_of_confirmation).returns(stub(deliver: true)).once
+    ReservationMailer.expects(:notify_host_of_confirmation).returns(stub(deliver: true)).once
+    ReservationSmsNotifier.expects(:notify_guest_with_state_change).returns(stub(deliver: true)).once
+
+    @tracker.expects(:confirmed_a_booking).with do |reservation|
+      reservation == assigns(:reservation)
+    end
+    @tracker.expects(:updated_profile_information).with do |user|
+      user == assigns(:reservation).owner
+    end
+    @tracker.expects(:updated_profile_information).with do |user|
+      user == assigns(:reservation).host
+    end
+
+    post :confirm, { listing_id: @reservation.listing.id, id: @reservation.id }
+
+    assert_redirected_to dashboard_guests_path
+  end
+
+  should "track and redirect a host to the Manage Guests page when they reject a booking" do
+    ReservationMailer.expects(:notify_guest_of_rejection).returns(stub(deliver: true)).once
+    ReservationSmsNotifier.expects(:notify_guest_with_state_change).returns(stub(deliver: true)).once
+
+    @tracker.expects(:rejected_a_booking).with do |reservation|
+      reservation == assigns(:reservation)
+    end
+    @tracker.expects(:updated_profile_information).with do |user|
+      user == assigns(:reservation).owner
+    end
+    @tracker.expects(:updated_profile_information).with do |user|
+      user == assigns(:reservation).host
+    end
+    put :reject, { listing_id: @reservation.listing.id, id: @reservation.id }
+    assert_redirected_to dashboard_guests_path
+  end
+
+  should "track and redirect a host to the Manage Guests page when they cancel a booking" do
+    ReservationMailer.expects(:notify_guest_of_cancellation_by_host).returns(stub(deliver: true)).once
+    ReservationSmsNotifier.expects(:notify_guest_with_state_change).returns(stub(deliver: true)).once
+
+    @reservation.confirm # Must be confirmed before can be cancelled
+
+    @tracker.expects(:cancelled_a_booking).with do |reservation, custom_options|
+      reservation == assigns(:reservation) && custom_options == { actor: 'host' }
+    end
+    @tracker.expects(:updated_profile_information).with do |user|
+      user == assigns(:reservation).owner
+    end
+    @tracker.expects(:updated_profile_information).with do |user|
+      user == assigns(:reservation).host
+    end
+    post :host_cancel, { listing_id: @reservation.listing.id, id: @reservation.id }
+    assert_redirected_to dashboard_guests_path
+  end
+
+  should "refund booking on cancel" do
     ActiveMerchant::Billing::Base.mode = :test
+    gateway.authorize(@reservation.total_amount_cents, credit_card)
+    @reservation.confirm
+
+    sign_in @reservation.listing.creator
+    User.any_instance.stubs(:accepts_sms_with_type?)
+
+    setup_refund_for_reservation(@reservation)
+
+    assert_difference 'Refund.count' do
+      post :host_cancel, { listing_id: @reservation.listing.id, id: @reservation.id }
+    end
+
+    assert_redirected_to dashboard_guests_path
+    assert_equal 'refunded', @reservation.reload.payment_status
   end
 
-  should "track booking review open" do
-    @tracker.expects(:reviewed_a_booking).with do |reservation|
-      reservation == assigns(:reservation_request).reservation.decorate
+  context 'PUT #reject' do
+    should 'set rejection reason' do
+      ReservationMailer.expects(:notify_guest_of_rejection).returns(stub(deliver: true)).once
+      ReservationIssueLogger.expects(:rejected_with_reason).with(@reservation, @user)
+      ReservationSmsNotifier.expects(:notify_guest_with_state_change).returns(stub(deliver: true)).once
+      put :reject, { listing_id: @reservation.listing.id, id: @reservation.id, reservation: { rejection_reason: 'Dont like him' } }
+      assert_equal @reservation.reload.rejection_reason, 'Dont like him'
     end
-    post :review, booking_params_for(@listing)
-    assert_response 200
-  end
-
-  context 'cancellation policy' do
-
-    setup do
-      TransactableType.update_all({
-        cancellation_policy_enabled: Time.zone.now,
-        cancellation_policy_hours_for_cancellation: 24,
-        cancellation_policy_penalty_percentage: 60
-      })
-    end
-
-    should 'store cancellation policy details if enabled' do
-      post :create, booking_params_for(@listing)
-      reservation = assigns(:reservation_request).reservation.reload
-      assert_equal 24, reservation.cancellation_policy_hours_for_cancellation
-      assert_equal 60, reservation.cancellation_policy_penalty_percentage
-    end
-
-    should 'not store cancellation policy details if disabled' do
-      TransactableType.update_all(cancellation_policy_enabled: nil)
-      post :create, booking_params_for(@listing)
-      reservation = assigns(:reservation_request).reservation.reload
-      assert_equal 0, reservation.cancellation_policy_hours_for_cancellation
-      assert_equal 0, reservation.cancellation_policy_penalty_percentage
-    end
-  end
-
-  should "track booking request" do
-    ReservationMailer.expects(:notify_host_with_confirmation).returns(stub(deliver: true)).once
-    ReservationMailer.expects(:notify_guest_with_confirmation).returns(stub(deliver: true)).once
-
-    @tracker.expects(:requested_a_booking).with do |reservation|
-      reservation == assigns(:reservation_request).reservation
-    end
-    @tracker.expects(:updated_profile_information).with do |user|
-      user == assigns(:reservation_request).reservation.owner
-    end
-    @tracker.expects(:updated_profile_information).with do |user|
-      user == assigns(:reservation_request).reservation.host
-    end
-
-    assert_difference 'Reservation.count' do
-      post :create, booking_params_for(@listing)
-    end
-
-    assert_response :redirect
-  end
-
-  context 'schedule expiry' do
-
-    should 'create a delayed_job task to run in 24 hours time when saved' do
-      Timecop.freeze(Time.zone.now) do
-        ReservationExpiryJob.expects(:perform_later).with do |time, id|
-          time == 24.hours.from_now
-        end
-        post :create, booking_params_for(@listing)
-      end
-    end
-
-  end
-
-  context "#twilio" do
-
-    context 'sending sms fails' do
-
-      should 'raise invalid phone number exception if message indicates so' do
-        ReservationMailer.expects(:notify_host_with_confirmation).returns(stub(deliver: true)).once
-        ReservationMailer.expects(:notify_guest_with_confirmation).returns(stub(deliver: true)).once
-
-        Rails.logger.expects(:error).never
-        User.any_instance.expects(:notify_about_wrong_phone_number).once
-        SmsNotifier::Message.any_instance.stubs(:send_twilio_message).raises(Twilio::REST::RequestError, "The 'To' number +16665554444 is not a valid phone number")
-        assert_nothing_raised do
-          post :create, booking_params_for(@listing)
-        end
-        assert @response.body.include?('redirect')
-        assert_redirected_to booking_successful_dashboard_user_reservation_path(Reservation.last)
-      end
-
-      should 'log twilio exceptions that have unknown message' do
-        ReservationMailer.expects(:notify_host_with_confirmation).returns(stub(deliver: true)).once
-        ReservationMailer.expects(:notify_guest_with_confirmation).returns(stub(deliver: true)).once
-
-        @controller.class.any_instance.expects(:handle_invalid_mobile_number).never
-        SmsNotifier::Message.any_instance.stubs(:send_twilio_message).raises(Twilio::REST::RequestError, "Some other error")
-        Rails.logger.expects(:error).once
-        assert_nothing_raised do
-          post :create, booking_params_for(@listing)
-        end
-        assert @response.body.include?('redirect')
-        assert_redirected_to booking_successful_dashboard_user_reservation_path(Reservation.last)
-      end
-
-    end
-
   end
 
   context 'versions' do
 
-    should 'store new version after creating reservation' do
-      assert_difference('PaperTrail::Version.where("item_type = ? AND event = ?", "Reservation", "create").count') do
+    should 'store new version after confirm' do
+      # 2 because attempt charge is triggered, which if successful generates second version
+      assert_difference('PaperTrail::Version.where("item_type = ? AND event = ?", "Reservation", "update").count', 2) do
         with_versioning do
-          post :create, booking_params_for(@listing)
+          post :confirm, { listing_id: @reservation.listing.id, id: @reservation.id }
+        end
+      end
+    end
+
+    should 'store new version after reject' do
+      assert_difference('PaperTrail::Version.where("item_type = ? AND event = ?", "Reservation", "update").count') do
+        with_versioning do
+          put :reject, { listing_id: @reservation.listing.id, id: @reservation.id, reservation: { rejection_reason: 'Dont like him' } }
+        end
+      end
+    end
+
+    should 'store new version after cancel' do
+      @reservation.confirm
+      assert_difference('PaperTrail::Version.where("item_type = ? AND event = ?", "Reservation", "update").count') do
+        with_versioning do
+          post :host_cancel, { listing_id: @reservation.listing.id, id: @reservation.id }
         end
       end
     end
 
   end
 
-  private
-
-  def booking_params_for(listing)
-    {
-      listing_id: listing.id,
-      reservation_request: {
-        dates: [Chronic.parse('Monday')],
-        quantity: "1",
-        card_number: 4242424242424242,
-        card_expires: "05/2020",
-        card_code: "411"
-      }
-    }
+  def credit_card
+    ActiveMerchant::Billing::CreditCard.new(
+      :number             => "4242424242424242",
+      :month              => "12",
+      :year               => "2020",
+      :verification_value => "411"
+    )
   end
 
-  def object_hash_for(reservation)
-    {
-      booking_desks: reservation.quantity,
-      booking_days: reservation.total_days,
-      booking_total: reservation.total_amount_dollars,
-      location_address: reservation.location.address,
-      location_currency: reservation.location.currency,
-      location_suburb: reservation.location.suburb,
-      location_city: reservation.location.city,
-      location_state: reservation.location.state,
-      location_country: reservation.location.country,
-      location_postcode: reservation.location.postcode
-    }
+  def gateway
+    Billing::Gateway::Processor::Incoming::ProcessorFactory.create(@user, @user.instance, "USD", 'US')
   end
 
+  def setup_refund_for_reservation(reservation)
+    reservation.reservation_charges.last.charge_attempts.successful.create(amount: reservation.total_amount_cents)
+    Billing::Gateway::Processor::Incoming::Stripe.any_instance.stubs(:refund_identification)
+      .returns({id: "123"}.to_json)
+    ActiveMerchant::Billing::StripeGateway.any_instance.stubs(:refund)
+      .returns(ActiveMerchant::Billing::BogusGateway.new.refund(reservation.total_amount_cents, "123"))
+  end
 end
+
---
+++ test/functional/listings/reservations_controller_test.rb
@@ -0,0 +1,165 @@
+require 'test_helper'
+
+class Listings::ReservationsControllerTest < ActionController::TestCase
+
+  setup do
+    @listing = FactoryGirl.create(:listing_in_san_francisco)
+
+    @user = FactoryGirl.create(:user, name: "Example LastName")
+    sign_in @user
+    stub_mixpanel
+    stub_request(:post, "https://www.googleapis.com/urlshortener/v1/url")
+
+    stub_billing_gateway(@listing.instance)
+    stub_active_merchant_interaction
+
+    ActiveMerchant::Billing::Base.mode = :test
+  end
+
+  should "track booking review open" do
+    @tracker.expects(:reviewed_a_booking).with do |reservation|
+      reservation == assigns(:reservation_request).reservation.decorate
+    end
+    post :review, booking_params_for(@listing)
+    assert_response 200
+  end
+
+  context 'cancellation policy' do
+
+    setup do
+      TransactableType.update_all({
+        cancellation_policy_enabled: Time.zone.now,
+        cancellation_policy_hours_for_cancellation: 24,
+        cancellation_policy_penalty_percentage: 60
+      })
+    end
+
+    should 'store cancellation policy details if enabled' do
+      post :create, booking_params_for(@listing)
+      reservation = assigns(:reservation_request).reservation.reload
+      assert_equal 24, reservation.cancellation_policy_hours_for_cancellation
+      assert_equal 60, reservation.cancellation_policy_penalty_percentage
+    end
+
+    should 'not store cancellation policy details if disabled' do
+      TransactableType.update_all(cancellation_policy_enabled: nil)
+      post :create, booking_params_for(@listing)
+      reservation = assigns(:reservation_request).reservation.reload
+      assert_equal 0, reservation.cancellation_policy_hours_for_cancellation
+      assert_equal 0, reservation.cancellation_policy_penalty_percentage
+    end
+  end
+
+  should "track booking request" do
+    ReservationMailer.expects(:notify_host_with_confirmation).returns(stub(deliver: true)).once
+    ReservationMailer.expects(:notify_guest_with_confirmation).returns(stub(deliver: true)).once
+
+    @tracker.expects(:requested_a_booking).with do |reservation|
+      reservation == assigns(:reservation_request).reservation
+    end
+    @tracker.expects(:updated_profile_information).with do |user|
+      user == assigns(:reservation_request).reservation.owner
+    end
+    @tracker.expects(:updated_profile_information).with do |user|
+      user == assigns(:reservation_request).reservation.host
+    end
+
+    assert_difference 'Reservation.count' do
+      post :create, booking_params_for(@listing)
+    end
+
+    assert_response :redirect
+  end
+
+  context 'schedule expiry' do
+
+    should 'create a delayed_job task to run in 24 hours time when saved' do
+      Timecop.freeze(Time.zone.now) do
+        ReservationExpiryJob.expects(:perform_later).with do |time, id|
+          time == 24.hours.from_now
+        end
+        post :create, booking_params_for(@listing)
+      end
+    end
+
+  end
+
+  context "#twilio" do
+
+    context 'sending sms fails' do
+
+      should 'raise invalid phone number exception if message indicates so' do
+        ReservationMailer.expects(:notify_host_with_confirmation).returns(stub(deliver: true)).once
+        ReservationMailer.expects(:notify_guest_with_confirmation).returns(stub(deliver: true)).once
+
+        Rails.logger.expects(:error).never
+        User.any_instance.expects(:notify_about_wrong_phone_number).once
+        SmsNotifier::Message.any_instance.stubs(:send_twilio_message).raises(Twilio::REST::RequestError, "The 'To' number +16665554444 is not a valid phone number")
+        assert_nothing_raised do
+          post :create, booking_params_for(@listing)
+        end
+        assert @response.body.include?('redirect')
+        assert_redirected_to booking_successful_dashboard_user_reservation_path(Reservation.last)
+      end
+
+      should 'log twilio exceptions that have unknown message' do
+        ReservationMailer.expects(:notify_host_with_confirmation).returns(stub(deliver: true)).once
+        ReservationMailer.expects(:notify_guest_with_confirmation).returns(stub(deliver: true)).once
+
+        @controller.class.any_instance.expects(:handle_invalid_mobile_number).never
+        SmsNotifier::Message.any_instance.stubs(:send_twilio_message).raises(Twilio::REST::RequestError, "Some other error")
+        Rails.logger.expects(:error).once
+        assert_nothing_raised do
+          post :create, booking_params_for(@listing)
+        end
+        assert @response.body.include?('redirect')
+        assert_redirected_to booking_successful_dashboard_user_reservation_path(Reservation.last)
+      end
+
+    end
+
+  end
+
+  context 'versions' do
+
+    should 'store new version after creating reservation' do
+      assert_difference('PaperTrail::Version.where("item_type = ? AND event = ?", "Reservation", "create").count') do
+        with_versioning do
+          post :create, booking_params_for(@listing)
+        end
+      end
+    end
+
+  end
+
+  private
+
+  def booking_params_for(listing)
+    {
+      listing_id: listing.id,
+      reservation_request: {
+        dates: [Chronic.parse('Monday')],
+        quantity: "1",
+        card_number: 4242424242424242,
+        card_expires: "05/2020",
+        card_code: "411"
+      }
+    }
+  end
+
+  def object_hash_for(reservation)
+    {
+      booking_desks: reservation.quantity,
+      booking_days: reservation.total_days,
+      booking_total: reservation.total_amount_dollars,
+      location_address: reservation.location.address,
+      location_currency: reservation.location.currency,
+      location_suburb: reservation.location.suburb,
+      location_city: reservation.location.city,
+      location_state: reservation.location.state,
+      location_country: reservation.location.country,
+      location_postcode: reservation.location.postcode
+    }
+  end
+
+end
