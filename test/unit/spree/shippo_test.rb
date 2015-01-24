a589b22aad03de5e8646b0c6c6fe0b49bf5c65b0
Andrei Canciu <andrei@canciu.ro>
2015-01-25 10:35:54 +1100

    NM-1074-shippo
    * fixed tests after latest changes

--- test/unit/spree/shippo_test.rb
+++ test/unit/spree/shippo_test.rb
@@ -16,16 +16,38 @@ class ShippoTest < ActionView::TestCase
     result
   end
 
-  def setup_stubs_for_shippo_order_testing(get_rates = true, clear_categories = false)
+  def setup_stubs_for_shippo_order_testing(get_rates = true, no_categories = false, shippo_enabled = true)
     shippo_mock_rates = [
       mock.stubs('provider' => 'USPS', 'servicelevel_name' => 'Library Mail', 'amount' => 2.56, 'object_id' => 'ab47a600b2114eb4899cb698731b97ff'),
       mock.stubs('provider' => 'USPS', 'servicelevel_name' => 'First-Class Package/Mail Parcel', 'amount' => 4.58, 'object_id' => 'adf6939b914a46f1b72882935b7244be')
     ]
 
+    if shippo_enabled
+      Spree::Product.any_instance.expects('shippo_enabled?').at_least(0).returns(true)
+      Spree::Product.any_instance.expects('shippo_enabled').at_least(0).returns(true)
+    else
+      Spree::Product.any_instance.expects('shippo_enabled?').at_least(0).returns(false)
+      Spree::Product.any_instance.expects('shippo_enabled').at_least(0).returns(false)
+    end
+
+    Spree::Product.any_instance.expects('user').at_least(0).returns(User.new)
+
+    User.any_instance.expects('name').at_least(0).returns('user name')
+    User.any_instance.expects('phone').at_least(0).returns('1231231234')
+    User.any_instance.expects('email').at_least(0).returns('user@company.com')
+    User.any_instance.expects('companies').at_least(0).returns([Company.new])
+
+    Company.any_instance.expects('name').at_least(0).returns('company name')
+    Company.any_instance.expects('street').at_least(0).returns('1 Microsoft Way')
+    Company.any_instance.expects('city').at_least(0).returns('Redmond')
+    Company.any_instance.expects('state_code').at_least(0).returns('WA')
+    Company.any_instance.expects('postcode').at_least(0).returns('98052')
+    Company.any_instance.expects('iso_country_code').at_least(0).returns('US')
+
     if get_rates
       shippo_api_mock = mock
       ignored_params = [:street2, :email, :street_no]
-      shippo_api_mock.expects(:get_rates).times(1).with do |address_from_info, address_to_info, parcel_info, customs_item_info, customs_declaration_info|
+      shippo_api_mock.expects(:get_rates).at_least(1).with do |address_from_info, address_to_info, parcel_info, customs_item_info, customs_declaration_info|
         check_all_params_not_blank(ShippoApi::ShippoAddressInfo::REQUIRED_PARAMS.reject { |p| ignored_params.include?(p) }, address_from_info) &&
         check_all_params_not_blank(ShippoApi::ShippoAddressInfo::REQUIRED_PARAMS.reject { |p| ignored_params.include?(p) }, address_to_info) &&
         check_all_params_not_blank(ShippoApi::ShippoParcelInfo::REQUIRED_PARAMS, parcel_info)
@@ -37,7 +59,7 @@ class ShippoTest < ActionView::TestCase
 
     order.line_items.map(&:variant).map(&:shipping_category).uniq.map(&:shipping_methods).flatten.compact.each { |shipping_method| shipping_method.destroy }
 
-    if clear_categories
+    if no_categories
       order.line_items.map(&:variant).map(&:shipping_category).uniq.each { |shipping_category| shipping_category.destroy }
     end
 
@@ -69,9 +91,7 @@ class ShippoTest < ActionView::TestCase
     end
 
     should 'create spree shipping methods if shippo enabled' do
-      PlatformContext.current.instance.expects('shippo_enabled?').times(2).returns(true)
-      
-      order = setup_stubs_for_shippo_order_testing
+      order = setup_stubs_for_shippo_order_testing(true, false, true)
 
       assert_equal 2, order.line_items.map(&:variant).map(&:shipping_category).uniq.map(&:shipping_methods).flatten.compact.length
       assert_equal 2, order.line_items.map(&:variant).map(&:shipping_category).uniq.map(&:shipping_methods).flatten.compact.map(&:shipping_rates).flatten.compact.length
@@ -83,9 +103,7 @@ class ShippoTest < ActionView::TestCase
     end
 
     should 'not create spree shipping methods if shippo not enabled' do
-      PlatformContext.current.instance.expects('shippo_enabled?').times(2).returns(false)
-
-      order = setup_stubs_for_shippo_order_testing(false)
+      order = setup_stubs_for_shippo_order_testing(false, false, false)
 
       assert_equal 0, Spree::Shipment.find_by(:order_id => order.id).shipping_methods.length
 
@@ -95,13 +113,11 @@ class ShippoTest < ActionView::TestCase
     end
 
     should 'create parent shipping category if it does not exist' do
-      PlatformContext.current.instance.expects('shippo_enabled?').times(2).returns(true)
-      
-      order = setup_stubs_for_shippo_order_testing(true, true)
+      order = setup_stubs_for_shippo_order_testing(true, true, true)
 
       order.line_items.map(&:variant).each { |v| v.reload }
 
-      shipping_categories = order.line_items.map(&:variant).map(&:shipping_category).uniq
+      shipping_categories = order.line_items.map(&:variant).map(&:shipping_category).compact.uniq
       assert_operator shipping_categories.length, :>, 0
       shipping_categories.each do |shipping_category|
         assert_match /^Shippo Auto-created .+$/, shipping_category.name
