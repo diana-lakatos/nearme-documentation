a86d33defa3df3fc360de6830c2730b82ff57d9d
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-02-03 10:27:13 +0100

    re-add config which might be not necessary

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,6 +10,7 @@ DesksnearMe::Application.configure do
   config.consider_all_requests_local       = true
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
+  config.assets.compile = true
 
   config.perform_social_jobs = false
   config.after_initialize do
