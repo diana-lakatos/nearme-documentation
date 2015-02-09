e5230c8ea06b7901427f48fea4cd2f01e44d6338
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2015-02-09 15:28:03 -0800

    NM-1303 Make shipping dimension units manditory.

--- test/unit/forms/product_form_test.rb
+++ test/unit/forms/product_form_test.rb
@@ -10,23 +10,6 @@ class ProductFormTest < ActiveSupport::TestCase
   end
 
   context "boarding form shipping testing" do
-    should "save shipping dimensions/weight to master variant" do
-      @product_form = ProductForm.new(@product, {})
-      @product_form.weight = 101
-      @product_form.height = 103
-      @product_form.width = 102
-      @product_form.depth = 104
-      @product_form.shipping_methods_attributes = {"0"=>{"name"=>"sdsadasad", "removed"=>"0", "processing_time"=>"0", "calculator_attributes"=>{"preferred_amount"=>"0.00", "id"=>"24"}, "zones_attributes"=>{"0"=>{"name"=>"Default - 7c9a98679439b6f3f6966f2246d1fe13", "kind"=>"state_based", "state_ids"=>"485" }}}}
-      @product.shippo_enabled = true
-      @product_form.unit_of_measure = 'metric'
-      @product_form.save!
-
-      assert_equal 101, @product.master.weight_user
-      assert_equal 103, @product.master.height_user
-      assert_equal 102, @product.master.width_user
-      assert_equal 104, @product.master.depth_user
-    end
-
     should 'convert shipping dimensions (imperial) to other units of measure' do
       @product_form = ProductForm.new(@product, {})
       @product_form.weight = 101
