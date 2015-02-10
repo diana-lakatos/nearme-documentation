881dfe9db726cd000cef8894ddad0b06b1058ba7
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-02-10 15:17:30 +0100

    Handle two variations of emails

--- app/models/support/receive_mails.rb
+++ app/models/support/receive_mails.rb
@@ -3,6 +3,7 @@ class Support::ReceiveMails
     return if PlatformContext.current.instance.support_imap_hash.blank?
     Mailman::Application.run(config) do
       subject("[Support Ticket #%ticket_id%]", Support::TicketMessage)
+      subject("[Ticket Support #%ticket_id%]", Support::TicketMessage)
       default do
         Support::Ticket.new.receive(message, params)
       end
