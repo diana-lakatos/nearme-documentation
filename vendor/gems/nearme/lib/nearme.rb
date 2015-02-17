c333fbe4b2a8f3c9a236f74442a5f229948d5ea0
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2015-02-17 14:45:20 -0800

    NM-1407 nearme opsworks stack info tool.

--- vendor/gems/nearme/lib/nearme.rb
+++ vendor/gems/nearme/lib/nearme.rb
@@ -3,6 +3,7 @@ require "nearme/deploy"
 require "nearme/sync_assets"
 require "nearme/balancer"
 require "nearme/backup"
+require "nearme/info"
 require "nearme/certificate_request_generator"
 
 module NearMe
