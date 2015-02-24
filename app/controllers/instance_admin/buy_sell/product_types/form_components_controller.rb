ca40ccbf52252031254bcbb0f67ff333b9e76a67
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2015-02-23 17:40:44 -0800

    NM-1406 fix tabbing and whitespace on form_components_controller.rb.

--- app/controllers/instance_admin/buy_sell/product_types/form_components_controller.rb
+++ app/controllers/instance_admin/buy_sell/product_types/form_components_controller.rb
@@ -1,16 +1,16 @@
-class InstanceAdmin::BuySell::ProductTypes::FormComponentsController < InstanceAdmin::FormComponentsController 
+class InstanceAdmin::BuySell::ProductTypes::FormComponentsController < InstanceAdmin::FormComponentsController
 
   private
 
-	def find_form_componentable
+  def find_form_componentable
     @form_componentable = Spree::ProductType.find(params[:product_type_id])
   end
 
   def redirect_path
-  	instance_admin_buy_sell_product_type_form_components_path(@form_componentable)
-	end
+    instance_admin_buy_sell_product_type_form_components_path(@form_componentable)
+  end
 
-	def permitting_controller_class
+  def permitting_controller_class
     @controller_scope ||= 'buy_sell'
   end
 end
