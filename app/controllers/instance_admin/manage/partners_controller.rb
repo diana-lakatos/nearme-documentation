3e058572b2d9d43bd08a2777967ab127d636c295
Michael Mitchell <michael.alexander.mitchell@gmail.com>
2015-02-27 15:15:09 -0800

    NM-1344 fix instance_admin/partners controller update action.

--- app/controllers/instance_admin/manage/partners_controller.rb
+++ app/controllers/instance_admin/manage/partners_controller.rb
@@ -8,6 +8,18 @@ class InstanceAdmin::Manage::PartnersController < InstanceAdmin::Manage::BaseCon
     create! { instance_admin_manage_partners_path }
   end
 
+  def update
+    @partner = Partner.find(params[:id])
+
+    if @partner.update_attributes(partner_params)
+      flash[:success] = t 'flash_messages.instance_admin.manage.partners.partner_updated'
+    else
+      flash[:error] = @partner.errors.full_messages.to_sentence
+    end
+
+    redirect_to edit_instance_admin_manage_partner_url(@partner.id)
+  end
+
   private
 
   def partner_params
