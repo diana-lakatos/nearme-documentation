9f323dbd055a6c2219d59b6c6db2b534a37433e6
Andrei Canciu <andrei@canciu.ro>
2015-03-05 04:28:06 +1100

    NM-1412-surface-vendor-approval-fields
    * changes after review

--- app/services/approval_request_template_creation.rb
+++ app/services/approval_request_template_creation.rb
@@ -19,26 +19,25 @@ class ApprovalRequestTemplateCreation
   private
 
   def get_or_create_form_component(transactable_type)
-    form_component = FormComponent.where(:form_type => FormComponent::SPACE_WIZARD, :form_componentable => transactable_type).detect do |form_component|
-      form_component.name == I18n.t('instance_admin.form_components.approval_requests_section_title')
-    end
+    form_component = FormComponent.where(form_type: FormComponent::SPACE_WIZARD, form_componentable: transactable_type, is_approval_request_surfacing: true).first
 
     if form_component.blank?
       form_component = FormComponent.new
       form_component.name = I18n.t('instance_admin.form_components.approval_requests_section_title')
+      form_component.is_approval_request_surfacing = true
       form_component.form_type = FormComponent::SPACE_WIZARD
       form_component.form_componentable = transactable_type
-      current_max_rank = FormComponent.where(:form_type => FormComponent::SPACE_WIZARD, :form_componentable => transactable_type).maximum(:rank)
+      current_max_rank = FormComponent.where(form_type: FormComponent::SPACE_WIZARD, form_componentable: transactable_type).maximum(:rank)
       next_max_rank = current_max_rank.nil? ? 0 : current_max_rank + 1
       form_component.rank = next_max_rank
-      form_component.save
+      form_component.save!
     end
 
     form_component
   end
 
   def is_already_present(transactable_type)
-    FormComponent.where(:form_type => FormComponent::SPACE_WIZARD, :form_componentable => transactable_type).detect do |form_component|
+    FormComponent.where(form_type: FormComponent::SPACE_WIZARD, form_componentable: transactable_type).detect do |form_component|
       form_component.form_fields.detect do |form_field|
         form_field[@approval_request_template.owner_type.to_s.underscore] == 'approval_requests'
       end.present?
@@ -52,7 +51,7 @@ class ApprovalRequestTemplateCreation
 
         form_component.form_fields << { @approval_request_template.owner_type.to_s.underscore => 'approval_requests' }
 
-        form_component.save
+        form_component.save!
       end
     end
   end
--- test/unit/approval_request_template_creation_test.rb
+++ test/unit/approval_request_template_creation_test.rb
@@ -25,22 +25,16 @@ class ApprovalRequestTemplateCreationTest < ActiveSupport::TestCase
       assert approval_request_template_creation.create
     end
 
-    params = { :form_type => FormComponent::SPACE_WIZARD, :name => I18n.t('instance_admin.form_components.approval_requests_section_title') }
+    params = { form_type: FormComponent::SPACE_WIZARD, name: I18n.t('instance_admin.form_components.approval_requests_section_title') }
 
-    assert_equal 1, FormComponent.where(params.merge({ :form_componentable => @transactable_types[0] })).length
-    all_form_fields = FormComponent.where(params.merge({ :form_componentable => @transactable_types[0] })).first.form_fields 
-    assert_equal 4, all_form_fields.length
-    assert_equal 4, ApprovalRequestTemplate::OWNER_TYPES.collect { |owner_type| all_form_fields.detect { |form_field| form_field[owner_type.to_s.underscore] == 'approval_requests' } }.compact.length
-
-    assert_equal 1, FormComponent.where(params.merge({ :form_componentable => @transactable_types[1] })).length
-    all_form_fields = FormComponent.where(params.merge({ :form_componentable => @transactable_types[1] })).first.form_fields 
-    assert_equal 4, all_form_fields.length
-    assert_equal 4, ApprovalRequestTemplate::OWNER_TYPES.collect { |owner_type| all_form_fields.detect { |form_field| form_field[owner_type.to_s.underscore] == 'approval_requests' } }.compact.length
-
-    assert_equal 1, FormComponent.where(params.merge({ :form_componentable => @transactable_types[2] })).length
-    all_form_fields = FormComponent.where(params.merge({ :form_componentable => @transactable_types[2] })).first.form_fields 
-    assert_equal 4, all_form_fields.length
-    assert_equal 4, ApprovalRequestTemplate::OWNER_TYPES.collect { |owner_type| all_form_fields.detect { |form_field| form_field[owner_type.to_s.underscore] == 'approval_requests' } }.compact.length
+    0.upto(2) do |index|
+      form_components = FormComponent.where(params.merge({ form_componentable: @transactable_types[index] }))
+      assert_equal 1, form_components.length
+      assert_equal true, form_components[0].is_approval_request_surfacing
+      all_form_fields = form_components.first.form_fields 
+      assert_equal 4, all_form_fields.length
+      assert_equal 4, ApprovalRequestTemplate::OWNER_TYPES.collect { |owner_type| all_form_fields.detect { |form_field| form_field[owner_type.to_s.underscore] == 'approval_requests' } }.compact.length
+    end
   end
 
 end
