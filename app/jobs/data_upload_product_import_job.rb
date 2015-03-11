dcabfba57d8c8d5daf0015365a51922fe46ea2aa
Dmitry Afanasyev <dimarzio1986@gmail.com>
2015-03-11 20:35:54 +0300

    Don't accept nil params on products upload

--- app/jobs/data_upload_product_import_job.rb
+++ app/jobs/data_upload_product_import_job.rb
@@ -6,7 +6,7 @@ class DataUploadProductImportJob < Job
 
   def perform
     @data_upload = DataUpload.find(@data_upload_id)
-    @data_upload.import!
+    @data_upload.import! unless @data_upload.importing?
     product_csv = csv_parser_class.new(@data_upload)
     DataImporter::Product::Importer.new(@data_upload, product_csv).import
   end
