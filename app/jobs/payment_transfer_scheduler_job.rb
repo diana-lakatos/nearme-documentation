6ab7721683f5eab206d2a80f570d5a4afb935775
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-03-12 18:16:15 +0100

    add uniq to payment transfer scheduler, just in case (should not be needed though)

--- app/jobs/payment_transfer_scheduler_job.rb
+++ app/jobs/payment_transfer_scheduler_job.rb
@@ -3,7 +3,7 @@
 # PaymentTransfers then need to be paid by the admin and flagged as such.
 class PaymentTransferSchedulerJob < Job
   def perform
-    Company.needs_payment_transfer.find_each do |company|
+    Company.needs_payment_transfer.uniq.find_each do |company|
       PlatformContext.current = PlatformContext.new(company.instance)
       if PaymentTransfers::SchedulerMethods.new(company.instance).generate_payment_transfers_today?
         company.schedule_payment_transfer
