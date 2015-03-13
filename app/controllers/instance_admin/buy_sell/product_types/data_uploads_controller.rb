fc2a6e6d83365957b5d5ac7606ff8c0a0af94f58
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-03-13 09:46:35 +0100

    NM-1520 undefined method permission_buy_sell

--- app/controllers/instance_admin/buy_sell/product_types/data_uploads_controller.rb
+++ app/controllers/instance_admin/buy_sell/product_types/data_uploads_controller.rb
@@ -8,10 +8,12 @@ class InstanceAdmin::BuySell::ProductTypes::DataUploadsController < InstanceAdmi
   end
 
   def permitting_controller_class
-    'buy_sell'
+    @controller_scope ||= 'buy_sell'
+    'buysell'
   end
 
   def find_importable
     @importable = Spree::ProductType.find(params[:product_type_id])
   end
 end
+
