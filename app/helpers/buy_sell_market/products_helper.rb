c2899bcdba616107738a7964cb248942285b3e95
Krzysztof Streflik <streflik@gmail.com>
2015-03-15 23:45:13 +0700

    NM-1528 changes after review

--- app/helpers/buy_sell_market/products_helper.rb
+++ app/helpers/buy_sell_market/products_helper.rb
@@ -19,4 +19,12 @@ module BuySellMarket::ProductsHelper
     end
   end
 
+  def image_for_table(product)
+    if photo = product.images.first
+      link_to product_path(product) do
+        image_tag photo.image.url(:thumb), :title => product.name, :alt => product.name
+      end
+    end
+  end
+
 end
