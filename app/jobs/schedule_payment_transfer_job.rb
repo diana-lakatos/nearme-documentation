ef2258b3b998b0355c26373a733cc796004747b0
Krzysztof Streflik <streflik@gmail.com>
2015-03-16 16:38:39 +0700

    NM-1536 adds error class to balanced rescue

--- app/jobs/schedule_payment_transfer_job.rb
+++ app/jobs/schedule_payment_transfer_job.rb
@@ -2,7 +2,7 @@ class SchedulePaymentTransferJob < Job
 
   def after_initialize(company_id)
     @company = Company.with_deleted.find(company_id)
-    PlatformContext.current = PlatformContext.new(company.instance)
+    PlatformContext.current = PlatformContext.new(@company.instance)
   end
 
   def perform
