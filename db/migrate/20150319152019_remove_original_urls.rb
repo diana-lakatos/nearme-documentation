62b4fb5347c9dc880cea7b230f95a3308c35c84a
Dmitry Afanasyev <dimarzio1986@gmail.com>
2015-03-22 01:17:42 +0300

    NM-1505 filepiker-> jquery-fileupload Keep original urls for a while

--- db/migrate/20150319152019_remove_original_urls.rb
+++
@@ -1,8 +0,0 @@
-class RemoveOriginalUrls < ActiveRecord::Migration
-  def change
-    %w(icon icon_retina favicon logo logo_retina hero).each do |attr|
-      remove_column :themes, "#{attr}_image_original_url"
-    end
-    remove_column :users, :avatar_original_url
-  end
-end
