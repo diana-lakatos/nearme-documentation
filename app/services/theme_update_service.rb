d6a99c6f435f6f775c22e856fc9a5457a3f667a9
Andrei Canciu <andrei@canciu.ro>
2015-03-24 06:18:04 +1100

    NM-1468A-workflows-dont-update-email-appropriately
    * updating all workflows not just support workflows, also only for those
    having had the old theme support email

--- app/services/theme_update_service.rb
+++ app/services/theme_update_service.rb
@@ -5,6 +5,7 @@ class ThemeUpdateService
   end
 
   def update(theme_params)
+    @theme_original_support_email = @theme.support_email
     if @theme.update_attributes(theme_params)
       update_support_email_in_workflows
 
@@ -17,10 +18,8 @@ class ThemeUpdateService
   private
 
   def update_support_email_in_workflows
-    # Maybe not the best in terms of performance but this doesn't happen often
-    # and premature optimization is also evil
-    workflow_alerts = Workflow.where(:workflow_type => 'support').collect(&:workflow_steps).flatten.collect(&:workflow_alerts).flatten
-    workflow_alerts.each { |workflow_alert| workflow_alert.update_attributes(:from => @theme.support_email, :reply_to => @theme.support_email) }
+    WorkflowAlert.where(from: @theme_original_support_email).update_all(from: @theme.support_email)
+    WorkflowAlert.where(reply_to: @theme_original_support_email).update_all(reply_to: @theme.support_email)
   end
 
 end
