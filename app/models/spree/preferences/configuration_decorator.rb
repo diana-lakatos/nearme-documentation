310d61a92d3ec6140c2af97bbc391ceefcb3a956
Tomasz Lemkowski <lemkowski@gmail.com>
2015-03-27 19:06:21 +0100

    NM-1609 fix spree instance scope

--- app/models/spree/preferences/configuration_decorator.rb
+++
@@ -1,5 +0,0 @@
-Spree::Preferences::Configuration.class_eval do
-  def preference_cache_key(name)
-    [rails_cache_id, PlatformContext.current.try(:instance).try(:id), self.class.name, name].compact.join('::').underscore
-  end
-end
