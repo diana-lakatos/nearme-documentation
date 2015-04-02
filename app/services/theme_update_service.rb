de35e499cce5ba9dede96913f611cea90540b542
Andrei Canciu <andrei@canciu.ro>
2015-04-03 07:28:59 +1200

    NM-1632-workflows-emails-updating
    * removed check preventing workflow alerts having a different original
    address from being updated

--- app/services/theme_update_service.rb
+++ app/services/theme_update_service.rb
@@ -5,7 +5,6 @@ class ThemeUpdateService
   end
 
   def update(theme_params)
-    @theme_original_support_email = @theme.support_email
     if @theme.update_attributes(theme_params)
       update_support_email_in_workflows
 
@@ -18,8 +17,8 @@ class ThemeUpdateService
   private
 
   def update_support_email_in_workflows
-    WorkflowAlert.where(from: @theme_original_support_email).update_all(from: @theme.support_email)
-    WorkflowAlert.where(reply_to: @theme_original_support_email).update_all(reply_to: @theme.support_email)
+    WorkflowAlert.update_all(from: @theme.support_email)
+    WorkflowAlert.update_all(reply_to: @theme.support_email)
   end
 
 end
