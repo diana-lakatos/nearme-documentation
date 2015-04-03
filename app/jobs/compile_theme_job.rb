4a5d113246b7399c38bc011da3cff8e12b3bb2c2
leafmind <leafmind@gmail.com>
2015-04-03 13:18:37 +0400

    fix ci

--- app/jobs/compile_theme_job.rb
+++ app/jobs/compile_theme_job.rb
@@ -1,16 +1,13 @@
 # Job that handles precompiling and storing the theme stylesheet for a given
 # owner.
 class CompileThemeJob < Job
-  require 'benchmark'
 
   def after_initialize(theme)
     @theme = theme
   end
 
   def perform
-  	Benchmark.bm do |bm|
-      bm.report('Theme::Compiler'){Theme::Compiler.new(@theme).generate_and_update_assets}
-    end
+    bm.report('Theme::Compiler'){Theme::Compiler.new(@theme).generate_and_update_assets}
   end
 end
 
--- features/support/photo_helpers.rb
+++ features/support/photo_helpers.rb
@@ -5,7 +5,7 @@ module PhotoHelpers
   end
 
   def attach_file_via_uploader
-    page.execute_script("$('input.browse-file').show().removeClass('hidden').css({width: 200, height: 50})")
+    page.execute_script("$('input.browse-file').css({display: 'block', visibility: 'visible', width: 200, height: 50})")
     attach_file(find('.browse-file')[:name], File.join(Rails.root, 'test', 'assets', 'foobear.jpeg'))
     using_wait_time 5 do
       page.should_not have_css('.photo-item .loading-icon')
