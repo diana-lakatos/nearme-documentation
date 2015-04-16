62376c4bf3a8c3708309b43d4fb4293e22881405
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-04-16 11:50:06 +0200

    NM-1417 Make CI much more stable
    * fix randmomly failing tests due to I18n
    * try to fix cucumbers failing due to uploading photo via ajax

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,9 +1,8 @@
-require Rails.root.join('lib/i18n/backend/dnm_key_value')
-
 if ActiveRecord::Base.connection.table_exists? Translation.table_name
   I18N_DNM_BACKEND = I18n::Backend::DNMKeyValue.new(Rails.cache)
   I18n.backend = I18n::Backend::Chain.new(I18N_DNM_BACKEND, I18n::Backend::Simple.new)
 else
-  Rails.logger.warn "translations table does not exist, we can't use I18N_DNM_BACKEND"
+  message = "translations table does not exist, we can't use I18N_DNM_BACKEND"
+  Rails.env.test? ? raise(message) : Rails.logger.warn(message)
   I18n.backend = I18n::Backend::Simple.new
 end
--- features/support/photo_helpers.rb
+++ features/support/photo_helpers.rb
@@ -7,10 +7,9 @@ module PhotoHelpers
   def attach_file_via_uploader
     page.execute_script("$('input.browse-file').css({display: 'block', visibility: 'visible', width: 200, height: 50})")
     attach_file(find('.browse-file')[:name], File.join(Rails.root, 'test', 'assets', 'foobear.jpeg'))
-    using_wait_time 5 do
-      page.should_not have_css('.photo-item .loading-icon')
-      page.should have_css('.photo-item img')
-    end
+    wait_for_ajax
+    page.should_not have_css('.photo-item .loading-icon')
+    page.should have_css('.photo-item img')
   end
 end
 
