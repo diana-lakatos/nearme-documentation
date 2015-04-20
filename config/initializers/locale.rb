0762bd9d0ef2693ba729fa5e42dfe39873c8f26f
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-04-20 18:47:20 +0200

    re-add require of dnm key value class

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,3 +1,4 @@
+require Rails.root.join('lib/i18n/backend/dnm_key_value')
 if ActiveRecord::Base.connection.table_exists? Translation.table_name
   I18N_DNM_BACKEND = I18n::Backend::DNMKeyValue.new(Rails.cache)
   I18n.backend = I18n::Backend::Chain.new(I18N_DNM_BACKEND, I18n::Backend::Simple.new)
