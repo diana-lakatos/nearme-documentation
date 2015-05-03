ffe4aa5e79d47564d98123e9142f9e69f640272e
Tomasz Lemkowski <lemkowski@gmail.com>
2015-05-03 21:56:20 +0200

    NM-1720a TransactableType to ServiceType

--- app/controllers/instance_admin/manage/transactable_types/custom_attributes_controller.rb
+++ app/controllers/instance_admin/manage/transactable_types/custom_attributes_controller.rb
@@ -1,5 +1,7 @@
 class InstanceAdmin::Manage::TransactableTypes::CustomAttributesController < InstanceAdmin::Manage::CustomAttributesController
 
+  before_filter :set_breadcrumbs_title
+
   protected
 
   def redirection_path
@@ -8,5 +10,9 @@ class InstanceAdmin::Manage::TransactableTypes::CustomAttributesController < Ins
 
   def find_target
     @target = TransactableType.find(params[:transactable_type_id])
+  end
+
+  def set_breadcrumbs_title
+    @breadcrumbs_title = 'Service Types'
   end
 end
