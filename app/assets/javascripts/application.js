2afe08f8ff027c66ece45918b4c03c362d8cb07b
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-05-10 23:21:46 +0200

    NM-1764 schedule css fixes for space wizard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -103,7 +103,7 @@ window.DNM = {
   },
 
   initializeCustomSelects: function(container){
-    container.find('select').not('.select2, .time-wrapper select, .custom-select, .recurring_select, .ordinary-select').customSelect();
+    container.find('select').not('.select2, .time-wrapper select, .custom-select, .recurring_select, .ordinary-select, .selectpicker').customSelect();
     container.find('.customSelect').not('.checkout-select, .no-icon').append('<i class="custom-select-dropdown-icon ico-chevron-down"></i>').closest('.controls').css({'position': 'relative'});
     container.find('.customSelect').siblings('select').css({'margin': '0px', 'z-index': 1 });
 
