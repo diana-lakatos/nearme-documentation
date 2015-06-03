b37aac14004aa048ffcaf6a2acb0c7dd77b67191
Evgeniy <leafmind@gmail.com>
2015-06-03 12:36:06 +0400

    fix ES initializer with deep_symbolize_keys

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,12 +1,12 @@
 config = {
-  'host' => "http://localhost:9200/",
-  'transport_options' => {
-    'request' => { 'timeout' => 5 }
+  host: "http://localhost:9200/",
+  transport_options: {
+    request: { timeout: 5 }
   }
 }
 
 if File.exists?("config/elasticsearch.yml")
-  elastic_config = YAML.load_file("config/elasticsearch.yml").symbolize_keys
+  elastic_config = YAML.load_file("config/elasticsearch.yml").deep_symbolize_keys
   config.merge!(elastic_config[Rails.env.to_sym])
 end
 
