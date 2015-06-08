c7a6700ee49d04a19bc5b30e8fc98ca8289981b5
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-06-08 13:00:09 +0200

    NM-1927A undefined method gsub

--- lib/reservation_ics_builder.rb
+++ lib/reservation_ics_builder.rb
@@ -31,7 +31,7 @@ class ReservationIcsBuilder
           event.dtend = period.date.strftime("%Y%m%dT") + "#{"%02d" % hour}#{"%02d" % minute}00"
           event.created = @reservation.created_at
           event.last_modified = @reservation.updated_at
-          event.location = @reservation.listing.address
+          event.location = @reservation.listing.address || ''
           event.url = Rails.application.routes.url_helpers.dashboard_user_reservations_url(id: @reservation.id, host: PlatformContext.current.instance.default_domain.name)
         end
       end
