9c8b99e89861a0ef7ec608c40faf4c0867ca8ca3
Andrei Canciu <andrei@canciu.ro>
2015-06-10 06:41:59 +1200

    NM-1948-autocomplete-for-categories
    * added autocomplete for categories

--- app/controllers/dashboard/api/categories_controller.rb
+++ app/controllers/dashboard/api/categories_controller.rb
@@ -1,5 +1,7 @@
 class Dashboard::Api::CategoriesController < Dashboard::Api::BaseController
 
+  skip_before_filter :authenticate_user!, only: [:show]
+
   def show
     category = Category.find(params[:id])
     categories = category.descendants.where(deleted_at: nil).select(:id, :name, :lft, :rgt).order(:permalink)
