c403caa2693ca3e2334e63f8db29bbb2d8f61c71
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-07-01 09:40:37 +0200

    NM-2099 missing partial error in user reviews

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -6,7 +6,7 @@ class UserReviewsController < ApplicationController
     @rating_questions = service.rating_questions_by_role
 
     render json: {
-      template: render_to_string(partial: 'registrations/profile/tabs/reviews_content', locals: {reviews: @reviews, rating_questions: @rating_questions}, layout: false),
+      template: render_to_string(partial: 'registrations/profile/tabs/reviews_content', formats: [:html], locals: {reviews: @reviews, rating_questions: @rating_questions}, layout: false),
       count: I18n.t('user_profile.labels.tabs.reviews', count: @reviews.count)
     }
   end
