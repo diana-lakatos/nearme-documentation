828ed9f6c7ee29b937f1b050a55880f2e9004c89
Dmitry Afanasyev <dimarzio1986@gmail.com>
2015-07-12 12:18:53 +0300

    NM-2017: Rails 4.1.12

--- app/jobs/shippo_purchase_rate_job.rb
+++ app/jobs/shippo_purchase_rate_job.rb
@@ -9,9 +9,9 @@ class ShippoPurchaseRateJob < Job
     purchase_result = ShippoExtensions::SpreeExtensions.purchase_shippo_quoted_shipping_rate(@shipping_method.shippo_rate_id)
 
     if purchase_result.present?
-      @shipping_method.order.update_attribute(:shippo_rate_purchased_at, Time.now)
-      @shipping_method.update_attribute(:shippo_label_url, purchase_result.label_url)
-      @shipping_method.update_attribute(:shippo_tracking_number, purchase_result.tracking_number)
+      @shipping_method.order.update_column(:shippo_rate_purchased_at, Time.now)
+      @shipping_method.update_column(:shippo_label_url, purchase_result.label_url)
+      @shipping_method.update_column(:shippo_tracking_number, purchase_result.tracking_number)
     else
       # This is supposed to make delayed job retry the job
       raise ShippoExtensions::ShippoApiMethodCallingError.new 'Error purchasing rate from Shippo.'
