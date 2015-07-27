7d13706155d01126a4fd1e48c46232f01776d9f4
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-07-28 00:47:52 +0200

    NM-2098 proper fix for seller reviews count

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -2,12 +2,14 @@ class UserReviewsController < ApplicationController
   def reviews_collections
     @user = User.find(params[:id])
     service = UserReviewsService.new(@user, platform_context, params)
-    @reviews = service.reviews_by_role.paginate(page: params[:reviews_page], per_page: 8)
+    @reviews = service.reviews_by_role
     @rating_questions = service.rating_questions_by_role
+    @total_reviews = @reviews.length
+    @reviews = @reviews.paginate(page: params[:reviews_page], per_page: 8, total_entries: @total_reviews)
 
     render json: {
       template: render_to_string(partial: 'registrations/profile/tabs/reviews_content', formats: [:html], locals: {reviews: @reviews, rating_questions: @rating_questions}, layout: false),
-      count: I18n.t('user_profile.labels.tabs.reviews', count: @reviews.count)
+      count: I18n.t('user_profile.labels.tabs.reviews', count: @total_reviews)
     }
   end
 end
