7a0225ecb6ae7d67a130b33a79b0b8e795ffda6b
Andrei Canciu <andrei@canciu.ro>
2015-07-29 01:27:11 +1200

    NM-2361-add-status-to-listing-and-users-reports
    * added deleted_at to users (with_deleted) to users reports
    and transactable_deleted_at to sales report

--- app/controllers/instance_admin/analytics/profiles_controller.rb
+++ app/controllers/instance_admin/analytics/profiles_controller.rb
@@ -1,12 +1,12 @@
 class InstanceAdmin::Analytics::ProfilesController < InstanceAdmin::Analytics::BaseController
 
   def show
-    users = User.pluck('users.id, users.email, users.name,
+    users = User.with_deleted.pluck('users.id, users.email, users.name,
       users.reservations_count, users.transactables_count, users.mobile_number,
-      users.current_location, users.properties')
+      users.current_location, users.properties, users.deleted_at')
 
     csv = CSV.generate do |csv|
-      csv.add_row %w(id email name bookings_count listings_count phone current_location user_properties)
+      csv.add_row %w(id email name bookings_count listings_count phone current_location user_properties deleted_at)
       users.each do |profile|
         csv.add_row profile
       end
