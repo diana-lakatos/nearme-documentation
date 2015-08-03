f1c2c581eb6021cd85bdfcc66dad72a1e2e0ee60
Krzysztof Streflik <streflik@gmail.com>
2015-08-03 21:30:52 +0200

    NM-2253A revert pubsub

--- app/subscribers/cache_expirations.rb
+++ app/subscribers/cache_expirations.rb
@@ -1,6 +1,6 @@
 # Listens on channel about info to expire certain cache part
-NearMeMessageBus.subscribe('/cache_expiration') do |msg|
-  return unless PlatformContext.current
-  CacheExpiration.handle_cache_expiration(msg)
-end
+# NearMeMessageBus.subscribe('/cache_expiration') do |msg|
+#   return unless PlatformContext.current
+#   CacheExpiration.handle_cache_expiration(msg)
+# end
 
