e4acbb92b0d623d67ae356e103f334dbefdb0dcc
Diego Ballona <root@dballona.com>
2015-08-06 16:48:38 -0300

    NM-2333 - Robots for subdomains

--- app/services/robots_txt_service.rb
+++ app/services/robots_txt_service.rb
@@ -1,4 +1,6 @@
 module RobotsTxtService
+  NEARME_SUBDOMAIN_REGEX = /^(?!www).*\.near-me.com$/
+
   module_function
   
   def content_for(domain)
@@ -9,20 +11,29 @@ module RobotsTxtService
       robots_txt = []
       
       robots_txt << "User-Agent: *"
-      robots_txt << "Disallow: /locations/*/host"
-      robots_txt << "Disallow: /locations/*/networking"
-      robots_txt << "Disallow: /locations/populate_address_components"
-      robots_txt << "Disallow: /listings/*/reservations"
-      robots_txt << "Disallow: /auth"
-      robots_txt << "Disallow: /search/show"
-      robots_txt << "Disallow: /authentications"
-      robots_txt << "Disallow: /v1"
-      robots_txt << "Disallow: /widgets"
-      robots_txt << "Sitemap: #{sitemap_url}"
+      
+      if nearme_subdomain?(domain)
+        robots_txt << "Disallow: /"
+      else
+        robots_txt << "Disallow: /locations/*/host"
+        robots_txt << "Disallow: /locations/*/networking"
+        robots_txt << "Disallow: /locations/populate_address_components"
+        robots_txt << "Disallow: /listings/*/reservations"
+        robots_txt << "Disallow: /auth"
+        robots_txt << "Disallow: /search/show"
+        robots_txt << "Disallow: /authentications"
+        robots_txt << "Disallow: /v1"
+        robots_txt << "Disallow: /widgets"
+        robots_txt << "Sitemap: #{sitemap_url}"
+      end
 
       robots_txt = robots_txt.join("\n")
     end
 
     return robots_txt
+  end
+
+  def nearme_subdomain?(domain)
+    domain.name.match(RobotsTxtService::NEARME_SUBDOMAIN_REGEX).present?
   end
 end
--- test/unit/services/robots_txt_service_test.rb
+++ test/unit/services/robots_txt_service_test.rb
@@ -18,6 +18,21 @@ class RobotsTxtServiceTest < ActiveSupport::TestCase
       @domain.save
       assert_equal @domain.robots.present?, true
     end
-  end
 
+    should 'disallow everything if near-me.com subdomain' do
+      disallow_root = /^Disallow: \/$/
+
+      @domain.name = "something.near-me.com"
+      @domain.save
+      subdomain_match = (@domain.name =~ RobotsTxtService::NEARME_SUBDOMAIN_REGEX).present?
+      assert subdomain_match
+      assert (@domain.robots =~ disallow_root).present?
+
+      @domain.name = "mymarketplace.com"
+      @domain.save
+      no_subdomain_matches = (@domain.name =~ RobotsTxtService::NEARME_SUBDOMAIN_REGEX).present?
+      refute no_subdomain_matches
+      refute (@domain.robots =~ disallow_root).present?
+    end
+  end
 end
