6a31d5ededd627990176176f698c47d850d5de99
Krzysztof Streflik <streflik@gmail.com>
2015-08-17 14:51:16 +0200

    NM-2465 wrong token parameter name in emails

--- app/drops/user_message_drop.rb
+++ app/drops/user_message_drop.rb
@@ -15,7 +15,7 @@ class UserMessageDrop < BaseDrop
   # url to the section in the app for viewing the message
   # includes authentication token
   def url
-    routes.listing_user_message_path(@user_message.thread_context, @user_message, :token => @user_message.recipient.temporary_token)
+    routes.listing_user_message_path(@user_message.thread_context, @user_message, token_key => @user_message.recipient.temporary_token)
   end
 
   def show_path_with_token
@@ -25,7 +25,7 @@ class UserMessageDrop < BaseDrop
   # url to the section in the app for viewing the message
   # includes authentication token and tracking
   def url_with_tracking
-    routes.listing_user_message_path(@user_message.thread_context, @user_message, :token => @user_message.recipient.temporary_token, :track_email_event => true)
+    routes.listing_user_message_path(@user_message.thread_context, @user_message, token_key => @user_message.recipient.temporary_token, :track_email_event => true)
   end
 
   # first name of the thread owner (user that started the conversation)
