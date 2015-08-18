06e486d4fa7f847f4138c2a5845adaf89b54f8bd
Tomasz Lemkowski <lemkowski@gmail.com>
2015-08-19 01:49:47 +0200

    NM-2478 update curencies list

--- app/models/payment_gateway/paypal_express_payment_gateway.rb
+++ app/models/payment_gateway/paypal_express_payment_gateway.rb
@@ -29,6 +29,10 @@ class PaymentGateway::PaypalExpressPaymentGateway < PaymentGateway
     ["AL", "DZ", "AD", "AO", "AI", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS", "BH", "BB", "BE", "BZ", "BJ", "BM", "BT", "BO", "BA", "BW", "BR", "BN", "BG", "BF", "BI", "KH", "CA", "CV", "KY", "TD", "CL", "CN", "CO", "KM", "CD", "CG", "CK", "CR", "HR", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "ER", "EE", "ET", "FK", "FJ", "FI", "FR", "GF", "PF", "GA", "GM", "GE", "DE", "GI", "GR", "GL", "GD", "GP", "GU", "GT", "GN", "GW", "GY", "VA", "HN", "HK", "HU", "IS", "IN", "ID", "IE", "IL", "IT", "JM", "JP", "JO", "KZ", "KE", "KI", "KR", "KW", "KG", "LA", "LV", "LS", "LI", "LT", "LU", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MN", "MS", "MA", "MZ", "NA", "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NU", "NF", "NO", "OM", "PW", "PA", "PG", "PE", "PH", "PN", "PL", "PT", "QA", "RE", "RO", "RU", "RW", "SH", "KN", "LC", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SK", "SI", "SB", "SO", "ZA", "KR", "ES", "LK", "SR", "SJ", "SZ", "SE", "CH", "TW", "TJ", "TZ", "TH", "TG", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "US", "UY", "VU", "VE", "VN", "VG", "WF", "YE", "ZM"]
   end
 
+  def supported_currencies
+    ["AUD", "BRL", "CZK", "DKK", "HDK", "HUF", "ILS", "MYR", "MXN", "NOK", "NZD", "PHP", "RUB", "SGD", "SEK", "CHF", "TWD", "THB", "TRY",  "USD", "GBP", "EUR", "JPY", "CAD", "PLN"]
+  end
+
   def authorize(authoriazable, options = {})
     PaymentAuthorizer::PaypalExpressPaymentAuthorizer.new(self, authoriazable, options).process!
   end
@@ -114,10 +118,6 @@ class PaymentGateway::PaypalExpressPaymentGateway < PaymentGateway
 
   def refund_identification(charge)
     charge.response.params["transaction_id"]
-  end
-
-  def supported_currencies
-    ["USD", "GBP", "EUR", "JPY", "CAD", "PLN"]
   end
 
   def token
