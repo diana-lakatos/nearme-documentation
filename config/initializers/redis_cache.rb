ef115ee9a7e137201eaf91fff527bbe42679c9bf
Krzysztof Streflik <streflik@gmail.com>
2015-08-19 18:58:13 +0200

    NM-2471A small fix for initializer

--- config/initializers/redis_cache.rb
+++ config/initializers/redis_cache.rb
@@ -1,10 +1,10 @@
 module RedisCache
   class << self
     def client
-      if Rails.configuration.redis_cache_client.is_a?(Redis)
-        @client ||= Redis.new(Rails.configuration.redis_settings)
+      @client ||= if Rails.configuration.redis_cache_client == Redis
+        Redis.new(Rails.configuration.redis_settings)
       else
-        @client ||= Rails.configuration.redis_cache_client
+        Rails.configuration.redis_cache_client
       end
     end
   end
