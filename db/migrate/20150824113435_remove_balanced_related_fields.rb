7b9b2c62e2794a796a41091730cd96df2e8471bd
Tomasz Lemkowski <lemkowski@gmail.com>
2015-08-25 11:42:13 +0200

    NM-2508 reveresed migration fix

--- db/migrate/20150824113435_remove_balanced_related_fields.rb
+++ db/migrate/20150824113435_remove_balanced_related_fields.rb
@@ -4,4 +4,10 @@ class RemoveBalancedRelatedFields < ActiveRecord::Migration
     remove_column :instances, :encrypted_live_balanced_api_key
     remove_column :instances, :encrypted_test_balanced_api_key
   end
+
+  def down
+    add_column :instance_clients, :encrypted_balanced_user_id, :string
+    add_column :instances, :encrypted_live_balanced_api_key, :string
+    add_column :instances, :encrypted_test_balanced_api_key, :string
+  end
 end
