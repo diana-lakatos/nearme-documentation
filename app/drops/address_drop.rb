a9d99e98114416c994736ae6277a3943ac3f9772
Andrei Canciu <andrei@canciu.ro>
2015-09-08 23:30:20 +1200

    NM-2578-surface-separate-location-attributes-via-liquid
    * added geocode location to user
    * added postcode to location drop

--- app/drops/address_drop.rb
+++ app/drops/address_drop.rb
@@ -10,7 +10,17 @@ class AddressDrop < BaseDrop
   #   returns the city as a string
   # suburb
   #   returns the suburb as a string
-  delegate :street, :address, :city, :suburb, :iso_country_code, to: :address_object
+  # country
+  #   returns the country as a string
+  # state
+  #   returns the state as a string
+  # postcode
+  #   returns the postcode as a string
+  # latitude
+  #   returns the latitude for this address
+  # longitude
+  #   returns the longitude for this address
+  delegate :street, :address, :city, :suburb, :iso_country_code, :country, :state, :postcode, :latitude, :longitude, to: :address_object
 
   def initialize(address_object)
     @address_object = address_object
