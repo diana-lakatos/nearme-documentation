992c0b9ff5cf0f2b32d6b0e2e685e726184dff95
Andrei Canciu <andrei@canciu.ro>
2015-10-02 01:52:35 +1200

    NM-2648-not-all-required-fields-are-marked-as-such
    * required? method for transactable and product telling if an attribute
    is required
    * for products description will be shown as required if and only if
    required; price will always be shown as required
    * for services name, description, confirm_reservations, capacity
    will be shown as required if and only if required

---
+++ lib/required_field_checker.rb
@@ -0,0 +1,20 @@
+class RequiredFieldChecker
+
+  def initialize(object, attribute)
+    @object = object
+    @attribute = attribute.to_s
+  end
+
+  def required?
+    is_required = @object.class.validators_on(@attribute).map(&:class).include?(ActiveRecord::Validations::PresenceValidator)
+    if !is_required && (@object.try(:transactable_type).present? || @object.try(:product_type).present?)
+      is_required = @object.custom_validators.detect do |validator|
+        validator.field_name == @attribute && !validator.validation_rules.try(:[], 'presence').nil?
+      end.present?
+    end
+
+    is_required
+  end
+
+end
+
