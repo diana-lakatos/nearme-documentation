1e18f2a1d82aeec8d0bbfc7f59904e12b465f75e
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-10-16 14:19:50 +0200

    NM-2290 MPO Admin Dashboard

--- app/controllers/instance_admin/base_controller.rb
+++ app/controllers/instance_admin/base_controller.rb
@@ -87,6 +87,7 @@ class InstanceAdmin::BaseController < ApplicationController
 
   PROJECTS_CONTROLLERS = {
     'project_types'  => { default_action: 'index' },
+    'projects'   => { default_action: 'index' },
     'topics'  => { default_action: 'index' },
     'users'   => { controller: '/instance_admin/manage/users', default_action: 'index' },
     'spam_reports'  => { default_action: 'index' }
