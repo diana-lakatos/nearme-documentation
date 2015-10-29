56399562d3b0c9e84ebac412a0b75ebe034a7fcb
Tomasz Lemkowski <lemkowski@gmail.com>
2015-10-29 10:06:35 +0100

    NM-2041 fix cart prices

--- app/decorators/cart_decorator.rb
+++ app/decorators/cart_decorator.rb
@@ -1,20 +1,40 @@
 class CartDecorator
   include MoneyRails::ActionViewExtension
+  include ActionView::Helpers::TagHelper
 
-  def initialize(user)
+  def initialize(user, order=nil, step=nil)
     @user = user
+    @step = step
+    @order = order
+  end
+
+  def cart_orders
+    @cart_orders ||= if @order
+      [@order]
+    else
+      @user.cart_orders
+    end
   end
 
   def items_count
-    @user.cart_orders.sum(:item_count)
+    cart_orders.size
   end
 
   def line_items_count
-    @user.cart_orders.joins(:line_items).count
+    cart_orders.map(&:line_items).flatten.size
   end
 
   def total
-    @user.cart_orders.sum(:total)
+    case @step
+    when :payment then cart_orders.map(&:total_amount_to_charge).sum
+    when :delivery then cart_orders.map(&:total).sum
+    else
+      cart_orders.map(&:subtotal_amount_to_charge).sum
+    end
+  end
+
+  def total_with_fees
+    cart_orders.sum(:total)
   end
 
   def navigation
@@ -22,11 +42,11 @@ class CartDecorator
   end
 
   def total_display
-    humanized_money_with_symbol(total.to_money(Spree::Config.currency))
+    "#{Currency.find_by_iso_code(Spree::Config.currency).try(:symbol)} #{content_tag(:span, humanized_money(total.to_money), data: {"cart-total": true})}"
   end
 
   def orders
-    @user.cart_orders.decorate
+    cart_orders.decorate
   end
 
   def empty?
