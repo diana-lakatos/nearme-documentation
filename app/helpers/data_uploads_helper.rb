4a8352c7b76dd76c93081525844a4cc867ef11f5
Andrei Canciu <andrei@canciu.ro>
2015-11-12 06:35:47 +1100

    NM-3075-add-notice-about-invalid-byte-sequences
    * make sure notice about invalid byte sequences will be displayed

---
+++ app/helpers/data_uploads_helper.rb
@@ -0,0 +1,17 @@
+module DataUploadsHelper 
+
+  def data_upload_full_log_summary(data_upload)
+    data_upload_sanitize_encountered_error(data_upload.encountered_error) + "\n" + data_upload.parsing_result_log.to_s
+  end
+
+  def data_upload_sanitize_encountered_error(encountered_error)
+    encountered_error = encountered_error.to_s
+    parts = encountered_error.split(/\n/)
+    if parts.length > 1
+      parts[0]
+    else
+      encountered_error
+    end
+  end
+
+end
