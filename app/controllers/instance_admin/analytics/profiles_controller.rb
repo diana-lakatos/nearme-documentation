ac98d664205fadabd46ed2ae12717b6468e9646b
Andrei Canciu <andrei@canciu.ro>
2015-12-05 07:56:04 +1100

    NM-3442-create-column-to-report-new-people-joining
    * added created_at column to report of people joining

--- app/controllers/instance_admin/analytics/profiles_controller.rb
+++ app/controllers/instance_admin/analytics/profiles_controller.rb
@@ -3,11 +3,14 @@ class InstanceAdmin::Analytics::ProfilesController < InstanceAdmin::Analytics::B
   def show
     users = User.without(User.unscoped.admin).with_deleted.pluck('users.id, users.email, users.name,
       users.reservations_count, users.transactables_count, users.mobile_number,
-      users.current_location, users.properties, users.deleted_at')
+      users.current_location, users.properties, users.created_at, users.deleted_at')
 
     csv = CSV.generate do |csv|
-      csv.add_row %w(id email name bookings_count listings_count phone current_location user_properties deleted_at)
+      csv.add_row %w(id email name bookings_count listings_count phone current_location user_properties created_at deleted_at)
       users.each do |profile|
+        profile[-1] = value_to_utc(profile[-1])
+        profile[-2] = value_to_utc(profile[-2])
+
         csv.add_row profile
       end
     end
@@ -17,4 +20,15 @@ class InstanceAdmin::Analytics::ProfilesController < InstanceAdmin::Analytics::B
     end
 
   end
+
+  protected
+
+  def value_to_utc(value)
+    if value.present?
+      return value.utc
+    end
+
+    value
+  end
+
 end
