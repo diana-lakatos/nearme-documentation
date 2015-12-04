0dd83a761b072b6d56b4ffc6922d2ba8e3679d86
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-12-04 14:11:28 +0100

    IDM-40 add max length validation

--- lib/validators/custom_validators.rb
+++ lib/validators/custom_validators.rb
@@ -1,7 +1,8 @@
 class CustomValidators < ActiveModel::Validator
   def validate(record)
     return true if record.skip_custom_attribute_validation
-    
+    return true if record.try(:draft?)
+
     record.custom_validators.each do |validator|
       if validator.validation_rules.present?
         validator.validation_rules.each do |validation_rule_type, validation_rule_options|
