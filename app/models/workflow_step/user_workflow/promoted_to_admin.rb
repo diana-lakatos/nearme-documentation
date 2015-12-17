6b8e7d6ce340ad82616d85adc39c2fc1e05d6ef4
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2015-12-17 18:36:29 +0100

    IDM-60 add workflow alert for inviting user as admin

---
+++ app/models/workflow_step/user_workflow/promoted_to_admin.rb
@@ -0,0 +1,25 @@
+class WorkflowStep::UserWorkflow::PromotedToAdmin < WorkflowStep::UserWorkflow::BaseStep
+
+  def initialize(user_id, admin_id)
+    @user = User.find(user_id)
+    @creator = User.find(admin_id)
+  end
+
+  def enquirer
+    @user
+  end
+
+  def lister
+    @creator
+  end
+
+  # user_who_was_promoted:
+  #   User object
+  # user_who_promoted:
+  #   User object
+  def data
+    { user_who_was_promoted: @user, user_who_promoted: @creator }
+  end
+
+end
+
