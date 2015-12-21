20bc7561a20f6738b061e668e8bc9517b49572e0
Andrei Canciu <andrei@canciu.ro>
2015-12-22 03:14:13 +1100

    NM-3522-bulk-upload-currency-and-approval-requests-issue
    * made currency into a csv field, initializing approval
    requests on upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -486,20 +486,7 @@ class ApplicationController < ActionController::Base
   end
 
   def build_approval_request_for_object(object)
-    object.approval_requests.to_a.reject! { |ar| !object.approval_request_templates.pluck(:id).include?(ar.approval_request_template_id) }
-    if (art = object.approval_request_templates.first).present?
-      unless ar = object.approval_requests.find { |approval_request| approval_request.approval_request_template_id == art.id }
-        ar = object.approval_requests.build(approval_request_template_id: art.id)
-        object.approval_requests << ar
-      end
-      ar.required_written_verification = art.required_written_verification
-      art.approval_request_attachment_templates.each do |arat|
-        if ara = current_user.approval_request_attachments.for_request_or_free(ar.id).for_attachment_template(arat.id).first
-          ar.approval_request_attachments << ara
-        end
-      end
-    end
-    object
+    ApprovalRequestInitializer.new(object).process
   end
 
   def ckeditor_toolbar_creator
