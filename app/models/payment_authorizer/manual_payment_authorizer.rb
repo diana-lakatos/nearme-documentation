fb18f207abea37b09e51e0e593867d9d4b03d266
Tomasz Lemkowski <lemkowski@gmail.com>
2015-12-21 16:53:44 +0100

    NM-3545 offline payment enhancements

--- app/models/payment_authorizer/manual_payment_authorizer.rb
+++ app/models/payment_authorizer/manual_payment_authorizer.rb
@@ -4,7 +4,8 @@ class PaymentAuthorizer::ManualPaymentAuthorizer < PaymentAuthorizer
     if @authorizable.instance_of?(Spree::Order)
       @authorizable.create_pending_payment!
     else
-      @authorizable.mark_as_paid!
+      @authorizable.mark_as_authorized!
+      @authorizable.mark_as_paid! if @authorizable.is_free?
     end
     true
   end
