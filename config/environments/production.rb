1af1871ff73bebe0111cda9ab7d186e0cbed5a4b
Krzysztof Danek <chris@veboolabs.com>
2016-02-11 17:57:45 +0100

    Enable raygun js tracking on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ DesksnearMe::Application.configure do
   config.webpack[:use_manifest] = true
   config.assets.manifest = "#{Rails.root}/public/assets/manifest.json"
 
-  # Disabled temporarily until tested on staging
-  config.raygun_js_reporting = false
+  config.raygun_js_reporting = true
   config.raygun_js_api_key = 'G/Y1+vaVvETUu7/5alKYZw=='
 end
