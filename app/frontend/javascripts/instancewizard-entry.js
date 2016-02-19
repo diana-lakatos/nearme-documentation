536f6c8a78b03166fabbf2f4128c0dcc5b093454
Krzysztof Danek <chris@veboolabs.com>
2016-02-19 11:15:46 +0100

    jQuery will now be loaded both on top to provide working env and on bottom in case it's overwritten by user scripts

--- app/frontend/javascripts/instancewizard-entry.js
+++ app/frontend/javascripts/instancewizard-entry.js
@@ -1,28 +1,19 @@
-/* global require, $ */
 'use strict';
-window.$ = require('jquery');
-require('jquery-ujs/src/rails');
 
-(function(){
+var DNM = require('./app');
 
-    var DNM = require('./dnm');
+DNM.registerInitializer(function(){
+    var els = $('[data-instance-wizard-form]');
+    if (els.length === 0) {
+        return;
+    }
 
-    DNM.registerInitializer(function(){
-        var els = $('[data-instance-wizard-form]');
-        if (els.length === 0) {
-            return;
-        }
-
-        require.ensure('./instance_wizard/instance_wizard_form', function(require){
-            var InstanceWizardForm = require('./instance_wizard/instance_wizard_form');
-            els.each(function(){
-                return new InstanceWizardForm(this);
-            })
-        });
+    require.ensure('./instance_wizard/instance_wizard_form', function(require){
+        var InstanceWizardForm = require('./instance_wizard/instance_wizard_form');
+        els.each(function(){
+            return new InstanceWizardForm(this);
+        })
     });
+});
 
-    DNM.run();
-
-    window.DNM = DNM;
-}());
-
+DNM.run();
