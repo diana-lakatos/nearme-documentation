924f4230664253c607bb35316e0da08423f6ce93
Andrei Canciu <andrei@canciu.ro>
2016-02-27 04:41:52 +1100

    NM-3995 Validations for missing approval request templates

--- app/models/transactable.rb
+++ app/models/transactable.rb
@@ -180,6 +180,7 @@ class Transactable < ActiveRecord::Base
   validates :quantity, presence: true, numericality: {greater_than: 0}
   validates :rental_shipping_type, inclusion: { in: RENTAL_SHIPPING_TYPES }
   validates_presence_of :dimensions_template, if: lambda { |record| ['delivery', 'both'].include?(record.rental_shipping_type) }
+  validates_associated :approval_requests
 
   validate :check_book_it_out_minimum_qty, if: ->(record) { record.book_it_out_minimum_qty.present? }
   validate :booking_availability, if: ->(record) { record.overnight_booking? }
