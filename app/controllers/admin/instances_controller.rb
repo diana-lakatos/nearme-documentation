ca1ad4f6a5e9a81c431c4d4ca933eccc78524d05
Andrei Canciu <andrei@canciu.ro>
2016-03-24 00:50:06 +1100

    NM-4218 Clickbox check to call doesnt work

--- app/controllers/admin/instances_controller.rb
+++ app/controllers/admin/instances_controller.rb
@@ -1,6 +1,17 @@
 class Admin::InstancesController < Admin::ResourceController
   before_filter lambda { PlatformContext.current = PlatformContext.new(Instance.find(params[:id])) }, :only => [:edit, :update, :destroy, :show]
 
+  def update
+    @instance = Instance.find(params[:id])
+    if @instance.update_attributes(instance_params)
+      flash[:success] = "Instance updated successfully"
+      redirect_to admin_instance_path(@instance)
+    else
+      flash.now[:error] = @instance.errors.full_messages.to_sentence
+      render 'edit'
+    end
+  end
+
   private
 
   def instance_params
