08237238a11621a6a0681de9e254423b61b4b032
marcek <ja.marcek@gmail.com>
2016-04-01 14:07:42 +0200

    add woff, ttf, eot and svg extentsions to ckeditor file types

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -2,6 +2,6 @@ Ckeditor.setup do |config|
   require 'ckeditor/orm/active_record'
   config.asset_path = '/assets/ckeditor/'
   config.assets_languages = %w(en)
-  config.attachment_file_types = %w(doc docx xls odt ods pdf rar zip tar tar.gz swf mp4 css txt text js xlsx)
-  config.image_file_types = %w(jpg jpeg png gif tiff)
+  config.attachment_file_types = %w(doc docx xls odt ods pdf rar zip tar tar.gz swf mp4 css txt text js xlsx woff ttf eot)
+  config.image_file_types = %w(jpg jpeg png gif tiff svg)
 end
