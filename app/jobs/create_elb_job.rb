b11ef6e40d7e23baa96689face50de1b98821dba
dariusz gorzeba <dgorzeba@gmail.com>
2016-04-13 11:43:23 +0200

    use domain#load_balancer_name using ELB;
    add ELB domain report;
    add tiny refactors;
    ensure jobs perform only in live env
    fix specs; refactor jobs
    ensure elb jobs run only on prod

--- app/jobs/create_elb_job.rb
+++ app/jobs/create_elb_job.rb
@@ -1,23 +1,12 @@
-class CreateElbJob < Job
-  def after_initialize(domain_id, certificate_body, private_key, certificate_chain)
-    @domain = Domain.find(domain_id)
-    @certificate_body = certificate_body
-    @private_key = private_key
-    @certificate_chain = certificate_chain
-  end
-
+class CreateElbJob < AbstractElbJob
   def perform
-    b = NearMe::Balancer.new(certificate_body: @certificate_body,
-                             name: @domain.to_dns_name,
-                             private_key: @private_key,
-                             certificate_chain: @certificate_chain)
-    begin
-      b.create!
-      @domain.elb_created!
-      @domain.update_column(:dns_name, b.dns_name)
-    rescue Exception => e
-      @domain.update_column(:error_message, e.message)
-      @domain.error!
-    end
+    return true if Rails.env.development?
+
+    balancer.create!
+    @domain.elb_created!
+    @domain.update_column(:dns_name, balancer.dns_name)
+  rescue
+    @domain.error!
+    @domain.update_column(:error_message, $!.message)
   end
 end
--- test/unit/jobs/create_elb_job_test.rb
+++ test/unit/jobs/create_elb_job_test.rb
@@ -3,17 +3,24 @@ require 'test_helper'
 class CreateElbJobTest < ActiveSupport::TestCase
   context '#perform' do
     setup do
-      @domain = FactoryGirl.create(:domain)
+      @domain = FactoryGirl.create(:domain, load_balancer_name: 'name')
       @domain.prepare_elb!
-      @certificate_body = "a"
-      @private_key = "b"
-      @certificate_chain = ""
+      @certificate_body = 'a'
+      @private_key = 'b'
+      @certificate_chain = ''
+
+      @balancer_options = {
+        name: @domain.load_balancer_name,
+        certificate_body: @certificate_body,
+        private_key: @private_key,
+        certificate_chain: @certificate_chain
+      }
     end
 
     should 'trigger Ballancer creation on valid and save dns_name' do
       dns_name = 'test-dns-name.com'
-      balancer = stub(:dns_name => dns_name, :create! => nil)
-      NearMe::Balancer.expects(:new).returns(balancer)
+      balancer = stub(dns_name: dns_name, :create! => nil)
+      NearMe::Balancer.expects(:new).with(@balancer_options).returns(balancer)
       CreateElbJob.perform(@domain.id, @certificate_body, @private_key, @certificate_chain)
       @domain.reload
       assert_equal @domain.dns_name, dns_name
@@ -21,10 +28,10 @@ class CreateElbJobTest < ActiveSupport::TestCase
     end
 
     should 'raise error when something went wrong and show error state' do
-      error_text = 'Exception'
-      balancer = stub(:errors => error_text)
-      balancer.stubs(:create!).raises(Exception)
-      NearMe::Balancer.expects(:new).returns(balancer)
+      error_text = 'StandardError'
+      balancer = stub(errors: error_text)
+      balancer.stubs(:create!).raises(StandardError)
+      NearMe::Balancer.expects(:new).with(@balancer_options).returns(balancer)
       CreateElbJob.perform(@domain.id, @certificate_body, @private_key, @certificate_chain)
       @domain.reload
       assert_equal @domain.error_message, error_text
