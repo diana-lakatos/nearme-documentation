b30cd94652ce6b9e9b250195118a7caaf8fc36de
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2016-04-15 12:28:59 +0200

    improve credit card management for Spacer

--- app/controllers/dashboard/user_recurring_bookings/payment_subscriptions_controller.rb
+++ app/controllers/dashboard/user_recurring_bookings/payment_subscriptions_controller.rb
@@ -2,16 +2,15 @@ class Dashboard::UserRecurringBookings::PaymentSubscriptionsController <  Dashbo
   before_filter :find_recurring_booking
   before_filter :find_payment_subscription
   before_filter :find_unpaid_recurring_booking_periods
-  before_filter :check_if_updateable, only: [:edit, :update]
 
   def edit
   end
 
   def update
     if @payment_subscription.update_attributes(payment_subscription_params)
-      if @unpaid_recurring_booking_periods.all? { |rbp| rbp.update_payment.paid? }
+      if @unpaid_recurring_booking_periods.count.zero? || @unpaid_recurring_booking_periods.all? { |rbp| rbp.update_payment.paid? }
         flash[:success] = t('flash_messages.payments.updated')
-        @recurring_booking.reconfirm!
+        @recurring_booking.reconfirm! if @recurring_booking.overdued?
       else
         flash[:error] = t('flash_messages.payments.authorization_failed_anyway')
       end
@@ -30,13 +29,7 @@ class Dashboard::UserRecurringBookings::PaymentSubscriptionsController <  Dashbo
 
   def find_payment_subscription
     @payment_subscription = @recurring_booking.payment_subscription
-  end
-
-  def check_if_updateable
-    unless @recurring_booking.overdued? && @unpaid_recurring_booking_periods.exists?
-      flash[:error] = t('flash_messages.payments.cannot_edit')
-      redirect_to dashboard_user_recurring_bookings_path
-    end
+    @payment_subscription.chosen_credit_card_id = @payment_subscription.credit_card_id
   end
 
   def payment_subscription_params
@@ -44,7 +37,7 @@ class Dashboard::UserRecurringBookings::PaymentSubscriptionsController <  Dashbo
   end
 
   def find_unpaid_recurring_booking_periods
-    @unpaid_recurring_booking_periods = @payment_subscription.subscriber.recurring_booking_periods
+    @unpaid_recurring_booking_periods = @payment_subscription.subscriber.recurring_booking_periods.unpaid
   end
 
 end
