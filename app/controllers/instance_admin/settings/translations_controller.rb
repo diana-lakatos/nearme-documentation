2bbe60fbec1456b1cd7b75c3443ee6502a111a68
Andrei Canciu <andrei@canciu.ro>
2016-05-03 02:57:24 +1200

    NM-4605 Uniqueness constraint violation for translations

--- app/controllers/instance_admin/settings/translations_controller.rb
+++ app/controllers/instance_admin/settings/translations_controller.rb
@@ -4,4 +4,16 @@ class InstanceAdmin::Settings::TranslationsController < InstanceAdmin::Settings:
     flash[:success] = t 'flash_messages.instance_admin.settings.locales.keys_updated'
     redirect_to instance_admin_settings_locales_path
   end
+
+  def update
+    super
+  # This avoids errors that have occurred in the past like when
+  # the user updated the same translation key from two separate windows
+  # the second edit failing because of missing id in params which
+  # triggered a uniqueness constraint violation
+  rescue ActiveRecord::RecordNotUnique => e
+    flash[:error] = t('general.unexpected_error_occurred')
+    redirect_to instance_admin_settings_locales_path
+  end
+
 end
