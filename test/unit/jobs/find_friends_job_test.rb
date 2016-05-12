73aef259a220c0bea1e6924f760a0fd2a8458bd8
Andrei Canciu <andrei@canciu.ro>
2016-05-13 00:44:23 +1200

    NM-4685 Undefined method friends for nil class

--- test/unit/jobs/find_friends_job_test.rb
+++ test/unit/jobs/find_friends_job_test.rb
@@ -5,7 +5,7 @@ class FindFriendsJobTest < ActiveSupport::TestCase
     should 'trigger FriendFinder for given user and authentication' do
       Rails.application.config.expects(:perform_social_jobs).returns(true)
 
-      authentication = stub(id: 1, user: mock)
+      authentication = stub(id: 1, user: mock(instance_id: PlatformContext.current.instance.id))
       finder_mock = mock(:find_friends!)
 
       Authentication.expects(:find)
