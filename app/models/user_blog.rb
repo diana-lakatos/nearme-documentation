f6659ec9a16dc4150b9afbeef88fc8a1e1419da9
marcek <ja.marcek@gmail.com>
2016-05-20 18:19:18 +0200

    Fixed recreating cache key for listing show when blog updates

--- app/models/user_blog.rb
+++ app/models/user_blog.rb
@@ -4,7 +4,7 @@ class UserBlog < ActiveRecord::Base
 
   class NotFound < ActiveRecord::RecordNotFound; end
 
-  belongs_to :user
+  belongs_to :user, touch: true
   belongs_to :instance
 
   validates :name, presence: true, if: lambda { |o| o.enabled? }
