ec0640de7d5742de8a7b92d8056294df2f1780c7
Tomasz Lemkowski <lemkowski@gmail.com>
2016-05-25 00:58:48 +0200

    Scope Amenity in API fix CI

--- app/controllers/v1/amenities_controller.rb
+++ app/controllers/v1/amenities_controller.rb
@@ -1,6 +1,6 @@
 class V1::AmenitiesController < V1::BaseController
   def index
-    @amenities = Amenity.joins(:amenity_type).where(amenity_types: {instance_id: 1})
+    @amenities = Amenity.joins(:amenity_type).where(amenity_types: {instance_id: 1}).distinct
     render json: {"amenities" => @amenities}, root: false
   end
 end
