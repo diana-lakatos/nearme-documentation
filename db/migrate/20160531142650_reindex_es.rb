c150a8a8bab13481baf2a1a8e90e6f99cdedb0f6
Krzysztof Streflik <streflik@gmail.com>
2016-05-31 18:30:14 +0200

    NM-4759A fix migration for tests

--- db/migrate/20160531142650_reindex_es.rb
+++ db/migrate/20160531142650_reindex_es.rb
@@ -1,5 +1,6 @@
 class ReindexEs < ActiveRecord::Migration
   def up
+    return if Rails.env.test?
     PlatformContext.clear_current
     Transactable.__elasticsearch__.create_index! force: true
     Transactable.searchable.import force: true
