214d6a3e73e0d150940d05a16a4b0b87736f7eaf
Andrei Canciu <andrei@canciu.ro>
2016-07-07 00:49:19 +1200

    NM-4859-B Customizable photo sizes

--- app/jobs/regenerate_uploader_versions_job.rb
+++ app/jobs/regenerate_uploader_versions_job.rb
@@ -10,59 +10,59 @@ class RegenerateUploaderVersionsJob < Job
         photo.image.delayed_processing = true
         photo.image.recreate_versions! rescue nil
         photo.skip_activity_feed_event = true
-        photo.save(validate: false)
+        photo.save(validate: false) rescue nil
 
         if photo.listing.present?
-          photo.listing_populate_photos_metadata!
+          photo.listing_populate_photos_metadata! rescue nil
         end
       end
 
       Spree::Image.find_each do |image|
         image.image.delayed_processing = true
         image.image.recreate_versions! rescue nil
-        image.save(validate: false)
+        image.save(validate: false) rescue nil
       end
     when 'AvatarUploader'
       User.find_each do |user|
         user.avatar.delayed_processing = true
         user.avatar.recreate_versions! rescue nil
-        user.save(validate: false)
+        user.save(validate: false) rescue nil
       end
     when 'AuthorAvatarUploader'
       UserBlogPost.find_each do |user_blog_post|
         user_blog_post.author_avatar_img.recreate_versions! rescue nil
-        user_blog_post.save(validate: false)
+        user_blog_post.save(validate: false) rescue nil
       end
     when 'CkeditorPictureUploader'
       Ckeditor::Picture.find_each do |picture|
         picture.data.recreate_versions! rescue nil
-        picture.save(validate: false)
+        picture.save(validate: false) rescue nil
       end
     when 'GroupCoverImageUploader'
       Group.find_each do |group|
         group.cover_image.recreate_versions! rescue nil
-        group.save(validate: false)
+        group.save(validate: false) rescue nil
       end
     when 'LinkImageUploader'
       Link.find_each do |link|
         link.image.recreate_versions! rescue nil
         link.skip_activity_feed_event = true
-        link.save(validate: false)
+        link.save(validate: false) rescue nil
       end
     when 'SimpleAvatarUploader'
       BlogPost.find_each do |blog_post|
         blog_post.author_avatar.recreate_versions! rescue nil
-        blog_post.save(validate: false)
+        blog_post.save(validate: false) rescue nil
       end
     when 'TopicCoverImageUploader'
       Topic.find_each do |topic|
         topic.cover_image.recreate_versions! rescue nil
-        topic.save(validate: false)
+        topic.save(validate: false) rescue nil
       end
     when 'TopicImageUploader'
       Topic.find_each do |topic|
         topic.image.recreate_versions! rescue nil
-        topic.save(validate: false)
+        topic.save(validate: false) rescue nil
       end
     end
   end
@@ -72,4 +72,7 @@ class RegenerateUploaderVersionsJob < Job
 
     super
   end
+
+  def max_attempts; 1; end
+
 end
