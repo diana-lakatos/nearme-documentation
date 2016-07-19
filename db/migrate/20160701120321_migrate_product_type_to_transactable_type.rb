a2a7e60b7bb6c726a4f56c70bdf3a8870e8368e2
Krzysztof Streflik <streflik@gmail.com>
2016-07-19 16:28:32 +0200

    NM-XXX removed products/orders migrations

--- db/migrate/20160701120321_migrate_product_type_to_transactable_type.rb
+++
@@ -1,9 +0,0 @@
-class MigrateProductTypeToTransactableType < ActiveRecord::Migration
-  disable_ddl_transaction!
-  def up
-    Transactable.reset_column_information
-    Rails.application.config.use_elastic_search = false
-    Rake::Task['migrate_spree:to_action_types'].invoke
-    Rails.application.config.use_elastic_search = true
-  end
-end
