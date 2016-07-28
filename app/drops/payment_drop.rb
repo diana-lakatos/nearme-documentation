768aac6b33f27fcbc0d3ba95c0d818f456f13139
Krzysztof Streflik <streflik@gmail.com>
2016-07-28 15:00:47 +0200

    fixing reservation view

--- app/drops/payment_drop.rb
+++ app/drops/payment_drop.rb
@@ -2,7 +2,7 @@ class PaymentDrop < BaseDrop
 
   attr_reader :payment
 
-  delegate :billing_authorizations, :successful_billing_authorization,
+  delegate :id, :billing_authorizations, :successful_billing_authorization,
     :successful_charge, :test_mode?, :active_merchant_payment?, to: :payment
 
   def initialize(payment)
