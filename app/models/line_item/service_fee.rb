6bcc800b14ef27f1eb3cc3db35fa0aaec28d288d
Tomasz Lemkowski <lemkowski@gmail.com>
2016-08-03 19:26:20 +0200

    NM-5197 fix 500 on payout

--- app/models/line_item/service_fee.rb
+++ app/models/line_item/service_fee.rb
@@ -1,2 +1,5 @@
 class LineItem::ServiceFee < LineItem
+  def cart_position
+    10
+  end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -68,6 +68,7 @@ class Payment < ActiveRecord::Base
 
   # === Scopes
 
+  scope :migrated_payment, -> { where("payable_type NOT IN(?)", ["OldReservation", "OldRecurringBookingPeriod", "Spree::Order"] )}
   scope :active, -> { where(state: ['authorized', 'paid'])}
   scope :live, -> { where(payment_gateway_mode: 'live')}
   scope :authorized, -> { where(state: 'authorized') }
@@ -77,7 +78,7 @@ class Payment < ActiveRecord::Base
   scope :refunded, -> { where("#{table_name}.state = 'refunded'") }
   scope :not_refunded, -> { where("#{table_name}.state IS NOT 'refunded'") }
   scope :last_x_days, lambda { |days_in_past| where('DATE(payments.created_at) >= ? ', days_in_past.days.ago) }
-  scope :needs_payment_transfer, -> { paid_or_refunded.where(payment_transfer_id: nil, offline: false) }
+  scope :needs_payment_transfer, -> { paid_or_refunded.where(payment_transfer_id: nil, offline: false).migrated_payment }
   scope :transferred, -> { where.not(payment_transfer_id: nil)}
 
   scope :total_by_currency, -> {
