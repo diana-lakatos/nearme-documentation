f54e3bcf43ca96a17a93da305007d996a34fbea7
Tomasz Lemkowski <lemkowski@gmail.com>
2016-08-05 17:29:58 +0200

    NM-5243 order tabs management migration vol.2

--- db/migrate/20160805133339_add_orders_tabs_to_instances.rb
+++ db/migrate/20160805133339_add_orders_tabs_to_instances.rb
@@ -3,7 +3,9 @@ class AddOrdersTabsToInstances < ActiveRecord::Migration
     add_column :instances, :orders_received_tabs, :string
     add_column :instances, :my_orders_tabs, :string
 
-    i = Instance.find('130')
+    Instance.reset_column_information
+    i = Instance.find(130)
+    i.set_context!
     i.attributes = { my_orders_tabs: ['not_archived', 'archived'], orders_received_tabs: ['unconfirmed', 'confirmed', 'overdue', 'archived']}
     i.save(validate: false)
   end
