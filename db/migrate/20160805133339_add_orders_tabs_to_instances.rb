81f70321a4ac15d3ac4f3145ea0b6aa8a59caf61
Krzysztof Streflik <streflik@gmail.com>
2016-08-06 10:10:20 +0200

    fixing migrations

--- db/migrate/20160805133339_add_orders_tabs_to_instances.rb
+++ db/migrate/20160805133339_add_orders_tabs_to_instances.rb
@@ -4,7 +4,8 @@ class AddOrdersTabsToInstances < ActiveRecord::Migration
     add_column :instances, :my_orders_tabs, :string
 
     Instance.reset_column_information
-    i = Instance.find(130)
+    i = Instance.find_by(id: 130)
+    return true if i.nil?
     i.set_context!
     i.attributes = { my_orders_tabs: ['not_archived', 'archived'], orders_received_tabs: ['unconfirmed', 'confirmed', 'overdue', 'archived']}
     i.save(validate: false)
