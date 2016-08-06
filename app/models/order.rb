0045d4243366e73e770618902064c52b1d59d1ec
Krzysztof Streflik <streflik@gmail.com>
2016-08-06 14:46:58 +0200

    NM-XXX fixing tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -77,8 +77,8 @@ class Order < ActiveRecord::Base
 
   # scope :archived, -> { active.where('archived_at IS NOT NULL') }
   # scope :not_archived, -> { active.where(archived_at: nil) }
-  scope :not_archived, -> { where("(type != 'RecurringBooking' AND state != 'inactive' AND archived_at IS NULL) OR (type = 'RecurringBooking' AND state NOT IN ('cancelled_by_guest', 'cancelled_by_host', 'rejected', 'expired'))") }
-  scope :archived, -> { where("(type != 'RecurringBooking' AND archived_at IS NOT NULL) OR (type = 'RecurringBooking' AND state IN ('rejected', 'expired', 'cancelled_by_host', 'cancelled_by_guest'))") }
+  scope :not_archived, -> { where("(orders.type != 'RecurringBooking' AND orders.state != 'inactive' AND orders.archived_at IS NULL) OR (orders.type = 'RecurringBooking' AND orders.state NOT IN ('cancelled_by_guest', 'cancelled_by_host', 'rejected', 'expired'))") }
+  scope :archived, -> { where("(orders.type != 'RecurringBooking' AND orders.archived_at IS NOT NULL) OR (type = 'RecurringBooking' AND orders.state IN ('rejected', 'expired', 'cancelled_by_host', 'cancelled_by_guest'))") }
   scope :reviewable, -> { where.not(archived_at: nil).confirmed }
   scope :cancelled, -> { with_state(:cancelled_by_guest, :cancelled_by_host) }
   scope :confirmed, -> { with_state(:confirmed)}
