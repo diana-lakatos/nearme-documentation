6542f75e32b3f184dc8ea1ba0e49d9fd7239f1ef
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2016-08-14 20:44:36 +0200

    silent raygun

--- lib/constraints/page_constraints.rb
+++ lib/constraints/page_constraints.rb
@@ -3,6 +3,6 @@ class Constraints::PageConstraints
     params = request.path_parameters
     Page.where(slug: Page.possible_slugs([params[:slug], params[:slug2], params[:slug3]].compact.join('/'), params[:format])).exists? ||
       Page.where(slug: Page.possible_slugs([params[:slug], params[:slug2]].compact.join('/'), params[:format])).exists? ||
-      Page.where(slug: Page.possible_slugs(params[:slug], params[:format])).exists?
+      Page.where(slug: Page.possible_slugs(params[:slug], params[:format])).exists?  rescue nil
   end
 end
