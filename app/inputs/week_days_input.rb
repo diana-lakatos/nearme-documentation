1666ce17e1c87ea7eacda540c3ecead532437ce3
Andrei Canciu <andrei@canciu.ro>
2016-09-14 23:32:51 +1200

    NM-5776 Add translation keys for availability days

--- app/inputs/week_days_input.rb
+++ app/inputs/week_days_input.rb
@@ -5,7 +5,7 @@ class WeekDaysInput < SimpleForm::Inputs::CollectionCheckBoxesInput
   end
 
   def collection
-    @collection = [ ['M', 1] , ['T', 2], ['W', 3], ['TH', 4], ['F', 5], ['SA', 6], ['SU', 0] ]
+    @collection = [ [t('week.short.monday'), 1] , [t('week.short.tuesday'), 2], [t('week.short.wednesday'), 3], [t('week.short.thursday'), 4], [t('week.short.friday'), 5], [t('week.short.saturday'), 6], [t('week.short.sunday'), 0] ]
   end
 
   def input_class
