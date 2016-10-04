a4339e654eb2e1bfcdfcfbd14b052c5bb5a57702
Ozkee <oskar@near-me.com>
2016-10-05 00:55:56 +0200

    NM-5095 performance improvment for bulk upload

--- test/integration/data_importer/tracker/summary_test.rb
+++ test/integration/data_importer/tracker/summary_test.rb
@@ -18,6 +18,7 @@ class DataImporter::Tracker::SummaryTest < ActiveSupport::TestCase
     @xml_file.parse
     assert_equal({ 'user' => 2, 'company' => 2, 'location' => 3, 'transactable' => 4, 'photo' => 4 }, @summary_tracker.new_entities)
     assert_equal({ 'user' => 0, 'company' => 0, 'location' => 0, 'transactable' => 0, 'photo' => 0 }, @summary_tracker.updated_entities)
+
     @xml_file = FactoryGirl.create(:xml_template_file)
     @summary_tracker = DataImporter::Tracker::Summary.new
     @xml_file.trackers << @summary_tracker
