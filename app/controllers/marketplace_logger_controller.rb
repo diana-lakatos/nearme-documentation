eeeb635702a1cf1d0c6a2a7f0109ccf9456f4b7a
Andrei Canciu <andrei@canciu.ro>
2016-10-05 20:59:55 +1200

    NM-5604 Performance issues with marketplace logs
    * Denormalization of marketplace errors
    * Removed logging of Javascript errors

--- app/controllers/marketplace_logger_controller.rb
+++
@@ -1,9 +0,0 @@
-class MarketplaceLoggerController < ApplicationController
-
-  JAVASCRIPT_ERROR = 'Javascript Error'
-
-  def create
-    MarketplaceLogger.error(JAVASCRIPT_ERROR, params[:message], raise: false, url: params[:url])
-    render nothing: true
-  end
-end
---
+++ app/models/marketplace_error_group.rb
@@ -0,0 +1,8 @@
+class MarketplaceErrorGroup < ActiveRecord::Base
+  auto_set_platform_context
+  scoped_to_platform_context
+  acts_as_paranoid
+
+  has_many :marketplace_errors, inverse_of: :marketplace_error_group
+
+end
