3625e493022b2d7af5cc5d39eda2033a315dcd10
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2016-10-06 16:14:44 +0200

    NM-6011-500-when-instance-admin-accesses-custom-theme

--- app/controllers/instance_admin/custom_templates/base_controller.rb
+++ app/controllers/instance_admin/custom_templates/base_controller.rb
@@ -4,5 +4,9 @@ class InstanceAdmin::CustomTemplates::BaseController < InstanceAdmin::ResourceCo
     redirect_to instance_admin_custom_templates_custom_themes_path
   end
 
+  def permitting_controller_class
+    'customtemplates'
+  end
+
 end
 
