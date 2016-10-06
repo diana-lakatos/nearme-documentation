9d1ea42deba501fb64583e218242cc1dd674e6a8
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2016-10-06 11:48:56 +0200

    NM-5999 seller attachments for purchasers

--- app/controllers/custom_assets_controller.rb
+++ app/controllers/custom_assets_controller.rb
@@ -3,7 +3,6 @@ class CustomAssetsController < ApplicationController
   def show
     attachment = Ckeditor::Asset.find(params[:id])
     if attachment.accessible_to?(current_user)
-      # send_file(attachment.data.path, type: attachment.data_content_type, disposition: 'attachment', status: 200)
       redirect_to attachment.url_content
     else
       redirect_to root_url
