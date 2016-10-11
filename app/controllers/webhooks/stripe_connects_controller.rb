ac3f1080a39477b850950b4ed2a345008f6e73b6
Tomasz Lemkowski <lemkowski@gmail.com>
2016-10-11 23:58:21 +0200

    NM-6043 Stripe webhook fixes

--- app/controllers/webhooks/stripe_connects_controller.rb
+++ app/controllers/webhooks/stripe_connects_controller.rb
@@ -1,50 +1,73 @@
-class Webhooks::StripeConnectsController < Webhooks::BaseWebhookController
-  def webhook
-    if request.post?
-      begin
-        event = @payment_gateway.parse_webhook(params[:id])
-        case event.type
-        when 'account.updated'
-          merchant_account = MerchantAccount::StripeConnectMerchantAccount.find_by(internal_payment_gateway_account_id: params[:user_id])
-          account = @payment_gateway.retrieve_account(event.data.object.id)
-          merchant_account.skip_validation = true
-          merchant_account.change_state_if_needed(account) do |new_state|
-            case new_state
-            when 'verified'
-              WorkflowStepJob.perform(WorkflowStep::PaymentGatewayWorkflow::MerchantAccountApproved, merchant_account.id)
-            when 'failed'
-              WorkflowStepJob.perform(WorkflowStep::PaymentGatewayWorkflow::MerchantAccountDeclined, merchant_account.id, "Missing fields: #{account.verification.fields_needed.join(', ')}")
-            end
-          end
-          if account.verification.fields_needed.present?
-            merchant_account.update_column :data, merchant_account.data.merge(fields_needed: account.verification.fields_needed)
-          end
-        when 'transfer.updated'
-          transfer = @payment_gateway.payment_transfers.pending.with_token(event.data.object.id).first
-          if transfer.pending?
-            transfer_state = event.data.object.status
-            if transfer_state == 'paid'
-              transfer.payout_attempts.last.payout_successful(event)
-            elsif %w(canceled failed).include?(transfer_state)
-              transfer.payout_attempts.last.payout_failed(event)
-            end
-          end
-        end
-      ensure
-        if merchant_account
-          merchant_account.webhooks.create!(response: params.to_yaml)
-        else
-          @payment_gateway.webhooks.create!(response: params.to_yaml)
-        end
+module Webhooks
+  class StripeConnectsController < Webhooks::BaseWebhookController
+    before_filter :fetch_event
+
+    def webhook
+      case @event.type
+      when 'account.updated' then account_updated
+      when 'transfer.updated' then transfer_updated
+      end
+    ensure
+      @payment_gateway.webhooks.create!(response: params.to_yaml)
+      render nothing: true
+    end
+
+    private
+
+    def account_updated
+      merchant_account.skip_validation = true
+      merchant_account.change_state_if_needed(account) { |state| workflow_for(state) }
+      update_needed_fields
+    end
+
+    def update_needed_fields
+      return unless account.verification.fields_needed.present?
+      merchant_account.update_column :data, merchant_account.data.merge(fields_needed: account.verification.fields_needed)
+    end
+
+    def workflow_for(state)
+      case state
+      when 'verified'
+        WorkflowStepJob.perform(WorkflowStep::PaymentGatewayWorkflow::MerchantAccountApproved, merchant_account.id)
+      when 'failed'
+        WorkflowStepJob.perform(
+          WorkflowStep::PaymentGatewayWorkflow::MerchantAccountDeclined,
+          merchant_account.id,
+          "Missing fields: #{account.verification.fields_needed.join(', ')}"
+        )
       end
     end
-  ensure
-    render nothing: true
-  end
 
-  protected
+    def merchant_account
+      @merchant_account ||= @payment_gateway.merchant_accounts.find_by!(
+        internal_payment_gateway_account_id: @event.data.object.id
+      )
+    end
 
-  def payment_gateway_class
-    PaymentGateway::StripeConnectPaymentGateway
+    def transfer_updated
+      transfer_state = @event.data.object.status
+
+      if transfer_state == 'paid'
+        transfer.payout_attempts.last.payout_successful(@event)
+      elsif %w(canceled failed).include?(transfer_state)
+        transfer.payout_attempts.last.payout_failed(@event)
+      end
+    end
+
+    def transfer
+      @transfer ||= @payment_gateway.payment_transfers.pending.with_token(@event.data.object.id).first!
+    end
+
+    def payment_gateway_class
+      PaymentGateway::StripeConnectPaymentGateway
+    end
+
+    def fetch_event
+      @event = @payment_gateway.parse_webhook(params[:id], params[:user_id])
+    end
+
+    def account
+      @account ||= @payment_gateway.retrieve_account(@event.data.object.id)
+    end
   end
 end
