8888dbb96a6373f0ecb1c48d25e2c51c01d669d4
Krzysztof Danek <chrisd@near-me.com>
2016-10-11 21:32:31 +0200

    UOT-54 Common options for all InstanceProfileType edit forms

--- lib/jira_wrapper.rb
+++ lib/jira_wrapper.rb
@@ -8,38 +8,26 @@ class JiraWrapper
     @client = JIRA::Client.new(username: 'jira-api', password: 'N#arM3123adam', context_path: '', site: 'https://near-me.atlassian.net', rest_base_path: '/rest/api/2', auth_type: :basic, read_timeout: 120)
   end
 
-  def initiate_current_sprint!
-    monday = Time.now.monday? ? Date.current : Chronic.parse('last monday')
-    sprint = @client.Sprint.find(monday.strftime('%Y-%m-%d'))['issues'].first['fields']['customfield_10007'].last
-    current_sprint_id = sprint.split('id=')[1].split(',')[0].to_i
-    sprint_name = sprint.split('name=')[1].split(',')[0]
-    start_date = sprint.split('startDate=')[1].split(',')[0]
-    end_date = sprint.split('endDate=')[1].split(',')[0]
-    ensure_version_present!(name: next_tag(1), description: sprint_name, user_released_data: end_date, user_start_date: start_date, start_date: start_date)
-    current_sprint_id
-  end
-
-  def initiate_hotfix!(description = 'Hotfix')
-    tag = next_tag(2)
-    ensure_version_present!(name: tag, description: description, user_released_data: Time.now.to_s, user_start_date: Time.now.to_s, start_date: Time.now.to_s)
-    tag
-  end
-
   def ensure_version_present!(name:, start_date:, user_released_data:, user_start_date:, description:)
+    puts "Ensuring version exists: #{name}"
     unless version(name).present?
+      hash = {
+        description: description,
+        name: name,
+        projectId: NM_PROJECT_ID,
+        userStartDate: Chronic.parse(user_start_date).strftime('%-d/%b/%y'),
+        userReleaseDate: Chronic.parse(user_released_data).strftime('%-d/%b/%y')
+      }
+      puts "\tDoes not exists, creating: #{hash.inspect}"
       current_version = @client.Version.build
-      current_version.save(description: description,
-                           name: name,
-                           projectId: NM_PROJECT_ID,
-                           userStartDate: Chronic.parse(user_start_date).strftime('%-d/%b/%y'),
-                           userReleaseDate: Chronic.parse(user_released_data).strftime('%-d/%b/%y'))
+      current_version.save(hash)
       @versions << current_version
     end
     current_version
   end
 
-  def issues(sprint)
-    @issues ||= @client.Issue.jql("project = \"Near Me\" AND sprint = #{sprint.to_i} AND status != Closed", max_results: 500)
+  def issues(jql)
+    @issues ||= @client.Issue.jql(jql, max_results: 500)
   end
 
   def find_issue(number)
@@ -57,9 +45,13 @@ class JiraWrapper
     @epics
   end
 
-  def issue_hash(number)
-    issue = find_issue(number)
+  def issue_hash_for_number
+    issue_hash(find_issue(number))
+  end
+
+  def issue_hash(issue)
     {
+      name: issue.key + ' ' + issue.summary,
       fixVersions: issue.fixVersions.map(&:name).join(', '),
       status: issue.status.name,
       assignee: issue.assignee.displayName,
@@ -72,12 +64,10 @@ class JiraWrapper
     epic_hash[issue.customfield_10008]
   end
 
-  def update_issue(number, options)
-    issue = find_issue(number)
-    issue.save(fields: { fixVersions: options[:tag] })
-    issue.save(fields: { customfield_10007: options[:sprint_number] })
+  def assign_version(issue, fixVersion)
+    issue.save(fields: { fixVersions: [{ name: fixVersion }] })
   rescue => e
-    puts "Error for card: #{card_in_sprint}. #{e}"
+    puts "Error for card: #{issue.key}. #{e}"
   end
 
   def version(tag)
