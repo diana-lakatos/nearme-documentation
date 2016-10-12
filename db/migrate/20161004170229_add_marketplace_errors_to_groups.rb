baa85389034746036f1aa939244733dba87e5230
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2016-10-12 11:23:24 +0200

    remove unnecessary tables and columns

--- db/migrate/20161004170229_add_marketplace_errors_to_groups.rb
+++ db/migrate/20161004170229_add_marketplace_errors_to_groups.rb
@@ -2,20 +2,6 @@ class AddMarketplaceErrorsToGroups < ActiveRecord::Migration
   def self.up
     index = 0
     MarketplaceError.where(error_type: 'Javascript Error').delete_all
-    MarketplaceError.order('id ASC').find_each do |marketplace_error|
-      index += 1
-      puts "At error #{index}" if index % 10_000 == 0
-
-      group = MarketplaceErrorGroup.where(error_type: marketplace_error.error_type,
-                                          message_digest: marketplace_error.message_digest,
-                                          instance_id: marketplace_error.instance_id).first_or_create! do |meg|
-        meg.message = marketplace_error.message
-        meg.instance_id = marketplace_error.instance_id
-      end
-
-      group.marketplace_errors << marketplace_error
-      group.update_column(:last_occurence, marketplace_error.created_at)
-    end
   end
 
   def self.down
