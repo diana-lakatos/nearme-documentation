2ce7442e61718f14f072a04695e1d0332c6b7d66
Andrei Canciu <andrei@canciu.ro>
2016-10-14 03:10:38 +1200

    NM-5990 Fix purchase mails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_161_007_150_808) do
+ActiveRecord::Schema.define(version: 20161013133742) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension 'btree_gin'
