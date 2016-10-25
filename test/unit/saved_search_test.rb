79b5431bbee7aa3fc326cef0549e44ddf37a7374
Krzysztof Streflik <streflik@gmail.com>
2016-10-25 12:40:15 +0100

    NM-6206 fixing saved search params for spacer

--- test/unit/saved_search_test.rb
+++ test/unit/saved_search_test.rb
@@ -20,12 +20,22 @@ class SavedSearchTest < ActiveSupport::TestCase
   context '#change_sort' do
     should 'change sorting to created_at desc' do
       saved_search = create(:saved_search, query: '?loc=Auckland&sort=relevance&query=&transactable_type_id=1&buyable=false&sort=something')
-      assert_equal '?loc=Auckland&query=&transactable_type_id=1&buyable=false&sort=created_at&order=desc', saved_search.query
+      assert_equal '?loc=Auckland&query=&transactable_type_id=1&buyable=false&sort=created_at_desc', saved_search.query
     end
 
     should 'chnage sorting to created_at desc with empty existing sort too' do
       saved_search = create(:saved_search, query: '?loc=Auckland&sort=relevance&query=&transactable_type_id=1&buyable=false&sort=')
-      assert_equal '?loc=Auckland&query=&transactable_type_id=1&buyable=false&sort=created_at&order=desc', saved_search.query
+      assert_equal '?loc=Auckland&query=&transactable_type_id=1&buyable=false&sort=created_at_desc', saved_search.query
+    end
+
+    should 'chnage sorting to created_at desc with multi field sort' do
+      saved_search = create(:saved_search, query: '?loc=Auckland&sort=relevance_asc,distance_desc&query=&transactable_type_id=1&buyable=false&sort=')
+      assert_equal '?loc=Auckland&query=&transactable_type_id=1&buyable=false&sort=created_at_desc', saved_search.query
+    end
+
+    should 'chnage sorting to created_at desc with multi field sort with 2 params' do
+      saved_search = create(:saved_search, query: '?loc=Auckland&sort=relevance_asc,distance_desc&query=&transactable_type_id=1&buyable=false&sort=relevance_asc,distance_desc')
+      assert_equal '?loc=Auckland&query=&transactable_type_id=1&buyable=false&sort=created_at_desc', saved_search.query
     end
   end
 end
