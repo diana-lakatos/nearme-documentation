ecaa3491c4d55c41a215336e64494e44140decd5
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2016-11-07 13:43:17 +0100

    NM-6362 load translations on startup for staging/prod

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,6 +1,7 @@
 require Rails.root.join('lib/i18n/backend/dnm_key_value')
 if ActiveRecord::Base.connection.table_exists? Translation.table_name
   I18N_DNM_BACKEND = I18n::Backend::DNMKeyValue.new(Rails.cache)
+  I18N_DNM_BACKEND.rebuild! if Rails.env.staging? || Rails.env.production?
   I18n.backend = I18n::Backend::Chain.new(I18N_DNM_BACKEND, I18n::Backend::Simple.new)
 else
   message = "translations table does not exist, we can't use I18N_DNM_BACKEND"
