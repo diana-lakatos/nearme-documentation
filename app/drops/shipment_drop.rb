b49100dbf9cd5a9b1e6a80f1c3a67d5fe03067d0
Andrei Canciu <andrei@canciu.ro>
2016-11-08 03:45:18 +1200

    NM-6370 Drops in root problem

--- app/drops/shipment_drop.rb
+++ app/drops/shipment_drop.rb
@@ -1,6 +1,23 @@
 class ShipmentDrop < BaseDrop
+  
+  # @return [Shipment]
   attr_reader :shipment
 
+  # @!method tracking_url_provider
+  #   URL for tracking the shipment
+  #   @return (see Shipment#tracking_url_provider)
+  # @!method tracking_number
+  #   Tracking number for the shipment
+  #   @return (see Shipment#tracking_number)
+  # @!method label_url
+  #   URL to the package label
+  #   @return (see Shipment#label_url)
+  # @!method direction
+  #   Shipment direction (inbound / outbound)
+  #   @return (see Shipment#direction)
+  # @!method shipping_rule
+  #   {ShippingRule} object associated with the shipment
+  #   @return (see Shipment#shipping_rule)
   delegate :tracking_url_provider, :tracking_number, :label_url, :direction, :shipping_rule, to: :shipment
 
   def initialize(shipment)
