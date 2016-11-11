55ec1cf933cdd1210a50ff4153ddda1e536d9120
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2016-11-11 12:18:08 +0100

    fix migration for intel

--- db/migrate/20161027212559_configure_photo_uploader_for_devmesh.rb
+++ db/migrate/20161027212559_configure_photo_uploader_for_devmesh.rb
@@ -5,22 +5,28 @@ class ConfigurePhotoUploaderForDevmesh < ActiveRecord::Migration
     if instance.present?
       instance.set_context!
       PhotoUploadVersion.create!(apply_transform: 'resize_to_fill',
+                                 theme_id: PlatformContext.current.theme.id,
                                  width:  680, height: 546, version_name: 'fullscreen',
                                  photo_uploader: 'PhotoUploader')
       PhotoUploadVersion.create!(apply_transform: 'resize_to_fill',
                                  width:  200, height: 175, version_name: 'thumb',
+                                 theme_id: PlatformContext.current.theme.id,
                                  photo_uploader: 'PhotoUploader')
       PhotoUploadVersion.create!(apply_transform: 'resize_to_fill',
+                                 theme_id: PlatformContext.current.theme.id,
                                  width:  250, height: 200, version_name: 'medium',
                                  photo_uploader: 'PhotoUploader')
       PhotoUploadVersion.create!(apply_transform: 'resize_to_fill',
+                                 theme_id: PlatformContext.current.theme.id,
                                  width:  600, height: 482, version_name: 'space_listing',
                                  photo_uploader: 'PhotoUploader')
       PhotoUploadVersion.create!(apply_transform: 'resize_to_fill',
+                                 theme_id: PlatformContext.current.theme.id,
                                  width:  1200, height: 800, version_name: 'golden',
                                  photo_uploader: 'PhotoUploader')
 
       PhotoUploadVersion.create!(apply_transform: 'resize_to_fill',
+                                 theme_id: PlatformContext.current.theme.id,
                                  width:  250, height: 200, version_name: 'medium',
                                  photo_uploader: 'AvatarUploader')
 
