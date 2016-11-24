107b28c4df36dd857d2cc0b4ac1532aaa78732bc
Paweł Kowalski <pavelloz@gmail.com>
2016-11-24 19:06:58 +0700

    NM-6468 Topic drop

--- app/drops/topic_drop.rb
+++ app/drops/topic_drop.rb
@@ -17,22 +17,26 @@ class TopicDrop < BaseDrop
   end
 
   # @return [String] url for this topic
+  # @todo -- depracate in favor of filter
   def show_url
     urlify(routes.topic_path(@topic))
   end
 
   # @return [String] cover image (url) for this topic - medium size
+  # @todo -- depracate in favor of filter
   def cover_image
     @topic.cover_image.url(:medium)
   end
 
   # @return [String] cover image (url) for this topic - big size
+  # @todo -- depracate in favor of filter
   def cover_image_big
     @topic.cover_image.url(:big)
   end
 
   # @return [String] generates a background-image style of the form 'background-image: url(...)'
   #   for the 'big' cover image if present, returns an empty string otherwise
+  # @todo -- lets use some kind of ImageDrop/tag/filter with images and lets return pure url not css
   def background_style_big
     if cover_image_big.present?
       "background-image: url(#{ActionController::Base.helpers.image_url(cover_image_big)});"
@@ -43,6 +47,7 @@ class TopicDrop < BaseDrop
 
   # @return [String] generates a background-image style of the form 'background-image: url(...)'
   #   for the 'medium' sized cover image if present, returns an empty string otherwise
+  # @todo -- lets use some kind of ImageDrop/tag/filter with images and lets return pure url not css
   def background_style
     if cover_image.present?
       "background-image: url(#{ActionController::Base.helpers.image_url(cover_image)});"
