80b25001450813056da6ad9209c6e76666e9206f
Krzysztof Danek <chrisd@near-me.com>
2016-12-08 21:33:26 +0100

    NM-6688 Allow uploading ogv and webm files

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
 Ckeditor.setup do |config|
   require 'ckeditor/orm/active_record'
   config.asset_path = '/assets/ckeditor/'
   config.assets_languages = %w(en)
-  config.attachment_file_types = %w(doc docx xls odt ods pdf rar zip tar tar.gz swf mp4 css txt text js xlsx woff ttf eot svg )
+  config.attachment_file_types = %w(doc docx xls odt ods pdf rar zip tar tar.gz swf mp4 webm ogv css txt text js xlsx woff ttf eot svg)
   config.image_file_types = %w(jpg jpeg png gif tiff svg)
 end
