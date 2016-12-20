040ac810e4cd89ba226a957f22e717a53ea48b82
Paweł Kowalski <pavelloz@gmail.com>
2016-12-20 21:25:05 +0700

    Revert "NM-6834 Change chevron direction according to accordion status"
    This reverts commit 5569f7efcbe0ce524ac7aa3382f4412bb8a2c3e8.

--- app/frontend/javascripts/initializers/application/payments_accordion.initializer.js
+++
@@ -1,7 +0,0 @@
-var accordion = document.querySelector('.checkout .accordion');
-
-if (accordion.length !== null) {
-  require.ensure('../../application/payments_accordion', function(require){
-    require('../../application/payments_accordion')();
-  });
-}
