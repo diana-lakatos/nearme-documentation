617c59e2d7181a5ca6c2b9dbae868b578ef1bc13
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2016-12-20 11:51:56 +0100

    fix migration

--- db/migrate/20161219102116_update_au_addresses_suburbs.rb
+++ db/migrate/20161219102116_update_au_addresses_suburbs.rb
@@ -1,6 +1,8 @@
 class UpdateAuAddressesSuburbs < ActiveRecord::Migration
   def change
-    Instance.find(194).set_context!
+    i = Instance.find_by(id: 194)
+    return true if i.nil?
+    i.set_context!
 
     Address.where(suburb: nil).find_each do |address|
       address.parse_address_components!
