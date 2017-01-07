299a1eccf6a26a0d7b8d906fd1ca8dcacd1b857b
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2017-01-07 13:23:49 +0100

    approving drivers for localdriva

--- app/controllers/instance_admin/manage/user_profiles_controller.rb
+++ app/controllers/instance_admin/manage/user_profiles_controller.rb
@@ -1,10 +1,9 @@
+# frozen_string_literal: true
 class InstanceAdmin::Manage::UserProfilesController < InstanceAdmin::Manage::BaseController
-
   def approve
-    resource.approved = !resource.approved
+    resource.approved = true
+    resource.enabled = true
     resource.save(validate: false)
-
     redirect_to request.referer.presence || instance_admin_manage_users_path
   end
-
 end
