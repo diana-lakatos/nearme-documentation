360b7000bc91e3fbbd5d8b43177b5e99997993de
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2017-01-07 18:15:14 +0100

    mailers for localdriva

--- lib/utils/default_alerts_creator/user_creator.rb
+++ lib/utils/default_alerts_creator/user_creator.rb
@@ -2,6 +2,11 @@ class Utils::DefaultAlertsCreator::UserCreator < Utils::DefaultAlertsCreator::Wo
   def create_all!
     create_unread_messages_email!
     create_user_promoted_email!
+    create_profile_approved!
+  end
+
+  def create_profile_approved!
+    create_alert!(associated_class: WorkflowStep::UserWorkflow::ProfileApproved, name: 'notify user of profile approved', path: 'user_mailer/profile_approved', subject: "Your profile has been approved!", alert_type: 'email', recipient_type: 'enquirer')
   end
 
   def create_unread_messages_email!
