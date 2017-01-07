c1117efdc73c41fdb2a2dfd9765b9d959834fe5e
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2017-01-07 13:42:16 +0100

    make it possible to toggle approval for drivers

--- app/controllers/instance_admin/manage/user_profiles_controller.rb
+++ app/controllers/instance_admin/manage/user_profiles_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 class InstanceAdmin::Manage::UserProfilesController < InstanceAdmin::Manage::BaseController
   def approve
-    resource.approved = true
-    resource.enabled = true
+    resource.approved = !resource.approved
+    resource.enabled = resource.approved
     resource.save(validate: false)
     redirect_to request.referer.presence || instance_admin_manage_users_path
   end
