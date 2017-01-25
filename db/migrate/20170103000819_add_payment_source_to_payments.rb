0815988aae12e745ccd3542665166e2dec878736
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2017-01-25 16:01:23 +0100

    fix migration

--- db/migrate/20170103000819_add_payment_source_to_payments.rb
+++ db/migrate/20170103000819_add_payment_source_to_payments.rb
@@ -1,10 +1,11 @@
 class AddPaymentSourceToPayments < ActiveRecord::Migration
+
   def change
     add_column :payments, :payment_source_id, :integer
     add_column :payments, :payment_source_type, :string
 
-    PaymentSource.reset_column_information
-    PaymentSource.update_all("payment_source_type = 'CreditCard'")
-    PaymentSource.update_all("payment_source_id = credit_card_id")
+    Payment.reset_column_information
+    Payment.update_all("payment_source_type = 'CreditCard'")
+    Payment.update_all("payment_source_id = credit_card_id")
   end
 end
