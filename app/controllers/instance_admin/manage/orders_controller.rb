88a61de82ff82bc1cd7e3e6bf3d0e661390df5a8
Andrei Canciu <andrei@canciu.ro>
2017-02-02 01:48:29 +1200

    NM-7235-uot-orders-reports-improved

--- app/controllers/instance_admin/manage/orders_controller.rb
+++ app/controllers/instance_admin/manage/orders_controller.rb
@@ -4,7 +4,10 @@ class InstanceAdmin::Manage::OrdersController < InstanceAdmin::Manage::BaseContr
   before_action :find_order, except: :index
 
   def index
-    @orders = order_scope.paginate(per_page: 10, page: params[:page])
+    respond_to do |format|
+      format.html { @orders = get_orders_from_params.paginate(per_page: 10, page: params[:page]) }
+      format.csv { send_data generate_csv(OrderDecorator.decorate_collection(get_orders_from_params)) }
+    end
   end
 
   def show
@@ -20,11 +23,28 @@ class InstanceAdmin::Manage::OrdersController < InstanceAdmin::Manage::BaseContr
 
   private
 
+  def generate_csv(orders)
+    CSV.generate do |csv|
+      csv << OrderDecorator.column_headings_for_report
+
+      orders.each do |resource|
+        csv << resource.column_values_for_report
+      end
+    end
+  end
+
+  def get_orders_from_params
+    scope_search_form = InstanceAdmin::OrderSearchForm.new
+    scope_search_form.validate(params)
+
+    SearchService.new(order_scope).search(scope_search_form.to_search_params)
+  end
+
   def find_order
     @order = order_scope.find(params[:id])
   end
 
   def order_scope
-    @order_scope ||= Order.without_state(:inactive).order('created_at DESC')
+    @order_scope ||= Order.without_state(:inactive)
   end
 end
