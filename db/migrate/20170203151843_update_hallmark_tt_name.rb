184ae46ddc4b6f745f07ec12881fb5640b9677a4
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2017-02-03 19:32:43 +0100

    HMK-43 change diy to share

--- db/migrate/20170203151843_update_hallmark_tt_name.rb
+++ db/migrate/20170203151843_update_hallmark_tt_name.rb
@@ -3,11 +3,25 @@ class UpdateHallmarkTtName < ActiveRecord::Migration
     i = Instance.find_by(id: 5011)
     return true if i.nil?
     i.set_context!
-    Translation.where(value: '%DIY%', instance_id: 5011).each do |t|
+    Translation.where('value like ?', '%DIY%').where(instance_id: 5011).each do |t|
       new_value = t.value.gsub('DIY', 'Share')
       puts "Updating #{t.value} to #{new_value}"
       t.update_attribute(:value, new_value)
     end
+
+    Dir.glob(Rails.root.join('marketplaces', 'hallmark', 'translations', '*.yml')).each do |yml_filename|
+      en_locales = YAML.load_file(yml_filename)
+      en_locales_hash = convert_hash_to_dot_notation(en_locales['en'])
+
+      en_locales_hash.each_pair do |key, value|
+        next if key.include?('transactable_type.diy')
+        next if Translation.where(key: key, instance_id: 5011).exists?
+        value = value.gsub('DIY', 'Share')
+        puts "Creating translation for: #{key} -> #{value}"
+        Translation.create!(locale: 'en', key: key, instance_id: 5011, value: value)
+      end
+    end
+
     Page.find_each do |p|
       p.update_attribute(:html_content, p.html_content.gsub('DIY', 'Share'))
     end
@@ -25,5 +39,20 @@ class UpdateHallmarkTtName < ActiveRecord::Migration
   end
 
   def down
+  end
+
+  protected
+
+
+  def convert_hash_to_dot_notation(hash, path = '')
+    hash.each_with_object({}) do |(k, v), ret|
+      key = path + k
+
+      if v.is_a? Hash
+        ret.merge! convert_hash_to_dot_notation(v, key + '.')
+      else
+        ret[key] = v
+      end
+    end
   end
 end
