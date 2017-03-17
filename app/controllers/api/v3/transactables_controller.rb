a9607df15e5a76f20a6f6188fac994effd3c4fe1
Maciej Krajowski-Kukiel <maciej@near-me.com>
2017-03-17 14:53:56 +0100

    fix test

--- app/controllers/api/v3/transactables_controller.rb
+++ app/controllers/api/v3/transactables_controller.rb
@@ -10,7 +10,7 @@ module Api
     def index
       params[:v] = 'listing_mixed'
       search_params = params
-      @searcher = InstanceType::SearcherFactory.new(@transactable_type, search_params, result_view, current_user).get_searcher
+      @searcher = InstanceType::Searcher::GeolocationSearcher::Listing.new(@transactable_type, search_params).tap(&:invoke)
       render json: ApiSerializer.serialize_collection(
         @searcher.results.includes(categories: [:parent]),
         include: ['categories', 'action-type', 'action-type.pricings'],
