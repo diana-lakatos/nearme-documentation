8cb909965b5c2dcaff9bc16b9d884ec72f68f18f
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2017-03-20 13:38:48 +0100

    NM-7826: fix ranges in liquid 4
      we can't use floats in ranges

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314140919) do
+ActiveRecord::Schema.define(version: 20170320112322) do
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
