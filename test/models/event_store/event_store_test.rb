6a82d89610cd38d54f3b6534d235ad135c3c412c
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2017-03-21 13:42:19 +0100

    NM-0000: dirty fix for event store to handle user decorator

--- test/models/event_store/event_store_test.rb
+++ test/models/event_store/event_store_test.rb
@@ -27,4 +27,27 @@ class EventStoreTest < ActiveSupport::TestCase
 
     assert_equal event.event_type, 'WorkflowStep::SignUpWorkflow::AccountCreated'
   end
+
+  test 'publish event from workflow step with user decorator' do
+    user = FactoryGirl.create(:user)
+    step = WorkflowStep::SignUpWorkflow::AccountCreated.new(user.id)
+
+    event = step.invoke!(user.decorate)
+
+    assert_equal event.event_type, 'WorkflowStep::SignUpWorkflow::AccountCreated'
+  end
+
+  test 'publish event with user decorator' do
+    ::DummyEvent = Class.new(EventStore::Event)
+    user = FactoryGirl.create(:user)
+
+    event = EventStore.publish_event(
+      DummyEvent.new(
+        payload: { foo: :bar, user: user },
+        triggered_by: user.decorate
+      )
+    )
+
+    assert_equal event.payload[:foo], :bar
+  end
 end
