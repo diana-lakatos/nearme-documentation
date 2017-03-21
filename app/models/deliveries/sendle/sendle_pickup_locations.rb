d69bf00d2d4d2697dec82860679beed26f69cf36
darek <darek@near-me.com>
2017-03-21 15:20:14 +0100

    NM-7840-improved-sendle-address-validations

--- app/models/deliveries/sendle/sendle_pickup_locations.rb
+++ app/models/deliveries/sendle/sendle_pickup_locations.rb
@@ -1,8 +1,7 @@
 module Deliveries
   class Sendle
     class SendlePickupLocations
-      def initialize
-      end
+      cattr_accessor :locations
 
       def all
         pickup_locations
@@ -25,7 +24,18 @@ module Deliveries
       end
 
       def pickup_locations
-        @locations ||= CSV.read('./vendor/gems/sendle_api/sendle_pickup_locations.csv', headers: true)
+        self.class.locations ||= CSV.read('./vendor/gems/sendle_api/sendle_pickup_locations.csv', headers: true).each_with_object([]) do |row, rows|
+          rows << row
+
+          if row['Suburb'].start_with? 'Mount '
+            rows << copy_row(row).tap { |r| r['Suburb'] = r['Suburb'].gsub(/^Mount\ /, 'Mt ') }
+            rows << copy_row(row).tap { |r| r['Suburb'] = r['Suburb'].gsub(/^Mount\ /, 'Mt. ') }
+          end
+        end
+      end
+
+      def copy_row(row)
+        CSV::Row.new(row.headers, row.fields)
       end
 
       def normalize(suburb)
--- test/unit/deliveries/sendle/validations/address_test.rb
+++ test/unit/deliveries/sendle/validations/address_test.rb
@@ -36,7 +36,7 @@ class Deliveries::Sendle::Validations::AddressTest < ActiveSupport::TestCase
     valid_au_addresses.each do |address|
       validator.validate(address)
 
-      assert_equal address.errors.messages, {}, format('Address failed: %s', address.address)
+      assert_equal address.errors.messages, {}, format('Address failed: %s [%s]', address.address, address.suburb, address.postcode)
     end
   end
 
@@ -58,30 +58,41 @@ class Deliveries::Sendle::Validations::AddressTest < ActiveSupport::TestCase
 
   def au_addresses
     [
-      'Potts Point               | New South Wales              | 2011     | Australia | Sydney, 152 Victoria Street, Potts Point, New South Wales, Australia
-       Browns Plains             | Queensland                   | 4118     | Australia | 748-750 Wembley Rd, Browns Plains, QLD, 4118, Australia
-       Ashfield                  | New South Wales              | 2131     | Australia | 73 Milton Street, Ashfield, New South Wales, Australia
-       Saint Leonards            | New South Wales              | 2065     | Australia | 1, Sergeants Lane, Saint Leonards, New South Wales, Australia
-       Maroubra                  | New South Wales              | 2035     | Australia | Maroubra, New South Wales, Australia
-       Camperdown                | New South Wales              | 2050     | Australia | 16-22 Australia St, Camperdown NSW 2050, Australia
-       Glebe                     | New South Wales              | 2037     | Australia | 4/14 Leichhardt St, Glebe, NSW, 2037, Australia
-       Red Hill                  | Australian Capital Territory | 2603     | Australia | 3 Nuyts St, Red Hill ACT 2603, Australia
-       Harris Park               | New South Wales              | 2150     | Australia | 2-6, Kendall St, Harris Park, NSW, Australia
-       Kensington                | New South Wales              | 2033     | Australia | Lorne Ave, Kensington NSW 2033, Australia
-       Wolli Creek               | New South Wales              | 2205     | Australia | 18-26, Allen Street, Wolli Creek, New South Wales, Australia
-       Croydon North             | Victoria                     | 3136     | Australia | 4 Hurst Court, Croydon North, Victoria, Australia
-       Turrella                  | New South Wales              | 2205     | Australia | 10, Alexandra St, Turrella, New South Wales, Australia
-       Coogee                    | New South Wales              | 2034     | Australia | 90 Mount Street, Coogee, New South Wales, Australia
-       South Yarra               | Victoria                     | 3141     | Australia | 11-17 Daly Street, SouthYarra, melbourne 3141.
-                                 | Queensland                   |          | Australia | Cairns, Queensland, Australia
-       Wentworthville            | New South Wales              | 2145     | Australia | Wentworthville Station, Wentworthville, New South Wales, Australia
-       Rushcutters Bay           | New South Wales              | 2011     | Australia | 2, Kurraghein Avenue, Rushcutters Bay, New South Wales, Australia
-       Mosman                    | New South Wales              | 2088     | Australia | 100 Spit Rd, Mosman, New South Wales, Australia
-       Melbourne                 | Victoria                     | 3000     | Australia | 283, Spring Street, Melbourne, Victoria, Australia
-       Ultimo                    | New South Wales              | 2007     | Australia | 558 Jones Street, Ultimo, NSW, Australia
-       Saint Kilda               | Victoria                     | 3182     | Australia | 416 St Kilda Road, Saint Kilda, Victoria, Australia
-       Lane Cove                 | New South Wales              | 2066     | Australia | Grace St, Lane Cove NSW 2066, Australia
-       Surry Hills               | New South Wales              | 2010     | Australia | 74/76 Campbell St, Surry Hills NSW 2010, Australia'
+      'Potts Point               | New South Wales              | 2011     | Australia | Sydney, 152 Victoria Street, Potts Point, New South Wales, Australia',
+       'Browns Plains             | Queensland                   | 4118     | Australia | 748-750 Wembley Rd, Browns Plains, QLD, 4118, Australia',
+       'Ashfield                  | New South Wales              | 2131     | Australia | 73 Milton Street, Ashfield, New South Wales, Australia',
+       'Saint Leonards            | New South Wales              | 2065     | Australia | 1, Sergeants Lane, Saint Leonards, New South Wales, Australia',
+       'Maroubra                  | New South Wales              | 2035     | Australia | Maroubra, New South Wales, Australia',
+       'Camperdown                | New South Wales              | 2050     | Australia | 16-22 Australia St, Camperdown NSW 2050, Australia',
+       'Glebe                     | New South Wales              | 2037     | Australia | 4/14 Leichhardt St, Glebe, NSW, 2037, Australia',
+       'Red Hill                  | Australian Capital Territory | 2603     | Australia | 3 Nuyts St, Red Hill ACT 2603, Australia',
+       'Harris Park               | New South Wales              | 2150     | Australia | 2-6, Kendall St, Harris Park, NSW, Australia',
+       'Kensington                | New South Wales              | 2033     | Australia | Lorne Ave, Kensington NSW 2033, Australia',
+       'Wolli Creek               | New South Wales              | 2205     | Australia | 18-26, Allen Street, Wolli Creek, New South Wales, Australia',
+       'Croydon North             | Victoria                     | 3136     | Australia | 4 Hurst Court, Croydon North, Victoria, Australia',
+       'Turrella                  | New South Wales              | 2205     | Australia | 10, Alexandra St, Turrella, New South Wales, Australia',
+       'Coogee                    | New South Wales              | 2034     | Australia | 90 Mount Street, Coogee, New South Wales, Australia',
+       'South Yarra               | Victoria                     | 3141     | Australia | 11-17 Daly Street, SouthYarra, melbourne 3141',
+       'Wentworthville            | New South Wales              | 2145     | Australia | Wentworthville Station, Wentworthville, New South Wales, Australia',
+       'Rushcutters Bay           | New South Wales              | 2011     | Australia | 2, Kurraghein Avenue, Rushcutters Bay, New South Wales, Australia',
+       'Mosman                    | New South Wales              | 2088     | Australia | 100 Spit Rd, Mosman, New South Wales, Australia',
+       'Melbourne                 | Victoria                     | 3000     | Australia | 283, Spring Street, Melbourne, Victoria, Australia',
+       'Ultimo                    | New South Wales              | 2007     | Australia | 558 Jones Street, Ultimo, NSW, Australia',
+       'Saint Kilda               | Victoria                     | 3182     | Australia | 416 St Kilda Road, Saint Kilda, Victoria, Australia',
+       'Lane Cove                 | New South Wales              | 2066     | Australia | Grace St, Lane Cove NSW 2066, Australia',
+       'Surry Hills               | New South Wales              | 2010     | Australia | 74/76 Campbell St, Surry Hills NSW 2010, Australia',
+       'Mount Lawley              | Western Australia            | 6050     | Australia | 100A Alma Rd, Mount Lawley WA 6050, Australia',
+       'Mt Lawley                 | Western Australia            | 6050     | Australia | 100A Alma Rd, Mount Lawley WA 6050, Australia',
+       'Mt. Lawley                 | Western Australia            | 6050     | Australia | 100A Alma Rd, Mount Lawley WA 6050, Australia',
+       'Mount Hawthorn            | Western Australia            | 6016     | Australia | 87 Federation Street, Mount Hawthorn, WA 6016',
+       'Mt Hawthorn               | Western Australia            | 6016     | Australia | 87 Federation Street, Mount Hawthorn, WA 6016',
+       'Mt. Hawthorn               | Western Australia            | 6016     | Australia | 87 Federation Street, Mount Hawthorn, WA 6016',
+       'Mount Hawthorn            | Western Australia            | 6016     | Australia | 87 Federation St, Mount Hawthorn WA 6016, Australia',
+       'Mt Hawthorn               | Western Australia            | 6016     | Australia | 87 Federation St, Mount Hawthorn WA 6016, Australia',
+       'Mt. Hawthorn               | Western Australia            | 6016     | Australia | 87 Federation St, Mount Hawthorn WA 6016, Australia',
+       'Mount Lawley              | Western Australia            | 6050     | Australia | 100A Alma Road, Mount Lawley 6050',
+       'Mt Lawley                 | Western Australia            | 6050     | Australia | 100A Alma Road, Mount Lawley 6050',
+       'Mt. Lawley                 | Western Australia            | 6050     | Australia | 100A Alma Road, Mount Lawley 6050'
     ].map { |row| row.split('|').map(&:strip) }
   end
 
