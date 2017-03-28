cbdaaffa1309a5229f0524d6601b1ec566d97c6f
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2017-03-28 16:43:01 +0200

    NM-7933: add pagination

--- app/controllers/api/v3/activity_feed_events_controller.rb
+++ app/controllers/api/v3/activity_feed_events_controller.rb
@@ -5,8 +5,28 @@ module Api
       skip_before_action :require_authentication
 
       def index
-        events = ActivityFeedEvent.all.order(:created_at)
-        render json: ApiSerializer.serialize_collection(events, namespace: ::V3)
+        events = ActivityFeedEvent.all.order(:created_at).paginate(page: page)
+        render json: ApiSerializer.serialize_collection(
+          events,
+          namespace: ::V3,
+          meta: { total_entries: events.total_entries, total_pages: events.total_pages },
+          links: pagination_links(events)
+        )
+      end
+
+      private
+
+      def pagination_links(events)
+        {
+          first: api_activity_feed_events_url(page: 1),
+          last: api_activity_feed_events_url(page: events.total_pages),
+          prev: page > 1 ? api_activity_feed_events_url(page: page - 1) : nil,
+          next: page < events.total_pages ? api_activity_feed_events_url(page: page + 1) : nil
+        }
+      end
+
+      def page
+        params[:page] || 1
       end
     end
   end
