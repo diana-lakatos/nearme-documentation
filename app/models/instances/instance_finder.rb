69a3db46f2024235c897b748fb9bb3cb0c2e9c86
Maciej Krajowski-Kukiel <maciej@near-me.com>
2017-04-05 10:12:45 +0200

    add finder

---
+++ app/models/instances/instance_finder.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+module Instances
+  module InstanceFinder
+    INSTANCE_IDS = { # use them in data migrations
+      bronxchange: 5014,
+      devmesh:     132,
+      hallmark:    5011,
+      litvault:    198,
+      localdriva:  211, # toodoooolooooo
+      spacerau:    130,
+      spacercom:   5020,
+      thevolte:    194,
+      toodooloo:   211,
+      uot:         195
+    }.freeze
+
+    # use it in data migrations ex. Instances::InstanceFinder.get(:uot).each
+    def self.get(*instance_names)
+      Instance.where(id: INSTANCE_IDS.slice(*instance_names).values)
+    end
+  end
+end
