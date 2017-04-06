3ce5f03b1461cf6540ed83cbff611d80fe2aa0fe
Krzysztof Danek <chrisd@near-me.com>
2017-04-06 22:18:27 +0200

    NM-0000 Disabled sourcemaps

--- gulp/webpack/production.config.js
+++ gulp/webpack/production.config.js
@@ -1,11 +1,6 @@
 var
   webpack = require('webpack'),
-  config = require('./manifest.config.js'),
-  _ = require('lodash');
-
-config = _.merge(config, {
-  devtool: 'source-map'
-});
+  config = require('./manifest.config.js');
 
 config.plugins.push(
   new webpack.optimize.UglifyJsPlugin({
