f59df5cf843e31828de48d435e07e4b8f6f1513b
Andrei Canciu <andrei@canciu.ro>
2017-04-12 03:01:29 +1300

    NM-8046 User phone liquid improvements

---
+++ lib/phone_helper.rb
@@ -0,0 +1,27 @@
+class PhoneHelper
+
+  def initialize(number:, country:)
+    @number = number
+    @country = country
+  end
+
+  def country_calling_code
+    return unless @country.present?
+
+    @country.calling_code
+  end
+
+  def full_number
+    return unless @number.present?
+
+    number_with_calling_code(@number)
+  end
+
+  private
+
+  def number_with_calling_code(number)
+    calling_code = country_calling_code.present? ? "+#{country_calling_code}" : ""
+    "#{calling_code}#{number.gsub(/^0/, '')}"
+  end
+
+end
