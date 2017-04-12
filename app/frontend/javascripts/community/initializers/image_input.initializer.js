20802f8b9b802114af53c1b76fc9c44909178c99
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2017-04-12 10:30:25 +0200

    NM-8061: make title image upload nicer
      JavaScript for image upload
    
      Icon

---
+++ app/frontend/javascripts/community/initializers/image_input.initializer.js
@@ -0,0 +1,19 @@
+function run(context = 'body') {
+  var els = $(context).find('[data-image-input]');
+  if (els.length > 0) {
+    require.ensure('../../dashboard/modules/image_input', function(require){
+      var ImageInput = require('../../dashboard/modules/image_input');
+      els.each(function(){
+        return new ImageInput(this);
+      });
+    });
+  }
+}
+
+let cocoonWrapper = $('.customizations');
+
+cocoonWrapper.on('cocoon:after-insert',  ()=>{
+  run(cocoonWrapper);
+});
+
+run();
