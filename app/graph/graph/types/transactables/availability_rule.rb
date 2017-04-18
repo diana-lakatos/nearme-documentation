dde47552107af78521e26e8a86f4ef1de53ed22e
Maciej Krajowski-Kukiel <maciej@near-me.com>
2017-04-18 17:55:13 +0200

    NM-0000 add availability template and exceptions to graph

---
+++ app/graph/graph/types/transactables/availability_rule.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+module Graph
+  module Types
+    module Transactables
+      AvailabilityRule = GraphQL::ObjectType.define do
+        name 'AvailabilityRule'
+
+        global_id_field :id
+
+        field :id, !types.Int
+        field :days, types[types.Int]
+        field :open_hour, types.Int
+        field :open_minute, types.Int
+        field :close_hour, types.Int
+        field :close_minute, types.Int
+      end
+    end
+  end
+end
