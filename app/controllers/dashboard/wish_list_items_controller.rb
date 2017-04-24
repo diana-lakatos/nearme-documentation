1304ac449f880513a5f4a7de0daada61c1480fc7
piotrze <piotr.bliszczyk@gmail.com>
2017-04-24 22:16:25 +0200

    NM-8174: refactor wish list (#5522)

--- app/controllers/dashboard/wish_list_items_controller.rb
+++ app/controllers/dashboard/wish_list_items_controller.rb
@@ -1,19 +1,17 @@
 class Dashboard::WishListItemsController < Dashboard::BaseController
-  before_filter :check_wish_lists_enabled
-  before_filter :find_default_wish_list
-  before_filter :find_item, only: [:destroy]
+  before_action :check_wish_lists_enabled
 
   def index
-    @items = @wish_list.items.by_date.decorate
+    @items = wish_list.items.by_date.decorate
   end
 
   def destroy
-    @item.destroy
+    item.destroy
     redirect_to dashboard_wish_list_items_path, notice: t('flash_messages.wish_list_items.item_deleted')
   end
 
   def clear
-    @wish_list.items.destroy_all
+    wish_list.items.destroy_all
     redirect_to dashboard_wish_list_items_path, notice: t('flash_messages.wish_list_items.all_items_deleted')
   end
 
@@ -23,11 +21,11 @@ class Dashboard::WishListItemsController < Dashboard::BaseController
     redirect_to(dashboard_path, notice: t('wish_lists.notices.wish_lists_disabled')) unless platform_context.instance.wish_lists_enabled?
   end
 
-  def find_default_wish_list
-    @wish_list = current_user.default_wish_list || current_user.wish_lists.create(name: t('wish_lists.name'), default: true)
+  def wish_list
+    @wish_list ||= current_user.default_wish_list
   end
 
-  def find_item
-    @item = @wish_list.items.find params[:id]
+  def item
+    wish_list.items.find params[:id]
   end
 end
