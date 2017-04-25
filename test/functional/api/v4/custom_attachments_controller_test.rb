914923b9a35db68a5935e913c6f76d4df4a76b7f
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2017-04-25 16:45:59 +0200

    NM-8164: move code to api controller

---
+++ test/functional/api/v4/custom_attachments_controller_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+module Api
+  module V4
+    class User::CustomAttachmentsControllerTest < ActionController::TestCase
+      setup do
+        @custom_attachment = FactoryGirl.create(:custom_attachment)
+      end
+
+      context 'with authorized user' do
+        setup do
+          sign_in FactoryGirl.create(:user)
+        end
+
+        should 'get file url' do
+          get :show, id: @custom_attachment
+
+          assert_redirected_to @custom_attachment.file.url
+        end
+      end
+
+      context 'without authorized user' do
+        should 'not see the attachment' do
+          get :show, id: @custom_attachment
+
+          assert_response :unauthorized
+        end
+      end
+    end
+  end
+end
