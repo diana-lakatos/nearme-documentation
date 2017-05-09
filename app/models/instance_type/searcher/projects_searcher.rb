ca11b9378bc3df89cf549e867dca53e09e38a04a
Maciej Krajowski-Kukiel <maciej@near-me.com>
2017-05-09 12:07:10 +0200

    NM-7207 filter by role for intel projects

--- app/models/instance_type/searcher/projects_searcher.rb
+++ app/models/instance_type/searcher/projects_searcher.rb
@@ -14,6 +14,7 @@ class InstanceType::Searcher::ProjectsSearcher
     @fetcher = @fetcher.by_topic(selected_topic_ids).custom_order(@params[:sort])
     @fetcher = @fetcher.seek_collaborators if @params[:seek_collaborators] == '1'
     @fetcher = @fetcher.joins(:categories).where(categories: { id: selected_category_ids }).distinct if selected_category_ids.present?
+    @fetcher = @fetcher.joins(creator: :default_profile).merge(UserProfile.filtered_by_custom_attribute('role', selected_roles)) if selected_roles.present?
     @fetcher = @fetcher.group('transactable_topics.id') if @params[:sort] =~ /collaborators/i && selected_topic_ids.present?
     @fetcher = @fetcher.paginate(page: @params[:page], per_page: @params[:per_page])
     @fetcher
@@ -29,6 +30,10 @@ class InstanceType::Searcher::ProjectsSearcher
 
   def selected_category_ids
     @params[:category_ids]&.select(&:present?)
+  end
+
+  def selected_roles
+    @params[:roles]&.select(&:present?)
   end
 
   def search_query_values
