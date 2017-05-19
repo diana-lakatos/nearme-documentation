f68f2c9a975d8f1afbcaec938d94385b3731cf78
Andrei Canciu <andrei@canciu.ro>
2017-05-20 03:04:58 +1300

    NM-8501 Liquidized support page

--- app/drops/support/ticket_drop.rb
+++ app/drops/support/ticket_drop.rb
@@ -1,32 +1,47 @@
 # frozen_string_literal: true
 class Support::TicketDrop < BaseDrop
-  # @return [Support::TicketDrop]
-  attr_reader :ticket
-
   def initialize(ticket)
-    @ticket = ticket
+    @source = ticket.decorate
   end
 
+  # @!method id
+  #   @return [Integer] numeric identifier for this ticket
   # @!method first_message
   #   @return [Support::TicketMessageDrop] first message for this support ticket thread
-  delegate :first_message, to: :ticket
+  # @!method target_rfq?
+  #   @return [Boolean] whether the ticket is connected to a Transactable
+  # @!method show_target_path
+  #   @return [String] url to the Transactable to which a ticket is connected
+  # @!method target
+  #   @return [Object] object to which this ticket is connected
+  # @!method verb
+  #   @return [String] status of the ticket in past tense
+  # @!method open_text
+  #   @return [String] text to be displayed for the action of opening a ticket e.g.
+  #     'View and resolve' for opened tickets, or 'View' for closed tickets
+  # @!method updated_at
+  #   @return [Date] updated at date for the ticket
+  # @!method recent_message
+  #   @return [Support::TicketMessageDrop] most recent message in the thread
+  delegate :id, :first_message, :target_rfq?, :show_target_path, :target, :verb, :open_text,
+           :updated_at, :recent_message, to: :source
 
   # @return [String] date/time when this ticket was created
   # @todo -- remove, DIY
   def created_at
-    ticket.created_at.to_s
+    source.created_at.to_s
   end
 
   # @return [String] url to this user's requests for quotes
   # @todo -- depracate, url filter
   def url
-    routes.dashboard_user_requests_for_quote_path(ticket) if @ticket.user && !@ticket.target.is_a?(Instance)
+    routes.dashboard_user_requests_for_quote_path(source) if @source.user && !@source.target.is_a?(Instance)
   end
 
   # @return [String] 'request' if free booking is enabled for the target listing
   #   otherwise returns 'offer'
   def rfq
-    if ticket.target.action_free_booking?
+    if source.target.action_free_booking?
       'request'
     else
       'offer'
@@ -41,25 +56,21 @@ class Support::TicketDrop < BaseDrop
   #   thread; this is the section where the admin can answer and resolve requests
   # @todo -- depracate, url filter
   def admin_url
-    case ticket.target
+    case source.target
     when Transactable
-      routes.dashboard_company_support_ticket_path(ticket)
+      routes.dashboard_company_support_ticket_path(source)
     when Instance
-      routes.instance_admin_support_ticket_path(ticket)
+      routes.instance_admin_support_ticket_path(source)
     else
-      raise NotImplementedError, "Unknown ticket target: #{ticket.target.class}"
+      raise NotImplementedError, "Unknown ticket target: #{source.target.class}"
     end
   end
-
-  # @!method id
-  #   @return [Integer] numeric identifier for this ticket
-  delegate :id, to: :ticket
 
   # @return [Array<Support::TicketMessageDrop>] the messages in this thread with the first one
   #   omitted
   def messages
-    if !ticket.messages.empty?
-      ticket.messages[1..-1]
+    if !source.messages.empty?
+      source.messages[1..-1]
     else
       []
     end
