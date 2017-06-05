e2c8e06f813c993e60bf95cf683cd0d1c731609a
Pawel Kowalski <pawel@near-me.com>
2017-06-05 10:51:11 +0200

    NM-0000 Try to fix pronto reports

--- ci/run_acceptance_tests.sh
+++ ci/run_acceptance_tests.sh
@@ -8,7 +8,7 @@ yarn --force --frozen-lockfile
 
 # Make sure staging is in git refs
 git fetch origin staging
-bundle exec pronto run -c origin/staging -f github_pr github_status --exit-code -r=rails_best_practices Brakeman ESLint Fasterer Reek Rubocop
+bundle exec pronto run -f github_pr github_status --exit-code -r=rails_best_practices Brakeman ESLint Fasterer Reek Rubocop -c origin/staging
 
 # build assets
 node ./node_modules/gulp/bin/gulp.js build:test
