be461479cfaf1d881c6acee1e4c6f21c0acb5e74
Michal Janeczek <michal.janeczek@ymail.com>
2017-06-07 17:15:02 +0200

    NM-8509 Ability to search by two attributes, fix workflow alert converter

--- app/interactors/new_marketplace_builder/converters/workflow_alert_converter.rb
+++ app/interactors/new_marketplace_builder/converters/workflow_alert_converter.rb
@@ -1,7 +1,7 @@
 module NewMarketplaceBuilder
   module Converters
     class WorkflowAlertConverter < BaseConverter
-      primary_key :template_path
+      primary_key :template_path, find_by: [:template_path, :recipient_type]
       properties :name, :alert_type, :recipient_type, :template_path, :delay, :subject, :layout_path, :from, :reply_to
 
       def scope
