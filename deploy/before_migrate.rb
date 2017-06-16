6d7a08157c2aa10c3a11ee48c4f3f881e126cce9
Andreas Wittig <andreas@widdix.de>
2017-06-16 15:32:49 +0200

    fixed typo in deploy/before_migrate.rb

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -4,7 +4,7 @@ file 'Create shared/config/application.yml'  do
   group new_resource.group
   owner new_resource.user
   mode '0660'
-  content YAML.dump(new_resource.environment.to_hash))
+  content YAML.dump(new_resource.environment.to_hash)
 end
 
 directory 'Create shared/node_modules' do
