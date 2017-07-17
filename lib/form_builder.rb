6e9111b477da57ffb9a572f084a411afeb35ce04
Maciej Krajowski-Kukiel <maciej@near-me.com>
2017-07-17 11:26:45 +0200

    NM-0000 cache anonymous classes for forms

--- lib/form_builder.rb
+++ lib/form_builder.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 class FormBuilder
+  def self.form_class_cache
+    @form_class_cache ||= FormClassCache.new
+  end
+
   def initialize(configuration:, base_form:, object:)
     @configuration = configuration
     @base_form = base_form
@@ -7,6 +11,26 @@ class FormBuilder
   end
 
   def build
-    @base_form.decorate(@configuration).new(@object)
+    fetch_from_form_class_cache do
+      @base_form.decorate(@configuration)
+    end.new(@object)
+  end
+
+  def fetch_from_form_class_cache
+    self.class.form_class_cache.fetch(base_form: @base_form, configuration: @configuration) do
+      yield
+    end
+  end
+
+  class FormClassCache
+    def initialize
+      @cache = {}
+    end
+
+    def fetch(base_form:, configuration:)
+      base_form = base_form.to_s
+      @cache[base_form] ||= {}
+      @cache[base_form][Digest::MD5.digest(configuration.to_s)] ||= yield
+    end
   end
 end
