a291802cfeb320b0a04fd3f5b8298f1f254785b2
Krzysztof Streflik <streflik@gmail.com>
2017-07-25 09:20:52 +0200

    NM-8864 add transactable_id to orders query

--- lib/liquid_blank_file_system.rb
+++ lib/liquid_blank_file_system.rb
@@ -13,20 +13,19 @@ module Liquid
         locale: [::I18n.locale]
       }
 
-      details.merge!(context.registers[:controller].send(:details_for_lookup)) if context.registers[:controller]
-
       result = begin
-        context.registers[:controller].lookup_context.find_template(path, '', true, details).source
+        if context.registers[:controller]
+          details.merge!(context.registers[:controller].send(:details_for_lookup))
+          context.registers[:controller].lookup_context.find_template(path, '', true, details).source
+        else
+          details[:instance_id] = PlatformContext.current.try(:instance).try(:id)
+          details[:custom_theme_id] = PlatformContext.current.try(:custom_theme).try(:id)
+          InstanceViewResolver.instance.find_templates(path, '', true, details).first.source
+        end
       rescue
         # our UI is not great, MPO might not check 'partial' - this is why this rescue
         begin
-          if context.registers[:controller]
-            context.registers[:controller].lookup_context.find_template(path, '', false, details).source
-          else
-            details[:instance_id] = PlatformContext.current.try(:instance).try(:id)
-            details[:custom_theme_id] = PlatformContext.current.try(:custom_theme).try(:id)
-            InstanceViewResolver.instance.find_templates(path, '', true, details).first.source
-          end
+          context.registers[:controller].lookup_context.find_template(path, '', false, details).source
         rescue
           Rails.logger.warn("Liquid Error: can't find LiquidView with path #{path}. Make sure it has been added in Marketplace Admin")
           ''
