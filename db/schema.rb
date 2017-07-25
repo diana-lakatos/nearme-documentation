19e75ac9b21ca9c832417aabdd6282073ab57bbf
dariusz gorzeba <dgorzeba@gmail.com>
2017-07-25 13:44:11 +0200

    NM-8880-uot-set-transactable-seek-collabs-on (#5966)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717115146) do
+ActiveRecord::Schema.define(version: 20170725091003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
