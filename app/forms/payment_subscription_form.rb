dcf48b20b70e6b0b1537b2e49d4945cceab1a035
Krzysztof Streflik <streflik@gmail.com>
2017-07-26 10:43:41 +0200

    NM-8864 fixing payment subscription form

--- app/forms/payment_subscription_form.rb
+++ app/forms/payment_subscription_form.rb
@@ -5,6 +5,10 @@ class PaymentSubscriptionForm < BaseForm
       Class.new(self) do
         inject_dynamic_fields(configuration, whitelisted: [:company_id, :payer_id, :payment_method_id, :credit_card_id, :bank_account_id, :payment_source_id, :payment_source_type, :chosen_credit_card_id])
 
+        # @!attribute credit_card_token
+        #   @return [String] token string identifying the credit card
+        property :credit_card_token, virtual: true
+
         def credit_card_token=(token)
           model.payer = model.subscriber.creator
           model.payment_source = CreditCard.new(credit_card_token: token,
@@ -19,8 +23,4 @@ class PaymentSubscriptionForm < BaseForm
     model.process!
     super
   end
-
-  # @!attribute credit_card_token
-  #   @return [String] token string identifying the credit card
-  property :credit_card_token, virtual: true
 end
